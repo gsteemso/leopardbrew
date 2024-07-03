356643686bcf4c97dc1ccd2bc466f85283782541
Gordon Steemson <gsteemso@gmail.com>
2024-07-03 14:37:44 -0700

    ruby:  bumped version from 2.4.1 to 3.3.3 (but build is partly broken)
    - :universal now works differently (broken upstream; had to use scour‐merge‐
      and‐meld instead, and differences are not yet all catalogued)
    
    - build fails on ppc64 with “branch out of range” (linker bug?)

--- Library/Formula/ruby.rb
+++ Library/Formula/ruby.rb
@@ -1,55 +1,41 @@
 class Ruby < Formula
   desc "Powerful, clean, object-oriented scripting language"
   homepage "https://www.ruby-lang.org/"
-  url "https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.1.tar.bz2"
-  sha256 "ccfb2d0a61e2a9c374d51e099b0d833b09241ee78fc17e1fe38e3b282160237c"
-  revision 1
-
-  bottle do
-  end
+  url "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.3.tar.xz"
+  sha256 "83c0995388399c9555bad87e70af069755b5a9d84bbaa74aa22d1e37ff70fc1e"
 
   head do
-    url "http://svn.ruby-lang.org/repos/ruby/trunk/"
+    url "https://github.com/ruby/ruby.git"
     depends_on "autoconf" => :build
   end
 
   option :universal
-  option "with-suffix", "Suffix commands with '24'"
+  option "with-suffix", "Suffix commands with “33”"
   option "with-doc", "Install documentation"
   option "with-tcltk", "Install with Tcl/Tk support"
 
-  depends_on "pkg-config" => :build
-  depends_on "readline" => :recommended
-  depends_on "gdbm" => :optional
-  depends_on "gmp" => :optional
-  depends_on "libffi" => :optional
-  depends_on "libyaml"
-  depends_on "openssl"
-  depends_on :x11 if build.with? "tcltk"
+  depends_on 'pkg-config' => :build
+  depends_on 'libyaml'
+  depends_on 'openssl3'
+  depends_on 'readline' => :recommended
+  depends_on 'gdbm' => :optional
+  depends_on 'gmp' => :optional
+  depends_on 'libffi' => :optional
+  depends_on :x11 if build.with? 'tcltk'
 
-  if true# MacOS.version <= :leopard
-    # fix for https://bugs.ruby-lang.org/issues/11054
-    patch do
-      url "https://github.com/ruby/ruby/commit/1c80c388d5bd48018c419a2ea3ed9f7b7514dfa3.patch?full_index=1"
-      sha256 "8ba0a24a36702d2cbc94aa73cb6f0b11793348b0158c11c8608e073c71601bb5"
-    end
-
-    # fix for https://bugs.ruby-lang.org/issues/13247
-    patch do
-      url "https://github.com/ruby/ruby/commit/9e1a9858c84142e32b1bc51b23fa06a025f98b46.patch?full_index=1"
-      sha256 "300f13461385804ddfb314d9b0880bb47ad4f53f48209681d193a800418c31e6"
-    end
-
-    # fix for ext/fiddle/libffi-3.2.1/src/x86/win32.S
-    # based on https://github.com/macports/macports-ports/blob/8964c98f0e33e4aaabc851d8b684f4c709edceef/devel/libffi/files/PR-44170.patch
-    patch :DATA
-  end
-
-  # fails_with :llvm do
-  #   build 2326
-  # end
+  patch :DATA if MacOS.version < :snow_leopard
 
   def install
+    if build.universal?
+      ENV.permit_arch_flags if superenv?
+      ENV.un_m64 if Hardware::CPU.family == :g5_64
+      archs = Hardware::CPU.universal_archs
+      mkdir 'arch-stashes'
+      dirs = []
+    else
+      archs = [MacOS.preferred_arch]
+    end
+
     # mcontext types had a member named `ss` instead of `__ss`
     # prior to Leopard; see
     # https://github.com/mistydemeo/tigerbrew/issues/473
@@ -75,29 +61,29 @@ class Ruby < Formula
 
     system "autoconf" if build.head?
 
-    args = %W[
-      --prefix=#{prefix} --enable-shared --disable-silent-rules
-      --with-sitedir=#{HOMEBREW_PREFIX}/lib/ruby/site_ruby
-      --with-vendordir=#{HOMEBREW_PREFIX}/lib/ruby/vendor_ruby
+    args = [
+      "--prefix=#{prefix}",
+      '--disable-silent-rules',
+      '--enable-debug-env',  # this enables an environment variable, not a debug build
+      '--enable-mkmf-verbose',
+      '--enable-shared',
+      '--with-mantype=man',
+      "--with-sitedir=#{HOMEBREW_PREFIX}/lib/ruby/site_ruby",
+      "--with-vendordir=#{HOMEBREW_PREFIX}/lib/ruby/vendor_ruby"
     ]
 
-    if build.universal?
-      ENV.universal_binary
-      args << "--with-arch=#{Hardware::CPU.universal_archs.join(",")}"
-    end
-
-    args << "--program-suffix=24" if build.with? "suffix"
+    args << "--program-suffix=33" if build.with? "suffix"
     args << "--with-out-ext=tk" if build.without? "tcltk"
     args << "--disable-install-doc" if build.without? "doc"
     args << "--disable-dtrace" unless MacOS::CLT.installed?
     args << "--without-gmp" if build.without? "gmp"
 
-    # Reported upstream: https://bugs.ruby-lang.org/issues/10272
+    # see https://bugs.ruby-lang.org/issues/10272
     args << "--with-setjmp-type=setjmp" if MacOS.version == :lion
 
     paths = [
       Formula["libyaml"].opt_prefix,
-      Formula["openssl"].opt_prefix
+      Formula["openssl3"].opt_prefix
     ]
 
     %w[readline gdbm gmp libffi].each do |dep|
@@ -106,24 +92,66 @@ class Ruby < Formula
 
     args << "--with-opt-dir=#{paths.join(":")}"
 
-    system "./configure", *args
+    archs.each do |arch|
+      if build.universal?
+        case arch
+          when :i386, :ppc then ENV.m32
+          when :x86_64, :ppc64 then ENV.m64
+        end
+        mkdir "arch-stashes/#{arch}-bin"
+      end
 
-    # Ruby has been configured to look in the HOMEBREW_PREFIX for the
-    # sitedir and vendordir directories; however we don't actually want to create
-    # them during the install.
-    #
-    # These directories are empty on install; sitedir is used for non-rubygems
-    # third party libraries, and vendordir is used for packager-provided libraries.
-    inreplace "tool/rbinstall.rb" do |s|
-      s.gsub! 'prepare "extension scripts", sitelibdir', ""
-      s.gsub! 'prepare "extension scripts", vendorlibdir', ""
-      s.gsub! 'prepare "extension objects", sitearchlibdir', ""
-      s.gsub! 'prepare "extension objects", vendorarchlibdir', ""
-    end
+      args << "--with-arch=#{arch}"  # in theory this supports building fat binaries directly; in
+                                     # practice, it fails when it gets to the coroutines
+      # specifically, for some reason `configure` thinks the endianness is “universal”!
+      case arch
+        when :ppc then args.concat %w[ac_cv_c_bigendian=yes --with-coroutine=ppc]
+        when :ppc64 then args.concat %w[ac_cv_c_bigendian=yes --with-coroutine=ppc64]
+        when :i386 then args.concat %w[ac_cv_c_bigendian=no --with-coroutine=x86]
+        when :x86_64 then args.concat %w[ac_cv_c_bigendian=no --with-coroutine=amd64]
+      end
 
-    system "make"
-    system "make", "install"
-  end
+      args << 'ac_cv_func_fcopyfile=no' if MacOS.version < :snow_leopard
+
+      system "./configure", *args
+
+      # Ruby has been configured to look in the HOMEBREW_PREFIX for the
+      # sitedir and vendordir directories; however we don't actually want to create
+      # them during the install.
+      #
+      # These directories are empty on install; sitedir is used for non-rubygems
+      # third party libraries, and vendordir is used for packager-provided libraries.
+      inreplace "tool/rbinstall.rb" do |s|
+        s.gsub! 'prepare "extension scripts", sitelibdir', ""
+        s.gsub! 'prepare "extension scripts", vendorlibdir', ""
+        s.gsub! 'prepare "extension objects", sitearchlibdir', ""
+        s.gsub! 'prepare "extension objects", vendorarchlibdir', ""
+      end
+
+      system "make"
+      system "make", "install"
+      if build.universal?
+        system 'make', 'clean'
+        Merge.scour_keg(prefix, "arch-stashes/#{arch}-bin")
+        # these headers are architecture-dependent; when installing :universal, these copies will
+        # be used in merging them all together
+        #   .ext/include/ppc64-darwin9.0/ruby/config.h
+        #   (...)
+        mkdir "arch-stashes/#{arch}-h"
+#        cp include/'<header>', "arch-stashes/#{arch}-h/<header>"
+        # undo architecture-specific tweaks before next run
+        case arch
+          when :i386, :ppc then ENV.un_m32
+          when :x86_64, :ppc64 then ENV.un_m64
+        end # case arch
+      end # universal?
+    end # archs.each
+
+    if build.universal?
+      Merge.mach_o(prefix, 'arch-stashes', archs)
+      Merge.c_headers(include, 'arch-stashes', archs)
+    end # universal?
+  end # install
 
   def post_install
     # Customize rubygems to look/install in the global gem directory
@@ -135,10 +163,10 @@ class Ruby < Formula
     # Create the sitedir and vendordir that were skipped during install
     mkdir_p `#{bin}/ruby -e 'require "rbconfig"; print RbConfig::CONFIG["sitearchdir"]'`
     mkdir_p `#{bin}/ruby -e 'require "rbconfig"; print RbConfig::CONFIG["vendorarchdir"]'`
-  end
+  end # post_install
 
   def abi_version
-    "2.2.0"
+    "3.3.0"
   end
 
   def rubygems_config; <<-EOS.undent
@@ -200,97 +228,204 @@ class Ruby < Formula
       end
 
       def self.ruby
-        "#{opt_bin}/ruby#{"24" if build.with? "suffix"}"
+        "#{opt_bin}/ruby#{"33" if build.with? "suffix"}"
       end
     end
     EOS
-  end
+  end # rubygems_config
 
   test do
     output = `#{bin}/ruby -e "puts 'hello'"`
     assert_equal "hello\n", output
     assert_equal 0, $?.exitstatus
-  end
-end
+  end # test
+end # Ruby
+
+class Merge
+  module Pathname_extension
+    def is_bare_mach_o?
+      # header word 0, magic signature:
+      #   MH_MAGIC    = 'feedface' – value with lowest‐order bit clear
+      #   MH_MAGIC_64 = 'feedfacf' – same value with lowest‐order bit set
+      # low‐order 24 bits of header word 1, CPU type:  7 is x86, 12 is ARM, 18 is PPC
+      # header word 3, file type:  no types higher than 10 are defined
+      # header word 5, net size of load commands, is far smaller than the filesize
+      if (self.file? and self.size >= 28 and mach_header = self.binread(24).unpack('N6'))
+        raise('Fat binary found where bare Mach-O file expected') if mach_header[0] == 0xcafebabe
+        ((mach_header[0] & 0xfffffffe) == 0xfeedface and
+          [7, 12, 18].detect { |item| (mach_header[1] & 0x00ffffff) == item } and
+          mach_header[3] < 11 and
+          mach_header[5] < self.size)
+      else
+        false
+      end
+    end unless method_defined?(:is_bare_mach_o?)
+  end # Pathname_extension
+
+  class << self
+    include FileUtils
+
+    def scour_keg(keg_prefix, stash, sub_path = '')
+      # don’t suffer a double slash when sub_path is null:
+      s_p = (sub_path == '' ? '' : sub_path + '/')
+      Dir["#{keg_prefix}/#{s_p}*"].each do |f|
+        pn = Pathname(f).extend(Pathname_extension)
+        spb = s_p + pn.basename
+        if pn.directory?
+          Dir.mkdir "#{stash}/#{spb}"
+          scour_keg(keg_prefix, stash, spb)
+        # the number of things that look like Mach-O files but aren’t is horrifying, so test
+        elsif ((not pn.symlink?) and pn.is_bare_mach_o?)
+          cp pn, "#{stash}/#{spb}"
+        end
+      end
+    end # scour_keg
+
+    def c_headers(include_dir, stash_root, archs, sub_path = '')
+      # Architecture-specific <header>.<extension> files need to be surgically combined and were
+      # stashed for this purpose.  The differences are relatively minor and can be “#if defined ()”
+      # together.  We make the simplifying assumption that the architecture-dependent headers in
+      # question are present on all architectures.
+      #
+      # Don’t suffer a double slash when sub_path is null:
+      s_p = (sub_path == '' ? '' : sub_path + '/')
+      Dir["#{stash_root}/#{archs[0]}-h/#{s_p}*"].each do |basis_file|
+        spb = s_p + File.basename(basis_file)
+        if File.directory?(basis_file)
+          c_headers(include_dir, stash_root, archs, spb)
+        else
+          diffpoints = {}  # Keyed by line number in the basis file.  Each value is an array of
+                           # three‐element hashes; containing the arch, the hunk’s displacement
+                           # (number of basis‐file lines it replaces), and an array of its lines.
+          archs[1..-1].each do |a|
+            raw_diffs = `diff --minimal --unified=0 #{basis_file} #{stash_root}/#{a}-h/#{spb}`
+            next unless raw_diffs
+            # The unified diff output begins with two lines identifying the source files, which are
+            # followed by a series of hunk records, each describing one difference that was found.
+            # Each hunk record begins with a line that looks like:
+            # @@ -line_number,length_in_lines +line_number,length_in_lines @@
+            diff_hunks = raw_diffs.lines[2..-1].join('').split(/(?=^@@)/)
+            diff_hunks.each do |d|
+              # lexical sorting of numbers requires that they all be the same length
+              base_linenumber_string = ('00000' + d.match(/\A@@ -(\d+)/)[1])[-6..-1]
+              unless diffpoints.has_key?(base_linenumber_string)
+                diffpoints[base_linenumber_string] = []
+              end
+              length_match = d.match(/\A@@ -\d+,(\d+)/)
+              # if the hunk length is 1, the comma and second number are not present
+              length_match = (length_match == nil ? 1 : length_match[1].to_i)
+              line_group = []
+              # we want the lines that are either unchanged between files or only found in the non‐
+              # basis file; and to shave off the leading ‘+’ or ‘ ’
+              d.lines { |line| line_group << line[1..-1] if line =~ /^[+ ]/ }
+              diffpoints[base_linenumber_string] << {
+                :arch => a,
+                :displacement => length_match,
+                :hunk_lines => line_group
+              }
+            end # each diff hunk |d|
+          end # each arch |a|
+          # Ideally, the logic would account for overlapping and/or different-displacement hunks at
+          # this point; but since most packages do not seem to generate such in the first place, it
+          # can wait.  That said, packages exist (e.g. both Python 2 and Python 3) which can and do
+          # generate quad fat binaries, so it can’t be ignored forever.
+          basis_lines = []
+          File.open(basis_file, 'r') { |text| basis_lines = text.read.lines[0..-1] }
+          # Bear in mind that the line-array indices are one less than the line numbers.
+          #
+          # Start with the last diff point so the insertions don’t screw up our line numbering:
+          diffpoints.keys.sort.reverse.each do |index_string|
+            diff_start = index_string.to_i - 1
+            diff_end = index_string.to_i + diffpoints[index_string][0][:displacement] - 2
+            adjusted_lines = [
+              "\#if defined (__#{archs[0]}__)\n",
+              basis_lines[diff_start..diff_end],
+              *(diffpoints[index_string].map { |dp|
+                  [ "\#elif defined (__#{dp[:arch]}__)\n", *(dp[:hunk_lines]) ]
+                }),
+              "\#endif\n"
+            ]
+            basis_lines[diff_start..diff_end] = adjusted_lines
+          end # each key |index_string|
+          File.new("#{include_dir}/#{spb}", 'w').syswrite(basis_lines.join(''))
+        end # if not a directory
+      end # each |basis_file|
+    end # c_headers
+
+    # install_prefix expects a Pathname object, not just a string
+    def mach_o(install_prefix, stash_root, archs, sub_path = '')
+      # don’t suffer a double slash when sub_path is null:
+      s_p = (sub_path == '' ? '' : sub_path + '/')
+      # generate a full list of files, even if some are not present on all architectures; bear in
+      # mind that the current _directory_ may not even exist on all archs
+      basename_list = []
+      arch_dirs = archs.map {|a| "#{a}-bin"}
+      arch_dir_list = arch_dirs.join(',')
+      Dir["#{stash_root}/{#{arch_dir_list}}/#{s_p}*"].map { |f|
+        File.basename(f)
+      }.each { |b|
+        basename_list << b unless basename_list.count(b) > 0
+      }
+      basename_list.each do |b|
+        spb = s_p + b
+        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{stash_root}/#{ad}/#{spb}") }
+        pn = Pathname("#{stash_root}/#{the_arch_dir}/#{spb}")
+        if pn.directory?
+          mach_o(install_prefix, stash_root, archs, spb)
+        else
+          arch_files = Dir["#{stash_root}/{#{arch_dir_list}}/#{spb}"]
+          if arch_files.length > 1
+            system 'lipo', '-create', *arch_files, '-output', install_prefix/spb
+          else
+            # presumably there's a reason this only exists for one architecture, so no error;
+            # the same rationale would apply if it only existed in, say, two out of three
+            cp arch_files.first, install_prefix/spb
+          end # if > 1 file?
+        end # if directory?
+      end # each basename |b|
+    end # mach_o
+  end # << self
+end # Merge
+
 __END__
---- a/ext/fiddle/libffi-3.2.1/src/x86/win32.S	2017-04-04 11:14:27.000000000 +0200
-+++ b/ext/fiddle/libffi-3.2.1/src/x86/win32.S	2017-04-04 11:16:20.000000000 +0200
-@@ -528,7 +528,7 @@
-         .text
-  
-         # This assumes we are using gas.
--        .balign 16
-+        .p2align 4
- FFI_HIDDEN(ffi_call_win32)
-         .globl	USCORE_SYMBOL(ffi_call_win32)
- #if defined(X86_WIN32) && !defined(__OS2__)
-@@ -711,7 +711,7 @@
-         popl %ebp
-         ret
- .ffi_call_win32_end:
--        .balign 16
-+        .p2align 4
- FFI_HIDDEN(ffi_closure_THISCALL)
-         .globl	USCORE_SYMBOL(ffi_closure_THISCALL)
- #if defined(X86_WIN32) && !defined(__OS2__)
-@@ -724,7 +724,7 @@
-         push	%ecx
-         jmp	.ffi_closure_STDCALL_internal
+--- old/io_buffer.c	2024-06-30 18:15:25.000000000 -0700
++++ new/io_buffer.c	2024-06-30 18:16:54.000000000 -0700
+@@ -28,10 +28,12 @@
+ size_t RUBY_IO_BUFFER_PAGE_SIZE;
+ size_t RUBY_IO_BUFFER_DEFAULT_SIZE;
  
--        .balign 16
-+        .p2align 4
- FFI_HIDDEN(ffi_closure_FASTCALL)
-         .globl	USCORE_SYMBOL(ffi_closure_FASTCALL)
- #if defined(X86_WIN32) && !defined(__OS2__)
-@@ -753,7 +753,7 @@
+-#ifdef _WIN32
+-#else
++#ifndef _WIN32
+ #include <unistd.h>
+ #include <sys/mman.h>
++#ifndef MAP_ANONYMOUS
++#define MAP_ANONYMOUS MAP_ANON
++#endif
+ #endif
  
- .LFE1:
-         # This assumes we are using gas.
--        .balign 16
-+        .p2align 4
- FFI_HIDDEN(ffi_closure_SYSV)
- #if defined(X86_WIN32)
-         .globl	USCORE_SYMBOL(ffi_closure_SYSV)
-@@ -897,7 +897,7 @@
- #define RAW_CLOSURE_USER_DATA_OFFSET (RAW_CLOSURE_FUN_OFFSET + 4)
+ enum {
+--- old/shape.c	2024-06-30 19:16:02.000000000 -0700
++++ new/shape.c	2024-06-30 19:20:28.000000000 -0700
+@@ -14,6 +14,9 @@
  
- #ifdef X86_WIN32
--        .balign 16
-+        .p2align 4
- FFI_HIDDEN(ffi_closure_raw_THISCALL)
-         .globl	USCORE_SYMBOL(ffi_closure_raw_THISCALL)
- #if defined(X86_WIN32) && !defined(__OS2__)
-@@ -916,7 +916,7 @@
- #endif /* X86_WIN32 */
+ #ifndef _WIN32
+ #include <sys/mman.h>
++#ifndef MAP_ANONYMOUS
++#define MAP_ANONYMOUS MAP_ANON
++#endif
+ #endif
  
-         # This assumes we are using gas.
--        .balign 16
-+        .p2align 4
- #if defined(X86_WIN32)
-         .globl	USCORE_SYMBOL(ffi_closure_raw_SYSV)
- #if defined(X86_WIN32) && !defined(__OS2__)
-@@ -1039,7 +1039,7 @@
- #endif /* !FFI_NO_RAW_API */
+ #ifndef SHAPE_DEBUG
+--- old/thread_pthread_mn.c	2024-06-30 19:16:38.000000000 -0700
++++ new/thread_pthread_mn.c	2024-06-30 19:21:48.000000000 -0700
+@@ -166,6 +166,9 @@
+ static rb_nativethread_lock_t nt_machine_stack_lock = RB_NATIVETHREAD_LOCK_INIT;
  
-         # This assumes we are using gas.
--        .balign	16
-+        .p2align 4
- FFI_HIDDEN(ffi_closure_STDCALL)
-         .globl	USCORE_SYMBOL(ffi_closure_STDCALL)
- #if defined(X86_WIN32) && !defined(__OS2__)
-@@ -1184,7 +1184,6 @@
- 
- #if defined(X86_WIN32) && !defined(__OS2__)
-         .section	.eh_frame,"w"
--#endif
- .Lframe1:
- .LSCIE1:
-         .long	.LECIE1-.LASCIE1  /* Length of Common Information Entry */
-@@ -1343,6 +1342,7 @@
-         /* End of DW_CFA_xxx CFI instructions.  */
-         .align 4
- .LEFDE5:
+ #include <sys/mman.h>
++#ifndef MAP_ANONYMOUS
++#define MAP_ANONYMOUS MAP_ANON
 +#endif
  
- #endif /* !_MSC_VER */
- 
+ // vm_stack_size + machine_stack_size + 1 * (guard page size)
+ static inline size_t
