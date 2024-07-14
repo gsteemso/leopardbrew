265660c420ac626fe3af91babdee82072a1e5916
Gordon Steemson <gsteemso@gmail.com>
2024-07-14 10:30:02 -0700

    berkeley-db4:  updated scour‐&‐merge, & to explicit‐list variant

--- Library/Formula/berkeley-db4.rb
+++ Library/Formula/berkeley-db4.rb
@@ -24,17 +24,37 @@ class BerkeleyDb4 < Formula
       ENV.permit_arch_flags if superenv?
       ENV.un_m64 if Hardware::CPU.family == :g5_64
       archs = Hardware::CPU.universal_archs
-      dirs = []
+      stashdir = 'arch-stashes'
+      the_binaries = %w[
+        bin/db_archive
+        bin/db_checkpoint
+        bin/db_deadlock
+        bin/db_dump
+        bin/db_hotbackup
+        bin/db_load
+        bin/db_printlog
+        bin/db_recover
+        bin/db_sql
+        bin/db_stat
+        bin/db_upgrade
+        bin/db_verify
+        lib/libdb.a
+        lib/libdb-4.8.a
+        lib/libdb-4.8.dylib
+        lib/libdb_cxx.a
+        lib/libdb_cxx-4.8.a
+        lib/libdb_cxx-4.8.dylib
+      ]
     else
       archs = [MacOS.preferred_arch]
     end
 
     archs.each do |arch|
       if build.universal?
-        ENV.append_to_cflags "-arch #{arch}"
-        dir = "stash-#{arch}"
-        mkdir dir
-        dirs << dir
+        case arch
+          when :i386, :ppc then ENV.m32
+          when :x86_64, :ppc64 then ENV.m64
+        end
       end
 
       # “debug” is already disabled
@@ -50,14 +70,17 @@ class BerkeleyDb4 < Formula
         system "make", "install"
         if build.universal?
           system 'make', 'clean'
-          Merge.scour_keg(prefix, "../#{dir}")
-          # undo architecture-specific tweak before next run
-          ENV.remove_from_cflags "-arch #{arch}"
+          Merge.prep(prefix, buildpath/"arch-stashes/bin-#{arch}", the_binaries)
+          # undo architecture-specific tweaks before next run
+          case arch
+            when :i386, :ppc then ENV.un_m32
+            when :ppc64, :x86_64 then ENV.un_m64
+          end # case arch
         end # universal?
       end # cd build_unix
     end # archs.each
 
-    Merge.mach_o(prefix, dirs) if build.universal?
+    Merge.mach_o(prefix, stashdir, archs) if build.universal?
   end # install
 
   test do
@@ -66,74 +89,51 @@ class BerkeleyDb4 < Formula
 end
 
 class Merge
-  module Pathname_extension
-    def is_bare_mach_o?
-      # header word 0, magic signature:
-      #   MH_MAGIC    = 'feedface' – value with lowest‐order bit clear
-      #   MH_MAGIC_64 = 'feedfacf' – same value with lowest‐order bit set
-      # low‐order 24 bits of header word 1, CPU type:  7 is x86, 12 is ARM, 18 is PPC
-      # header word 3, file type:  no types higher than 10 are defined
-      # header word 5, net size of load commands, is far smaller than the filesize
-      if (self.file? and self.size >= 28 and mach_header = self.binread(24).unpack('N6'))
-        raise('Fat binary found where bare Mach-O file expected') if mach_header[0] == 0xcafebabe
-        ((mach_header[0] & 0xfffffffe) == 0xfeedface and
-          [7, 12, 18].detect { |item| (mach_header[1] & 0x00ffffff) == item } and
-          mach_header[3] < 11 and
-          mach_header[5] < self.size)
-      else
-        false
-      end
-    end unless method_defined?(:is_bare_mach_o?)
-  end # Pathname_extension
-
   class << self
     include FileUtils
 
-    def scour_keg(keg_prefix, stash, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      Dir["#{keg_prefix}/#{s_p}*"].each do |f|
-        pn = Pathname(f).extend(Pathname_extension)
-        spb = s_p + pn.basename
-        if pn.directory?
-          Dir.mkdir "#{stash}/#{spb}"
-          scour_keg(keg_prefix, stash, spb)
-        # the number of things that look like Mach-O files but aren’t is horrifying, so test
-        elsif ((not pn.symlink?) and pn.is_bare_mach_o?)
-          cp pn, "#{stash}/#{spb}"
-        end
-      end
-    end # scour_keg
+    # The keg_prefix and stash_root are expected to be Pathname objects.
+    # The list members are just strings.
+    def prep(keg_prefix, stash_root, list)
+      list.each do |item|
+        source = keg_prefix/item
+        dest = stash_root/item
+        mkpath dest.parent
+        cp source, dest
+      end # each binary
+    end # prep
 
-    def mach_o(install_prefix, arch_dirs, sub_path)
+    # The keg_prefix is expected to be a Pathname object.  The rest are just strings.
+    def mach_o(keg_prefix, stash_root, archs, sub_path = '')
       # don’t suffer a double slash when sub_path is null:
       s_p = (sub_path == '' ? '' : sub_path + '/')
       # generate a full list of files, even if some are not present on all architectures; bear in
       # mind that the current _directory_ may not even exist on all archs
       basename_list = []
+      arch_dirs = archs.map {|a| "bin-#{a}"}
       arch_dir_list = arch_dirs.join(',')
-      Dir["{#{arch_dir_list}}/#{s_p}*"].map { |f|
+      Dir["#{stash_root}/{#{arch_dir_list}}/#{s_p}*"].map { |f|
         File.basename(f)
-      }.each do |b|
+      }.each { |b|
         basename_list << b unless basename_list.count(b) > 0
-      end
+      }
       basename_list.each do |b|
         spb = s_p + b
-        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{ad}/#{spb}") }
-        pn = Pathname("#{the_arch_dir}/#{spb}")
+        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{stash_root}/#{ad}/#{spb}") }
+        pn = Pathname("#{stash_root}/#{the_arch_dir}/#{spb}")
         if pn.directory?
-          mach_o(install_prefix, arch_dirs, spb)
+          mach_o(keg_prefix, stash_root, archs, spb)
         else
-          arch_files = Dir["{#{arch_dir_list}}/#{spb}"]
+          arch_files = Dir["#{stash_root}/{#{arch_dir_list}}/#{spb}"]
           if arch_files.length > 1
-            system 'lipo', '-create', *arch_files, '-output', install_prefix/spb
+            system 'lipo', '-create', *arch_files, '-output', keg_prefix/spb
           else
             # presumably there's a reason this only exists for one architecture, so no error;
             # the same rationale would apply if it only existed in, say, two out of three
-            cp arch_files.first, install_prefix/spb
+            cp arch_files.first, keg_prefix/spb
           end # if > 1 file?
         end # if directory?
-      end # basename_list.each
+      end # each basename |b|
     end # mach_o
   end # << self
 end # Merge
