1ecff95ffae5658d19b7384138b2649da63c8d00
Gordon Steemson <gsteemso@gmail.com>
2024-07-14 10:04:53 -0700

    global.rb:  consolidated more imported env vars; tweaked spacing/comments

--- Library/Homebrew/global.rb
+++ Library/Homebrew/global.rb
@@ -14,38 +14,43 @@ require 'os'
 require 'utils'
 
 if ENV['HOMEBREW_BREW_FILE']
-  # Path to `bin/brew` main executable in {HOMEBREW_PREFIX}
+  # Path to main executable ($HOMEBREW_PREFIX/bin/brew):
   HOMEBREW_BREW_FILE = Pathname.new(ENV['HOMEBREW_BREW_FILE'])
 else
   odie '$HOMEBREW_BREW_FILE was not exported! Please call bin/brew directly!'
 end
 
 # Predefined pathnames:
-HOMEBREW_CACHE           = Pathname.new(ENV['HOMEBREW_CACHE'])
-                           # Where downloads (bottles, source tarballs, etc.) are cached
-  HOMEBREW_CACHE_FORMULA =   HOMEBREW_CACHE/'Formula'
-                             # Where brews installed via URL are cached
-HOMEBREW_CELLAR          = Pathname.new(ENV['HOMEBREW_CELLAR'])
-HOMEBREW_LIBRARY         = Pathname.new(ENV['HOMEBREW_LIBRARY'])
-  HOMEBREW_CONTRIB       =   HOMEBREW_LIBRARY/'Contributions'
-  HOMEBREW_LIBRARY_PATH  =   HOMEBREW_LIBRARY/'Homebrew'            # Homebrew’s Ruby libraries
-  HOMEBREW_LOAD_PATH     =   HOMEBREW_LIBRARY_PATH
-                             # The path to our libraries /when invoking Ruby/.  Is sometimes set to
-                             # a custom value during unit testing of Homebrew itself.
-HOMEBREW_PREFIX          = Pathname.new(ENV['HOMEBREW_PREFIX'])     # Where we link under
-HOMEBREW_REPOSITORY      = Pathname.new(ENV['HOMEBREW_REPOSITORY']) # Where .git is found
-HOMEBREW_VERSION         = Pathname.new(ENV['HOMEBREW_VERSION'])    # Permanently fixed at 0.9.5
+HOMEBREW_CACHE         = Pathname.new(ENV['HOMEBREW_CACHE'])
+                         # Where downloads (bottles, source tarballs, etc.) are cached
+  HOMEBREW_CACHE_FORMULA = HOMEBREW_CACHE/'Formula'
+                           # Where brews installed via URL are cached
+HOMEBREW_CELLAR        = Pathname.new(ENV['HOMEBREW_CELLAR'])
+HOMEBREW_CURL          = Pathname.new(ENV['HOMEBREW_CURL'])
+HOMEBREW_LIBRARY       = Pathname.new(ENV['HOMEBREW_LIBRARY'])
+  HOMEBREW_CONTRIB     =   HOMEBREW_LIBRARY/'Contributions'
+  HOMEBREW_LIBRARY_PATH =  HOMEBREW_LIBRARY/'Homebrew'            # Homebrew’s Ruby libraries
+  HOMEBREW_LOAD_PATH   =   HOMEBREW_LIBRARY_PATH
+                           # The path to our libraries /when invoking Ruby/.  Is sometimes set to a
+                           # custom value during unit testing of Homebrew itself.
+HOMEBREW_PREFIX        = Pathname.new(ENV['HOMEBREW_PREFIX'])     # Where we link under
+HOMEBREW_REPOSITORY    = Pathname.new(ENV['HOMEBREW_REPOSITORY']) # Where .git is found
+HOMEBREW_RUBY_PATH     = Pathname.new(ENV['HOMEBREW_RUBY_PATH'])  # Path → our internal Ruby binary
+HOMEBREW_VERSION       = Pathname.new(ENV['HOMEBREW_VERSION'])    # Permanently fixed at 0.9.5
 
 # Predefined miscellaneous values:
+HOMEBREW_SYSTEM          = ENV['HOMEBREW_SYSTEM']
 HOMEBREW_USER_AGENT_CURL = ENV['HOMEBREW_USER_AGENT_CURL']
+OS_VERSION               = ENV['HOMEBREW_OS_VERSION']
 
 # Optional user‐defined values:
 HOMEBREW_GITHUB_API_TOKEN = ENV['HOMEBREW_GITHUB_API_TOKEN']
                             # For unthrottled access to Github repositories
-HOMEBREW_LOGS = Pathname.new(ENV.fetch 'HOMEBREW_LOGS', '~/Library/Logs/Homebrew/').expand_path
-                # Where build, postinstall, and test logs of formulæ are written to
-HOMEBREW_TEMP = Pathname.new(ENV.fetch 'HOMEBREW_TEMP', '/tmp')
-                # Where temporary folders for building and testing formulæ are created
+HOMEBREW_LOGS          = Pathname.new(ENV.fetch 'HOMEBREW_LOGS', '~/Library/Logs/Homebrew/').expand_path
+                         # Where build, postinstall, and test logs of formulæ are written to
+HOMEBREW_TEMP          = Pathname.new(ENV.fetch 'HOMEBREW_TEMP', '/tmp')
+                         # Where temporary folders for building and testing formulæ are created
+NO_COMPAT              = ENV['HOMEBREW_NO_COMPAT']
 
 ARGV.extend(HomebrewArgvExtension)
 
@@ -60,15 +65,14 @@ else
     RbConfig::CONFIG['ruby_install_name'] + RbConfig::CONFIG['EXEEXT']
   )
 end
-RUBY_BIN = RUBY_PATH.dirname  # the directory our Ruby interpreter lives in
+RUBY_BIN = RUBY_PATH.dirname  # the directory the system Ruby interpreter lives in
 
 if RUBY_PLATFORM =~ /darwin/  # TODO:  need to disambiguate Mac OS from bare Darwin
-  MACOS_FULL_VERSION = `/usr/bin/sw_vers -productVersion`.chomp
+  MACOS_FULL_VERSION = ENV['HOMEBREW_OSX_VERSION'].chomp
   MACOS_VERSION = MACOS_FULL_VERSION[/\d\d\.\d+/]
-  OS_VERSION = "Mac OS #{MACOS_FULL_VERSION}"
+  MACOS_VERSION = MACOS_VERSION.slice(0, 2) if MACOS_VERSION.to_f >= 11
 else
   MACOS_FULL_VERSION = MACOS_VERSION = '0'
-  OS_VERSION = RUBY_PLATFORM
 end
 
 ruby_version = "#{RUBY_VERSION}#{"-p#{RUBY_PATCHLEVEL}" if defined? RUBY_PATCHLEVEL}"
@@ -88,7 +92,7 @@ end
 
 HOMEBREW_PULL_OR_COMMIT_URL_REGEX = %r[https://github\.com/([\w-]+)/tigerbrew(-[\w-]+)?/(?:pull/(\d+)|commit/[0-9a-fA-F]{4,40})]
 
-require 'compat' unless ARGV.include?('--no-compat') || ENV['HOMEBREW_NO_COMPAT']
+require 'compat' unless ARGV.include?('--no-compat') || NO_COMPAT
 
 ORIGINAL_PATHS = ENV['PATH'].split(File::PATH_SEPARATOR).map { |p| Pathname.new(p).expand_path rescue nil }.compact.freeze
 
