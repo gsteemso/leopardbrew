2004f7f3863ebd7e8b6de71562fe3f910e51d3fd
Gordon Steemson <gsteemso@gmail.com>
2024-07-17 20:42:45 -0700

    gdbm:  bumped version 1.23 → 1.24; inverted an option; added a bunch of things
    - upgraded to the latest version.
    
    - inverted the option for building the libgdbm compatibility shim, to be “on”
      by default, and made the formula keg-only if it is (due to a header it
      installs that shadows a system header).
    
    - added a dependency on the coreutils formula, as the package has mildly
      reduced functionality in the absence of the `base64` utility, and told
      `configure` how to find it (as “gbase64”).
    
    - patched an absurd omission in the makefiles that prevented one of the unit
      tests from building correctly, and enabled the unit tests now that they’re
      actually working.

--- Library/Formula/gdbm.rb
+++ Library/Formula/gdbm.rb
@@ -1,33 +1,37 @@
 class Gdbm < Formula
-  desc "GNU database manager"
-  homepage "https://www.gnu.org/software/gdbm/"
-  # audit --strict complained about these URLs
-  url "http://ftpmirror.gnu.org/gdbm/gdbm-1.23.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gdbm/gdbm-1.23.tar.gz"
-  sha256 "74b1081d21fff13ae4bd7c16e5d6e504a4c26f7cde1dca0d963a484174bbcacd"
-
-  bottle do
-    cellar :any
-    sha256 "adfa78e136cc5c3cddf6b4d2b953a072eda010d22677d869ec7996d70cbb504f" => :tiger_altivec
-  end
+  desc 'GNU database manager'
+  homepage 'https://www.gnu.org/software/gdbm/'
+  url 'http://ftpmirror.gnu.org/gdbm/gdbm-1.24.tar.gz'
+  mirror 'https://ftp.gnu.org/gnu/gdbm/gdbm-1.24.tar.gz'
+  sha256 '695e9827fdf763513f133910bc7e6cfdb9187943a4fec943e57449723d2b8dbf'
 
   option :universal
-  option "with-libgdbm-compat", "Build libgdbm_compat, a compatibility layer which provides UNIX-like dbm and ndbm interfaces."
+  option 'without-libgdbm-compat', 'Omit the libgdbm_compat compatibility layer, providing UNIX-like dbm/ndbm interfaces'
 
-  depends_on "readline"
+  depends_on 'coreutils'
+  depends_on 'readline'
+
+  keg_only :provided_by_osx if build.with? 'libgdbm-compat'
+
+  # the “t_wordwrap” test has a ridiculous oversight where they omitted one of its dependency
+  # libraries from the Makefile, causing it to not build
+  patch :DATA
 
   def install
     ENV.universal_binary if build.universal?
 
-    args = %W[
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --prefix=#{prefix}
+    args = [
+      "--prefix=#{prefix}",
+      '--disable-dependency-tracking',
+      '--disable-silent-rules',
+      "BASE64_BIN=#{Formula['coreutils'].opt_bin/'gbase64'}"
     ]
-    args << "--enable-libgdbm-compat" if build.with? "libgdbm-compat"
+    args << '--enable-libgdbm-compat' if build.with? 'libgdbm-compat'
 
-    system "./configure", *args
-    system "make", "install"
+    system './configure', *args
+    system 'make'
+    system 'make', 'check'
+    system 'make', 'install'
   end
 
   test do
@@ -36,3 +40,25 @@ class Gdbm < Formula
     assert_match /2/, pipe_output("#{bin}/gdbmtool --norc test", "fetch 1\nquit\n")
   end
 end
+
+__END__
+--- old/tests/Makefile.in	2024-07-15 22:08:03.000000000 -0700
++++ new/tests/Makefile.in	2024-07-15 22:09:43.000000000 -0700
+@@ -214,7 +214,7 @@
+ t_dumpload_DEPENDENCIES = ../src/libgdbm.la
+ t_wordwrap_SOURCES = t_wordwrap.c
+ t_wordwrap_OBJECTS = t_wordwrap.$(OBJEXT)
+-t_wordwrap_DEPENDENCIES = ../tools/libgdbmapp.a
++t_wordwrap_DEPENDENCIES = ../src/libgdbm.la ../tools/libgdbmapp.a
+ AM_V_P = $(am__v_P_@AM_V@)
+ am__v_P_ = $(am__v_P_@AM_DEFAULT_V@)
+ am__v_P_0 = false
+@@ -550,7 +550,7 @@
+ dtfetch_LDADD = ../src/libgdbm.la ../compat/libgdbm_compat.la
+ dtdel_LDADD = ../src/libgdbm.la ../compat/libgdbm_compat.la
+ d_creat_ce_LDADD = ../src/libgdbm.la ../compat/libgdbm_compat.la
+-t_wordwrap_LDADD = ../tools/libgdbmapp.a
++t_wordwrap_LDADD = ../src/libgdbm.la ../tools/libgdbmapp.a
+ SUBDIRS = gdbmtool
+ all: all-recursive
+ 
