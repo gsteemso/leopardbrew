c269de6d62957d97bb569390ee2ac2af28b36db6
Gordon Steemson <gsteemso@gmail.com>
2024-07-19 09:05:28 -0700

    libarchive:  switched from scour+merge to list+merge

--- Library/Formula/libarchive.rb
+++ Library/Formula/libarchive.rb
@@ -4,7 +4,17 @@ class Libarchive < Formula
   url "https://www.libarchive.org/downloads/libarchive-3.7.4.tar.xz"
   sha256 "f887755c434a736a609cbd28d87ddbfbe9d6a3bb5b703c22c02f6af80a802735"
 
+  bottle do
+    cellar :any
+    sha256 "9c382bde083c41d3abab1be03642cf95a73151cc83514d57840a8fa9ae278f0c" => :tiger_altivec
+  end
+
   option :universal
+
+  depends_on "bzip2"
+  depends_on "lz4"
+  depends_on "xz"
+  depends_on "zlib"
 
   keg_only :provided_by_osx
 
@@ -13,22 +23,26 @@ class Libarchive < Formula
       ENV.permit_arch_flags if superenv?
       ENV.un_m64 if Hardware::CPU.family == :g5_64
       archs = Hardware::CPU.universal_archs
-      dirs = []
+      stashdir = buildpath/'arch-stashes'
+      the_binaries = %w[
+        bin/bsdcat
+        bin/bsdcpio
+        bin/bsdtar
+        bin/bsdunzip
+        lib/libarchive.13.dylib
+        lib/libarchive.a
+      ]
     else
       archs = [MacOS.preferred_arch]
-    end
+    end # universal?
 
     archs.each do |arch|
       if build.universal?
-        ENV.append_to_cflags "-arch #{arch}"
-#       case arch
-#         when :i386, :ppc then ENV.m32
-#         when :x86_64, :ppc64 then ENV.m64
-#       end
-        dir = "stash-#{arch}"
-        mkdir dir
-        dirs << dir
-      end
+        case arch
+          when :i386, :ppc then ENV.m32
+          when :ppc64, :x86_64 then ENV.m64
+        end
+      end # universal?
 
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
@@ -37,29 +51,29 @@ class Libarchive < Formula
                           "--without-lzo2",
                           "--without-nettle",
                           "--without-xml2",
-                          "--without-expat"
+                          "--without-expat",
+                          "ac_cv_header_sys_queue_h=no" # Use the up to date copy provided to obtain STAILQ_FOREACH
       system 'make'
       system "make", "install"
 
       if build.universal?
         system 'make', 'clean'
-        Merge.scour_keg(prefix, dir)
-        # undo architecture-specific tweak(s) before next run
-        ENV.remove_from_cflags "-arch #{arch}"
-#       case arch
-#         when :i386, :ppc then ENV.un_m32
-#         when :x86_64, :ppc64 then ENV.un_m64
-#       end # case arch
+        Merge.prep(prefix, stashdir/"bin-#{arch}", the_binaries)
+        # undo architecture-specific tweaks before next run
+        case arch
+          when :i386, :ppc then ENV.un_m32
+          when :ppc64, :x86_64 then ENV.un_m64
+        end # case arch
       end # universal?
     end # archs.each
-    Merge.mach_o(prefix, dirs) if build.universal?
+    Merge.mach_o(prefix, stashdir, archs) if build.universal?
   end # install
 
   test do
     (testpath/"test").write("test")
     system bin/"bsdtar", "-czvf", "test.tar.gz", "test"
     assert_match /test/, shell_output("#{bin}/bsdtar -xOzf test.tar.gz")
-  end
+  end # test
 end # Libarchive
 
 class Merge
@@ -86,51 +100,62 @@ class Merge
   class << self
     include FileUtils
 
-    def scour_keg(keg_prefix, stash, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      Dir["#{keg_prefix}/#{s_p}*"].each do |f|
-        pn = Pathname(f).extend(Pathname_extension)
-        spb = s_p + pn.basename
-        if pn.directory?
-          Dir.mkdir "#{stash}/#{spb}"
-          scour_keg(keg_prefix, stash, spb)
-        # the number of things that look like Mach-O files but aren’t is horrifying, so test
-        elsif ((not pn.symlink?) and pn.is_bare_mach_o?)
-          cp pn, "#{stash}/#{spb}"
+    # The destination is expected to be a Pathname object.
+    # The source is just a string.
+    def cp_mkp(source, destination)
+      if destination.exists?
+        if destination.is_directory?
+          cp source, destination
+        else
+          raise "File exists at destination:  #{destination}"
         end
-      end
-    end # scour_keg
+      else
+        mkdir_p destination.parent unless destination.parent.exists?
+        cp source, destination
+      end # destination exists?
+    end # cp_mkp
 
-    def mach_o(install_prefix, arch_dirs, sub_path = '')
+    # The keg_prefix and stash_root are expected to be Pathname objects.
+    # The list members are just strings.
+    def prep(keg_prefix, stash_root, list)
+      list.each do |item|
+        source = keg_prefix/item
+        dest = stash_root/item
+        cp_mkp source, dest
+      end # each binary
+    end # prep
+
+    # The keg_prefix is expected to be a Pathname object.  The rest are just strings.
+    def mach_o(keg_prefix, stash_root, archs, sub_path = '')
       # don’t suffer a double slash when sub_path is null:
       s_p = (sub_path == '' ? '' : sub_path + '/')
       # generate a full list of files, even if some are not present on all architectures; bear in
       # mind that the current _directory_ may not even exist on all archs
       basename_list = []
+      arch_dirs = archs.map {|a| "bin-#{a}"}
       arch_dir_list = arch_dirs.join(',')
-      Dir["{#{arch_dir_list}}/#{s_p}*"].map { |f|
+      Dir["#{stash_root}/{#{arch_dir_list}}/#{s_p}*"].map { |f|
         File.basename(f)
-      }.each do |b|
+      }.each { |b|
         basename_list << b unless basename_list.count(b) > 0
-      end
+      }
       basename_list.each do |b|
         spb = s_p + b
-        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{ad}/#{spb}") }
-        pn = Pathname("#{the_arch_dir}/#{spb}")
+        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{stash_root}/#{ad}/#{spb}") }
+        pn = Pathname("#{stash_root}/#{the_arch_dir}/#{spb}")
         if pn.directory?
-          mach_o(install_prefix, arch_dirs, spb)
+          mach_o(keg_prefix, stash_root, archs, spb)
         else
-          arch_files = Dir["{#{arch_dir_list}}/#{spb}"]
+          arch_files = Dir["#{stash_root}/{#{arch_dir_list}}/#{spb}"]
           if arch_files.length > 1
-            system 'lipo', '-create', *arch_files, '-output', install_prefix/spb
+            system 'lipo', '-create', *arch_files, '-output', keg_prefix/spb
           else
             # presumably there's a reason this only exists for one architecture, so no error;
             # the same rationale would apply if it only existed in, say, two out of three
-            cp arch_files.first, install_prefix/spb
+            cp arch_files.first, keg_prefix/spb
           end # if > 1 file?
         end # if directory?
-      end # basename_list.each
+      end # each basename |b|
     end # mach_o
   end # << self
 end # Merge
