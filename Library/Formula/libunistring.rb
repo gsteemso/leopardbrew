87fd6f907bf8ea081aa356fa9f89de48d982a388
Gordon Steemson <gsteemso@gmail.com>
2024-07-19 09:08:32 -0700

    libunistring:  add bottle (upstream’s caught up to version)

--- Library/Formula/libunistring.rb
+++ Library/Formula/libunistring.rb
@@ -1,9 +1,14 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "http://ftpmirror.gnu.org/libunistring/libunistring-1.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz"
-  sha256 "fd6d5662fa706487c48349a758b57bc149ce94ec6c30624ec9fdc473ceabbc8e"
+  url "http://ftpmirror.gnu.org/libunistring/libunistring-1.2.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.xz"
+  sha256 "632bd65ed74a881ca8a0309a1001c428bd1cbd5cd7ddbf8cedcd2e65f4dcdc44"
+
+  bottle do
+    cellar :any
+    sha256 "8074558a34abdb2121379dd2df80fc1572d62983f39e750b37e69f20cc8fa35c" => :tiger_altivec
+  end
 
   option :universal
 
