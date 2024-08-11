f352dd9fd75b0223db85038ceb68d98efcead82d
Gordon Steemson <gsteemso@gmail.com>
2024-08-11 16:19:02 -0700

    flac:  v1.3.1 → v1.4.3; " → '

--- Library/Formula/flac.rb
+++ Library/Formula/flac.rb
@@ -1,74 +1,65 @@
 class Flac < Formula
-  desc "Free lossless audio codec"
-  homepage "https://xiph.org/flac/"
-  url "http://downloads.xiph.org/releases/flac/flac-1.3.1.tar.xz"
-  sha256 "4773c0099dba767d963fd92143263be338c48702172e8754b9bc5103efe1c56c"
+  desc 'Free lossless audio codec'
+  homepage 'https://xiph.org/flac/'
+  url 'https://downloads.xiph.org/releases/flac/flac-1.4.3.tar.xz'
+  sha256 '6c58e69cd22348f441b861092b825e591d0b822e106de6eb0ee4d05d27205b70'
 
   head do
-    url "https://git.xiph.org/flac.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
-  bottle do
-    cellar :any
-    sha256 "993da436abd03400977ec266a2747a955595a6418462b8fb328661ff83b41f9e" => :leopard_g3
-    sha256 "eebc1cd5a2204c9d223c476e5148ada3663ba58ffe5caa6faeaa24eb2d6a5cc8" => :leopard_altivec
+    url 'https://git.xiph.org/flac.git'
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool'  => :build
   end
 
   option :universal
 
-  depends_on "pkg-config" => :build
-  depends_on "libogg" => :optional
+  depends_on 'pkg-config' => :build
+  depends_on 'libogg'     => :recommended
 
   fails_with :llvm do
     build 2326
-    cause "Undefined symbols when linking"
+    cause 'Undefined symbols when linking'
   end
 
   fails_with :clang do
     build 500
-    cause "Undefined symbols ___cpuid and ___cpuid_count"
+    cause 'Undefined symbols ___cpuid and ___cpuid_count'
   end
 
   def install
     ENV.universal_binary if build.universal?
-
-    ENV.append "CFLAGS", "-std=gnu89"
+    archs = (build.universal? ? Hardware::CPU.universal_archs : [MacOS.preferred_arch])
+    archs.extend ArchitectureListExtension
 
     args = %W[
-      --disable-dependency-tracking
-      --disable-debug
       --prefix=#{prefix}
-      --mandir=#{man}
-      --enable-sse
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
       --enable-static
     ]
+    args << '--disable-asm-optimizations' if build.universal? || Hardware.is_32_bit?
+    args << '--disable-64-bit-words' if Hardware.is_32_bit?
+    args << '--without-ogg' if build.without? 'libogg'
 
-    args << "--disable-asm-optimizations" if build.universal? || Hardware.is_32_bit?
-    args << "--without-ogg" if build.without? "libogg"
-
-    system "./autogen.sh" if build.head?
-    system "./configure", *args
-
-    ENV["OBJ_FORMAT"] = "macho"
+    system './autogen.sh' if build.head?
+    system './configure', *args
 
     # adds universal flags to the generated libtool script
-    inreplace "libtool" do |s|
-      s.gsub! ":$verstring\"", ":$verstring -arch #{Hardware::CPU.arch_32_bit} -arch #{Hardware::CPU.arch_64_bit}\""
+    inreplace 'libtool' do |s|
+      s.gsub! '\\$verstring ', "\\$verstring #{archs.as_arch_flags} "
     end
 
-    system "make", "install"
+    system 'make', 'install'
   end
 
   test do
     raw_data = "pseudo audio data that stays the same \x00\xff\xda"
-    (testpath/"in.raw").write raw_data
+    (testpath/'in.raw').write raw_data
     # encode and decode
-    system "#{bin}/flac", "--endian=little", "--sign=signed", "--channels=1", "--bps=8", "--sample-rate=8000", "--output-name=in.flac", "in.raw"
-    system "#{bin}/flac", "--decode", "--force-raw", "--endian=little", "--sign=signed", "--output-name=out.raw", "in.flac"
+    system bin/'flac', '--endian=little', '--sign=signed', '--channels=1', '--bps=8', '--sample-rate=8000', '--output-name=in.flac', 'in.raw'
+    system bin/'flac', '--decode', '--force-raw', '--endian=little', '--sign=signed', '--output-name=out.raw', 'in.flac'
     # diff input and output
-    system "diff", "in.raw", "out.raw"
+    assert_equal `diff -q in.raw out.raw`, ''
   end
 end
