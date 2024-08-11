d35ac8d6d2cac2a2605ff2a4ae27fb11e436dbe3
Gordon Steemson <gsteemso@gmail.com>
2024-08-11 15:25:02 -0700

    scons:  v3.1.2→v4.8.0; + --HEAD for pip3 install

--- Library/Formula/scons.rb
+++ Library/Formula/scons.rb
@@ -1,34 +1,36 @@
 class Scons < Formula
-  desc "Substitute for classic 'make' tool with autoconf/automake functionality"
-  homepage "http://www.scons.org"
-  url "https://downloads.sourceforge.net/project/scons/scons/3.1.2/scons-3.1.2.tar.gz"
-  sha256 "7801f3f62f654528e272df780be10c0e9337e897650b62ddcee9f39fde13f8fb"
+  desc 'Substitute for classic “make” with autotools-like functionality'
+  homepage 'https://www.scons.org/'
+  url 'https://files.pythonhosted.org/packages/ec/5c/cc835a17633de8b260ec1a6e527b5c57f4975cee5949f49e57ad4d5fab4b/scons-4.8.0.tar.gz'
+  sha256 '2c7377ff6a22ca136c795ae3dc3d0824696e5478d1e4940f2af75659b0d45454'
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "d9f98df17802f66a5c0fdfadf2109a577b6717fa936a624baea7114a989baf9d" => :tiger_altivec
+  head do
+    url "file://#{HOMEBREW_LIBRARY_PATH}/test/tarballs/testball-0.1.tbz"  # dummy download, we don’t use it
+    sha256 '91e3f7930c98d7ccfb288e115ed52d06b0e5bc16fec7dce8bdda86530027067b'
   end
 
-  depends_on :python if MacOS.version < :snow_leopard
+  depends_on :python3
 
   def install
-    man1.install gzip("scons-time.1", "scons.1", "sconsign.1")
-    system "python", "setup.py", "install",
-             "--prefix=#{prefix}",
-             "--standalone-lib",
-             # SCons gets handsy with sys.path---`scons-local` is one place it
-             # will look when all is said and done.
-             "--install-lib=#{libexec}/scons-local",
-             "--install-scripts=#{bin}",
-             "--install-data=#{libexec}",
-             "--no-version-script", "--no-install-man"
-
-    # Re-root scripts to libexec so they can import SCons and symlink back into
-    # bin. Similar tactics are used in the duplicity formula.
-    bin.children.each do |p|
-      mv p, "#{libexec}/#{p.basename}.py"
-      bin.install_symlink "#{libexec}/#{p.basename}.py" => p.basename
+    if build.head?
+      system 'pip3', 'install', 'scons'
+    else
+      system 'pip3', 'install', '.'
     end
+    # pip3 installs the executable scripts inside the Python framework.  This does not facilitate
+    # using them directly.  If we symlink them into `bin`, it keeps our installation from looking
+    # empty and lets them be correctly `brew link`ed.
+    # The previous, non‐pip3’d version of this formula also put them in `libexec` for proper
+    # importation, but that’s redundant after pip3 did its work.
+    p3f = Formula['python3']
+    scons_bins = Dir.glob("#{p3f.opt_prefix}/Frameworks/Python.framework/Versions/#{p3f.version.to_s.slice(/^[^.]+\.[^.]+/)}/bin/scons*")
+    mkdir_p bin
+    ln_s scons_bins, bin
+  end # install
+
+  def uninsinuate
+    # this is needed for the installed Python package to actually go away upon removal
+    system 'pip3', 'uninstall', '--yes', 'scons'
   end
 
   test do
@@ -37,10 +39,11 @@ class Scons < Formula
       int main()
       {
         printf("Homebrew");
+        return(0);
       }
     EOS
-    (testpath/"SConstruct").write "Program('test.c')"
-    system bin/"scons"
-    assert_equal "Homebrew", shell_output("#{testpath}/test")
-  end
-end
+    (testpath/'SConstruct').write "Program('test.c')"
+    system bin/'scons'
+    assert_equal 'Homebrew', shell_output(testpath/'test')
+  end # test
+end # Scons
