82c957d6ea4a54ddb73d678132735b91952e43d6
Gordon Steemson <gsteemso@gmail.com>
2024-08-11 16:42:27 -0700

    LAME:  v3.99.5 → v3.100; added --HEAD; " → '

--- Library/Formula/lame.rb
+++ Library/Formula/lame.rb
@@ -1,28 +1,62 @@
 class Lame < Formula
-  desc "Lame Aint an MP3 Encoder (LAME)"
-  homepage "http://lame.sourceforge.net/"
-  url "https://downloads.sourceforge.net/sourceforge/lame/lame-3.99.5.tar.gz"
-  sha256 "24346b4158e4af3bd9f2e194bb23eb473c75fb7377011523353196b19b9a23ff"
+  desc 'Lame Ain’t an MP3 Encoder (LAME)'
+  homepage 'http://lame.sourceforge.net/'
+  url 'https://downloads.sourceforge.net/sourceforge/lame/lame-3.100.tar.gz'
+  sha256 'ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e'
 
-  bottle do
-    cellar :any
-    sha1 "ac89c99c2024cd109cd7aaa1d1edc9424715ac88" => :leopard_g3
-    sha1 "975485f851402e9f57ae26fe61115a9d295c02fb" => :leopard_altivec
-  end
+  head 'https://svn.code.sf.net/p/lame/svn/trunk/lame'
 
   option :universal
+
+  patch :DATA
+
+  unless build.head?
+    patch <<EoP
+--- old/include/libmp3lame.sym	2017-09-06 12:33:35.000000000 -0700
++++ new/include/libmp3lame.sym	2024-08-05 11:40:42.000000000 -0700
+@@ -1,5 +1,4 @@
+ lame_init
+-lame_init_old
+ lame_set_num_samples
+ lame_get_num_samples
+ lame_set_in_samplerate
+EoP
+  end
 
   def install
     ENV.universal_binary if build.universal?
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-debug",
-                          "--prefix=#{prefix}",
-                          "--enable-nasm"
-    system "make", "install"
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-debug',
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules',
+                          '--enable-nasm'
+    system 'make', 'install'
   end
 
   test do
-    system "#{bin}/lame", "--genre-list", test_fixtures("test.mp3")
+    system "#{bin}/lame", '--genre-list', test_fixtures('test.mp3')
   end
 end
+
+__END__
+--- old/frontend/parse.c	2017-10-10 12:08:39.000000000 -0700
++++ new/frontend/parse.c	2024-08-05 11:55:30.000000000 -0700
+@@ -2001,7 +2001,7 @@
+                     nogap_tags = 1;
+ 
+                 T_ELIF("nogapout")
+-                    int const arg_n = strnlen(nextArg, PATH_MAX);
++                    int const arg_n = strlen(nextArg);
+                     if (arg_n >= PATH_MAX) {
+                         error_printf("%s: %s argument length (%d) exceeds limit (%d)\n", ProgramName, token, arg_n, PATH_MAX);
+                         return -1;
+@@ -2011,7 +2011,7 @@
+                     argUsed = 1;
+ 
+                 T_ELIF("out-dir")
+-                    int const arg_n = strnlen(nextArg, PATH_MAX);
++                    int const arg_n = strlen(nextArg);
+                     if (arg_n >= PATH_MAX) {
+                         error_printf("%s: %s argument length (%d) exceeds limit (%d)\n", ProgramName, token, arg_n, PATH_MAX);
+                         return -1;
