60af4d40339bb26c24fbe05e3e95998abd97c196
Gordon Steemson <gsteemso@gmail.com>
2024-09-22 20:42:08 -0700

    xz:  apply upstream patch; " →'

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -1,38 +1,40 @@
 # Upstream project has requested we use a mirror as the main URL
 # https://github.com/Homebrew/homebrew/pull/21419
 class Xz < Formula
-  desc "General-purpose data compression with high compression ratio"
-  homepage "http://tukaani.org/xz/"
-  url "https://fossies.org/linux/misc/xz-5.6.2.tar.gz"
-  mirror "https://github.com/tukaani-project/xz/releases/download/v5.6.2/xz-5.6.2.tar.gz"
-  sha256 "8bfd20c0e1d86f0402f2497cfa71c6ab62d4cd35fd704276e3140bfb71414519"
-
-  bottle do
-    sha256 "adbf7a74ab4205a88c3950514c1c9deb793a109a24a0ce1a0e6b8180a54cb338" => :tiger_altivec
-  end
+  desc 'General-purpose data compression with high compression ratio'
+  homepage 'http://tukaani.org/xz/'
+  url 'https://fossies.org/linux/misc/xz-5.6.2.tar.bz2'
+  mirror 'https://github.com/tukaani-project/xz/releases/download/v5.6.2/xz-5.6.2.tar.bz2'
+  sha256 'e12aa03cbd200597bd4ce11d97be2d09a6e6d39a9311ce72c91ac7deacde3171'
+  version '5.6.2p1'
 
   option :universal
 
-  def install
-    ENV.universal_binary if build.universal?
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+  patch do
+    url 'https://github.com/tukaani-project/xz/releases/download/v5.6.2/xz-5213-547-562-libtool.patch'
+    sha256 '31f58851acdf0d24d15bce14782dafa5a447ee922eaa39859170277dc9a8fae7'
   end
 
+  def install
+    ENV.universal_binary if build.universal?
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-debug',
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules'
+    system 'make', 'install'
+  end # install
+
   test do
-    path = testpath/"data.txt"
-    original_contents = "." * 1000
+    path = testpath/'data.txt'
+    original_contents = '.' * 1000
     path.write original_contents
 
     # compress: data.txt -> data.txt.xz
-    system bin/"xz", path
+    system bin/'xz', path
     assert !path.exist?
 
     # decompress: data.txt.xz -> data.txt
-    system bin/"xz", "-d", "#{path}.xz"
+    system bin/'xz', '-d', "#{path}.xz"
     assert_equal original_contents, path.read
-  end
-end
+  end # test
+end # Xz
