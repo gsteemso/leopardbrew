36e2e4d43be9234e3dce33d2671a495f7fc5b913
Gordon Steemson <gsteemso@gmail.com>
2024-09-22 20:22:49 -0700

    libmetalink:  v0.1.2 → 0.1.3; universal & with-tests options; " → '

--- Library/Formula/libmetalink.rb
+++ Library/Formula/libmetalink.rb
@@ -1,22 +1,22 @@
 class Libmetalink < Formula
-  desc "C library to parse Metalink XML files"
-  homepage "https://launchpad.net/libmetalink/"
-  url "https://launchpad.net/libmetalink/trunk/packagingfix/+download/libmetalink-0.1.2.tar.bz2"
-  sha256 "cbed9121bf550ef14a434d6ed3d8806ded7a339db16b698cfa2f39fdc3d48bf6"
+  desc 'C library to parse Metalink XML files'
+  homepage 'https://launchpad.net/libmetalink/'
+  url 'https://launchpad.net/libmetalink/trunk/libmetalink-0.1.3/+download/libmetalink-0.1.3.tar.xz'
+  sha256 '86312620c5b64c694b91f9cc355eabbd358fa92195b3e99517504076bf9fe33a'
 
-  bottle do
-    cellar :any
-    revision 1
-    sha1 "0fd4cd6106168e23a2ce691bcc3db3afaef44001" => :yosemite
-    sha1 "3cbcf570dd17a7c617da687d1d90b20d03e9e299" => :mavericks
-    sha1 "ceeab999a4e6b19c7859ffc8e4d0319cd365cb48" => :mountain_lion
-  end
+  option :universal
+  option 'with-tests', 'Build and run unit tests during brewing (requires CUNIT)'
 
-  depends_on "pkg-config" => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'cunit' if build.with? 'tests'
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    ENV.universal_binary if build.universal?
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules'
+    system 'make'
+    system 'make', 'check' if build.with? 'tests'
+    system 'make', 'install'
   end
 end
