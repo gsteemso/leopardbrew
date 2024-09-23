3f6a4436a95631146c21fa44175413aacb782389
Gordon Steemson <gsteemso@gmail.com>
2024-09-22 20:38:45 -0700

    gnutls: libidn+gettext+pkgconfig gnutls||openssl3 ¬scour+merge "→'

--- Library/Formula/gsasl.rb
+++ Library/Formula/gsasl.rb
@@ -1,124 +1,39 @@
 class Gsasl < Formula
-  desc "Simple Authentication and Security Layer library command-line interface"
-  homepage "https://www.gnu.org/software/gsasl/"
+  desc 'Simple Authentication and Security Layer library command-line interface'
+  homepage 'https://www.gnu.org/software/gsasl/'
   url 'http://ftpmirror.gnu.org/gsasl/gsasl-2.2.1.tar.gz'
   mirror 'https://ftp.gnu.org/gsasl/gsasl-2.2.1.tar.gz'
   sha256 'd45b562e13bd13b9fc20b372f4b53269740cf6279f836f09ce11b9d32bcee075'
 
   option :universal
 
-  option "with-gnutls",  "Build with STARTTLS support"
-  option "with-libidn",  "Build with IDN StringPrep"
-  option "with-libntlm", "Interoperate with some versions of Windows"
+  option 'with-gnutls',  'Build with STARTTLS support'
+  option 'with-libntlm', 'Interoperate with some versions of Windows'
 
-  depends_on "gnutls"  => :optional
-  depends_on "libidn"  => :optional
-  depends_on "libntlm" => :optional
+  depends_on 'gnutls'  => :optional
+  depends_on 'libntlm' => :optional
+  depends_on 'gettext'
+  depends_on 'libidn'
+  depends_on 'openssl3' if build.without? 'gnutls'
+  depends_on 'pkg-config'
 
   def install
-    if build.universal?
-      ENV.permit_arch_flags if superenv?
-      ENV.un_m64 if Hardware::CPU.family == :g5_64
-      archs = Hardware::CPU.universal_archs
-      dirs = []
-    else
-      archs = [MacOS.preferred_arch]
-    end
-    archs.each do |arch|
-      if build.universal?
-        ENV.append_to_cflags "-arch #{arch}"
-        dir = "stash-#{arch}"
-        mkdir dir
-        dirs << dir
-      end
-      system "./configure", "--disable-dependency-tracking",
-                            "--with-gssapi-impl=mit",
-                            "--prefix=#{prefix}"
-      system "make", "install"
-      if build.universal?
-        system 'make', 'clean'
-        Merge.scour_keg(prefix, dir)
-        # undo architecture-specific tweak before next run
-        ENV.remove_from_cflags "-arch #{arch}"
-      end # universal?
-    end # archs.each
-    Merge.mach_o(prefix, dirs) if build.universal?
-  end
+    ENV.universal_binary if build.universal?
+
+    args = [
+      "--prefix=#{prefix}",
+      '--disable-dependency-tracking',
+      '--with-gssapi-impl=mit'
+    ]
+    args << '--with-openssl=auto' if build.without? 'gnutls'
+
+    system './configure', *args
+    system 'make'
+    system 'make', 'check'
+    system 'make', 'install'
+  end # install
 
   test do
-    assert_match /#{version}/, shell_output("#{bin}/gsasl")
-  end
-end
-
-class Merge
-  module Pathname_extension
-    def is_bare_mach_o?
-      # header word 0, magic signature:
-      #   MH_MAGIC    = 'feedface' – value with lowest‐order bit clear
-      #   MH_MAGIC_64 = 'feedfacf' – same value with lowest‐order bit set
-      # low‐order 24 bits of header word 1, CPU type:  7 is x86, 12 is ARM, 18 is PPC
-      # header word 3, file type:  no types higher than 10 are defined
-      # header word 5, net size of load commands, is far smaller than the filesize
-      if (self.file? and self.size >= 28 and mach_header = self.binread(24).unpack('N6'))
-        raise('Fat binary found where bare Mach-O file expected') if mach_header[0] == 0xcafebabe
-        ((mach_header[0] & 0xfffffffe) == 0xfeedface and
-          [7, 12, 18].detect { |item| (mach_header[1] & 0x00ffffff) == item } and
-          mach_header[3] < 11 and
-          mach_header[5] < self.size)
-      else
-        false
-      end
-    end unless method_defined?(:is_bare_mach_o?)
-  end # Pathname_extension
-
-  class << self
-    include FileUtils
-
-    def scour_keg(keg_prefix, stash, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      Dir["#{keg_prefix}/#{s_p}*"].each do |f|
-        pn = Pathname(f).extend(Pathname_extension)
-        spb = s_p + pn.basename
-        if pn.directory?
-          Dir.mkdir "#{stash}/#{spb}"
-          scour_keg(keg_prefix, stash, spb)
-        # the number of things that look like Mach-O files but aren’t is horrifying, so test
-        elsif ((not pn.symlink?) and pn.is_bare_mach_o?)
-          cp pn, "#{stash}/#{spb}"
-        end
-      end
-    end # scour_keg
-
-    def mach_o(install_prefix, arch_dirs, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      # generate a full list of files, even if some are not present on all architectures; bear in
-      # mind that the current _directory_ may not even exist on all archs
-      basename_list = []
-      arch_dir_list = arch_dirs.join(',')
-      Dir["{#{arch_dir_list}}/#{s_p}*"].map { |f|
-        File.basename(f)
-      }.each do |b|
-        basename_list << b unless basename_list.count(b) > 0
-      end
-      basename_list.each do |b|
-        spb = s_p + b
-        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{ad}/#{spb}") }
-        pn = Pathname("#{the_arch_dir}/#{spb}")
-        if pn.directory?
-          mach_o(install_prefix, arch_dirs, spb)
-        else
-          arch_files = Dir["{#{arch_dir_list}}/#{spb}"]
-          if arch_files.length > 1
-            system 'lipo', '-create', *arch_files, '-output', install_prefix/spb
-          else
-            # presumably there's a reason this only exists for one architecture, so no error;
-            # the same rationale would apply if it only existed in, say, two out of three
-            cp arch_files.first, install_prefix/spb
-          end # if > 1 file?
-        end # if directory?
-      end # basename_list.each
-    end # mach_o
-  end # << self
-end # Merge
+    assert_match /#{version}/, shell_output("#{bin}/gsasl -V")
+  end # test
+end # Gsasl
