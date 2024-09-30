806db5b88d90af9586a23fb807bae0db151a517b
Gordon Steemson <gsteemso@gmail.com>
2024-09-29 18:12:07 -0700

    utils.rb:  new method .arch_system() for `test` blocks

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -10,63 +10,26 @@ require "open-uri"
 
 class Tty
   class << self
-    def blue
-      bold 34
-    end
+    def blue; bold 34; end
+    def white; bold 39; end
+    def red; underline 31; end
+    def yellow; underline 33; end
+    def em; underline 39; end
+    def green; bold 32; end
+    def gray; bold 30; end
 
-    def white
-      bold 39
-    end
+    def reset; escape 0; end
 
-    def red
-      underline 31
-    end
-
-    def yellow
-      underline 33
-    end
-
-    def reset
-      escape 0
-    end
-
-    def em
-      underline 39
-    end
-
-    def green
-      bold 32
-    end
-
-    def gray
-      bold 30
-    end
-
-    def width
-      `/usr/bin/tput cols`.strip.to_i
-    end
-
-    def truncate(str)
-      str.to_s[0, width - 4]
-    end
+    def width; `/usr/bin/tput cols`.strip.to_i; end
+    def truncate(str); str.to_s[0, width - 4]; end
 
     private
 
-    def color(n)
-      escape "0;#{n}"
-    end
+    def color(n); escape "0;#{n}"; end
+    def bold(n); escape "1;#{n}"; end
+    def underline(n); escape "4;#{n}"; end
 
-    def bold(n)
-      escape "1;#{n}"
-    end
-
-    def underline(n)
-      escape "4;#{n}"
-    end
-
-    def escape(n)
-      "\033[#{n}m" if $stdout.tty?
-    end
+    def escape(n); "\033[#{n}m" if $stdout.tty?; end
   end
 end
 
@@ -239,6 +202,24 @@ def quiet_system(cmd, *args)
   end
 end
 
+# repeats cmd for each of its fat‐binary architectures (useful for `test` blocks)
+def arch_system(cmd, *args)
+  if (arch_cmd = which 'arch')
+    cmd = which(cmd) unless cmd.to_s.start_with? '/'
+    archs = archs_for_command(cmd).select { |a|
+        Hardware::CPU.can_run?(a)
+      }
+    archs.each do |a|
+      system arch_cmd, '-arch', a.to_s, cmd, *args
+    end
+  else
+    opoo "Can’t find the “arch” command.  Running #{cmd} with the default architecture only:"
+    safe_system cmd, *args
+  end
+rescue
+  raise ErrorDuringExecution.new(cmd, args)
+end # arch_system
+
 def curl(*args)
   curl = Pathname.new ENV["HOMEBREW_CURL"]
   raise "#{curl} is not executable" unless curl.exist? && curl.executable?
@@ -337,7 +318,7 @@ end
 
 # Returns array of architectures that the given command or library is built for.
 def archs_for_command(cmd)
-  cmd = which(cmd) unless Pathname.new(cmd).absolute?
+  cmd = which(cmd) unless cmd.start_with? '/'
   Pathname.new(cmd).archs
 end
 
