9d35616f21c00a9afc799aec57494ddb32684438
Gordon Steemson <gsteemso@gmail.com>
2024-09-30 19:25:49 -0700

    webp:  v1.3.2 → 1.4.0; fixed ; " → '

--- Library/Formula/webp.rb
+++ Library/Formula/webp.rb
@@ -1,43 +1,40 @@
 class Webp < Formula
-  desc "Image format providing lossless and lossy compression for web images"
-  homepage "https://developers.google.com/speed/webp/"
-  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.2.tar.gz"
-  sha256 "2a499607df669e40258e53d0ade8035ba4ec0175244869d1025d460562aa09b4"
-  license "BSD-3-Clause"
-
-  bottle do
-    sha256 "e44fb323f2fabe5fef1e31018feb83055786b4dfe19e730bb635f8e00dc9d591" => :tiger_altivec
-  end
+  desc 'Image format providing lossless and lossy compression for web images'
+  homepage 'https://developers.google.com/speed/webp/'
+  url 'https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.4.0.tar.gz'
+  sha256 '61f873ec69e3be1b99535634340d5bde750b2e4447caa1db9f61be3fd49ab1e5'
+  license 'BSD-3-Clause'
 
   head do
-    url "https://chromium.googlesource.com/webm/libwebp.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
+    url 'https://chromium.googlesource.com/webm/libwebp.git'
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool'  => :build
   end
 
   option :universal
 
-  depends_on "giflib"
-  depends_on "jpeg"
-  depends_on "libpng"
-  depends_on "libtiff"
+  depends_on 'giflib'
+  depends_on 'jpeg'
+  depends_on 'libpng'
+  depends_on 'libtiff'
+  depends_on 'sdl2'
 
   def install
-    system "./autogen.sh" if build.head?
+    system './autogen.sh' if build.head?
 
     ENV.universal_binary if build.universal?
-    system "./configure", "--disable-dependency-tracking",
-                          "--enable-libwebpmux",
-                          "--enable-libwebpdemux",
-                          "--enable-libwebpdecoder",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules',
+                          '--enable-everything',
+                          '--enable-swap-16bit-csp'
+    system 'make', 'install'
   end
 
   test do
-    system "#{bin}/cwebp", test_fixtures("test.png"), "-o", "webp_test.png"
-    system "#{bin}/dwebp", "webp_test.png", "-o", "webp_test.webp"
-    assert File.exist?("webp_test.webp")
+    system bin/'cwebp', '-lossless', '-mt', test_fixtures('test.png'), '-o', 'webp_test.webp'
+    system bin/'dwebp', '-mt', 'webp_test.webp', '-o', 'webp_test.png'
+    assert File.exists?('webp_test.webp') and File.exists?('webp_test.png')
   end
 end
