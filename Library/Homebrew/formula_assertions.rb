e69b54513b6648a858f9f3d6bd845c4d75206624
Gordon Steemson <gsteemso@gmail.com>
2024-10-09 16:48:45 -0700

    formula_assertions.rb:  new method:  per-architecture output

--- Library/Homebrew/formula_assertions.rb
+++ Library/Homebrew/formula_assertions.rb
@@ -4,19 +4,14 @@ module Homebrew
       begin
         gem "minitest", "< 5.0.0"
       rescue Gem::LoadError
-        require "test/unit/assertions"
       else
         require "minitest/unit"
-        require "test/unit/assertions"
       end
-    else
-      require "test/unit/assertions"
     end
+    require "test/unit/assertions"
 
     if defined?(MiniTest::Assertion)
       FailedAssertion = MiniTest::Assertion
-    elsif defined?(Minitest::Assertion)
-      FailedAssertion = Minitest::Assertion
     else
       FailedAssertion = Test::Unit::AssertionFailedError
     end
@@ -27,9 +22,33 @@ module Homebrew
     def shell_output(cmd, result = 0)
       ohai cmd
       output = `#{cmd}`
-      puts output if ARGV.verbose?
+      puts output if VERBOSE
       assert_equal result, $?.exitstatus
       output
+    end
+
+    # Returns the outputs of running each arch of cmd, and asserts the exit statuses
+    def arch_outputs(cmd, argstr, result = 0)
+      outputs = []
+      cmd = which(cmd) unless cmd.to_s =~ %r{/}
+      cmd = Pathname.new(cmd) unless cmd.class == Pathname
+      if cmd.universal?
+        if (arch_cmd = which 'arch')
+          cmd.archs.select { |a| Hardware::CPU.can_run?(a) }.each do |a|
+            cmdstr = "#{arch_cmd} -arch #{a.to_s} #{cmd} #{argstr}"
+            ohai cmdstr
+            outputs << `#{cmdstr}` 
+            puts outputs[-1] if VERBOSE
+            assert_equal result, $?.exitstatus
+          end
+        else
+          opoo "Can’t find the “arch” command.  Running #{cmd} with the default architecture only:"
+          outputs << shell_output("#{cmd} #{argstr}", result)
+        end
+      else
+        outputs << shell_output("#{cmd} #{argstr}", result)
+      end
+      outputs
     end
 
     # Returns the output of running the cmd with the optional input, and
@@ -41,7 +60,7 @@ module Homebrew
         pipe.close_write
         pipe.read
       end
-      puts output if ARGV.verbose?
+      puts output if VERBOSE
       assert_equal result, $?.exitstatus unless result.nil?
       output
     end
