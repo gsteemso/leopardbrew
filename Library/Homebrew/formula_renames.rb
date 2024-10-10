264b81bec62c9068d21c0791c06e35f927138e5a
Gordon Steemson <gsteemso@gmail.com>
2024-10-09 17:12:17 -0700

    GPG2 → gnupg rename: finishing touches

--- Library/Homebrew/formula_renames.rb
+++ Library/Homebrew/formula_renames.rb
@@ -7,9 +7,14 @@ FORMULA_RENAMES = {
   "go-app-engine-64"    => "app-engine-go-64",
   "google-perftools"    => "gperftools",
   "google-app-engine"   => "app-engine-python",
-  'gpg-agent'           => 'gnupg',
   "libcppa"             => "caf",
   "mpich2"              => "mpich",
   "objective-caml"      => "ocaml",
   "plt-racket"          => "racket",
+}
+
+FORMULA_SUBSUMPTIONS = {
+  'dirmngr'             => 'gnupg',
+  'gnupg2'              => 'gnupg',
+  'gpg-agent'           => 'gnupg',
 }
