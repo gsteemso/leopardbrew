4bdb3bc6b130750cc285c4cd5b8511d64c68754f
Gordon Steemson <gsteemso@gmail.com>
2024-10-15 22:07:30 -0700

    guile: v2.0.11→3.0.10(64b only); →simple :universal; "→'

--- Library/Formula/guile.rb
+++ Library/Formula/guile.rb
@@ -1,102 +1,67 @@
 class Guile < Formula
-  desc "GUILE:  GNU Ubiquitous Intelligent Language for Extensions"
-  homepage "https://www.gnu.org/software/guile/"
-  url "http://ftpmirror.gnu.org/guile/guile-2.0.11.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/guile/guile-2.0.11.tar.xz"
-  sha256 "aed0a4a6db4e310cbdfeb3613fa6f86fddc91ef624c1e3f8937a6304c69103e2"
-  revision 3
-
-  bottle do
-    sha256 "d7e7ad8d491f84c1405b82ee8ef0da5b21f551b6a0f2795bae92e8bec2f19be2" => :el_capitan
-    sha256 "8e4d3e402e6eb6d95dcfc308b067beb3f7bed522e801c04f2291ffb29aab8908" => :yosemite
-    sha256 "c62b53570f7ac7061820c2c3009c649ff7fbf176bddd0acc36802303ede235e2" => :mavericks
-    sha256 "51f5f379e25fab5cf8fb7cede02841aa716c0e90356705be2abc6a18c6af5371" => :mountain_lion
-  end
+  desc 'GUILE:  GNU Ubiquitous Intelligent Language for Extensions'
+  homepage 'https://www.gnu.org/software/guile/'
+  url 'http://ftpmirror.gnu.org/guile/guile-3.0.10.tar.lz'
+  mirror 'https://ftp.gnu.org/gnu/guile/guile-3.0.10.tar.lz'
+  sha256 'f0d97cccf506e4b6104c51639bd1b7bf67400e0b3401f0b2f6b7136532040327'
 
   head do
-    url "http://git.sv.gnu.org/r/guile.git"
+    url 'http://git.sv.gnu.org/r/guile.git'
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "gettext" => :build
+    depends_on 'autoconf'   => :build
+    depends_on 'automake'   => :build
+    depends_on 'flex'       => :build
+    depends_on 'gperftools' => :build
+    depends_on 'texinfo'    => :build
   end
 
   option :universal
 
-  depends_on "pkg-config" => :build
-  depends_on "libtool" => :run
-  depends_on "libffi"
-  depends_on "libunistring"
-  depends_on "bdw-gc"
-  depends_on "gmp"
-  depends_on "readline"
+  depends_on 'gettext'    => [:build, :run]
+  depends_on 'pkg-config' => :build
+  depends_on 'bdw-gc'
+  depends_on 'gmp'
+  depends_on 'libffi'
+  depends_on 'libtool'    => :run
+  depends_on 'libunistring'
+  depends_on 'readline'
 
+  # does it still?  hells if I know
   fails_with :llvm do
     build 2336
-    cause "Segfaults during compilation"
+    cause 'Segfaults during compilation'
   end
 
+  # does it still?  hells if I know
   fails_with :clang do
     build 211
-    cause "Segfaults during compilation"
+    cause 'Segfaults during compilation'
   end
 
+  # - GCC 4.x can’t handle a repeated typedef if one of the iterations uses a structure declaration
+  #   and the other uses its definition.
+  # - Older Macs don’t have dprintf().
+  patch :DATA
+
   def install
-    if build.universal?
-      ENV.permit_arch_flags if superenv?
-      ENV.un_m64 if Hardware::CPU.family == :g5_64
-      archs = Hardware::CPU.universal_archs
-      mkdir 'arch-stashes'
-      dirs = []
-    else
-      archs = [MacOS.preferred_arch]
-    end
-
-    archs.each do |arch|
-      if build.universal?
-        case arch
-          when :i386, :ppc then ENV.m32
-          when :x86_64, :ppc64 then ENV.m64
-        end
-        mkdir "arch-stashes/#{arch}-bin"
-      end
-
-      system "./autogen.sh" if build.head?
-      system "./configure", "--disable-dependency-tracking",
-                            '--disable-silent-rules',
-                            "--prefix=#{prefix}",
-                            "--with-libreadline-prefix=#{Formula["readline"].opt_prefix}"
+    ENV.universal_binary if build.universal?
+    system './autogen.sh' if build.head?
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules'
+    ENV.deparallelize do
       system 'make'
-      system "make", "install"
-
-      if build.universal?
-        system 'make', 'clean'
-        Merge.scour_keg(prefix, "arch-stashes/#{arch}-bin")
-        # "guile/2.0/libguile/scmconfig.h" is architecture-dependent; when installing :universal,
-        # this copy will be used in merging them all together
-        mkdir_p "arch-stashes/#{arch}-h/guile/2.0/libguile"
-        cp include/'guile/2.0/libguile/scmconfig.h', "arch-stashes/#{arch}-h/guile/2.0/libguile/scmconfig.h"
-        # undo architecture-specific tweaks before next run
-        case arch
-          when :i386, :ppc then ENV.un_m32
-          when :x86_64, :ppc64 then ENV.un_m64
-        end # case arch
-      end # universal?
-    end # archs.each
-
-    if build.universal?
-      Merge.mach_o(prefix, 'arch-stashes', archs)
-      Merge.c_headers(include, 'arch-stashes', archs)
-      Merge.pkg_cfg(lib/'pkgconfig')
-    end # universal?
+      system 'make', '-i', '-k', 'check'
+    end
+    system 'make', 'install'
 
     # A really messed up workaround required on OS X --mkhl
-    Pathname.glob("#{lib}/*.dylib") do |dylib|
+    Pathname.glob(lib/'*.dylib') do |dylib|
       lib.install_symlink dylib.basename => "#{dylib.basename(".dylib")}.so"
     end
 
     (share/"gdb/auto-load").install Dir["#{lib}/*-gdb.scm"]
-  end
+  end # install
 
   test do
     hello = testpath/"hello.scm"
@@ -108,160 +73,55 @@ class Guile < Formula
     ENV["GUILE_AUTO_COMPILE"] = "0"
 
     system bin/"guile", hello
-  end
-end
+  end # test
+end # Guile
 
-class Merge
-  module Pathname_extension
-    def is_bare_mach_o?
-      # header word 0, magic signature:
-      #   MH_MAGIC    = 'feedface' – value with lowest‐order bit clear
-      #   MH_MAGIC_64 = 'feedfacf' – same value with lowest‐order bit set
-      # low‐order 24 bits of header word 1, CPU type:  7 is x86, 12 is ARM, 18 is PPC
-      # header word 3, file type:  no types higher than 10 are defined
-      # header word 5, net size of load commands, is far smaller than the filesize
-      if (self.file? and self.size >= 28 and mach_header = self.binread(24).unpack('N6'))
-        raise('Fat binary found where bare Mach-O file expected') if mach_header[0] == 0xcafebabe
-        ((mach_header[0] & 0xfffffffe) == 0xfeedface and
-          [7, 12, 18].detect { |item| (mach_header[1] & 0x00ffffff) == item } and
-          mach_header[3] < 11 and
-          mach_header[5] < self.size)
-      else
-        false
-      end
-    end unless method_defined?(:is_bare_mach_o?)
-  end # Pathname_extension
-
-  class << self
-    include FileUtils
-
-    def scour_keg(keg_prefix, stash, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      Dir["#{keg_prefix}/#{s_p}*"].each do |f|
-        pn = Pathname(f).extend(Pathname_extension)
-        spb = s_p + pn.basename
-        if pn.directory?
-          Dir.mkdir "#{stash}/#{spb}"
-          scour_keg(keg_prefix, stash, spb)
-        # the number of things that look like Mach-O files but aren’t is horrifying, so test
-        elsif ((not pn.symlink?) and pn.is_bare_mach_o?)
-          cp pn, "#{stash}/#{spb}"
-        end
-      end
-    end # scour_keg
-
-    def c_headers(include_dir, stash_root, archs, sub_path = '')
-      # Architecture-specific <header>.<extension> files need to be surgically combined and were
-      # stashed for this purpose.  The differences are relatively minor and can be “#if defined ()”
-      # together.  We make the simplifying assumption that the architecture-dependent headers in
-      # question are present on all architectures.
-      #
-      # Don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      Dir["#{stash_root}/#{archs[0]}-h/#{s_p}*"].each do |basis_file|
-        spb = s_p + File.basename(basis_file)
-        if File.directory?(basis_file)
-          c_headers(include_dir, stash_root, archs, spb)
-        else
-          diffpoints = {}  # Keyed by line number in the basis file.  Each value is an array of
-                           # three‐element hashes; containing the arch, the hunk’s displacement
-                           # (number of basis‐file lines it replaces), and an array of its lines.
-          archs[1..-1].each do |a|
-            raw_diffs = `diff --minimal --unified=0 #{basis_file} #{stash_root}/#{a}-h/#{spb}`
-            next unless raw_diffs
-            # The unified diff output begins with two lines identifying the source files, which are
-            # followed by a series of hunk records, each describing one difference that was found.
-            # Each hunk record begins with a line that looks like:
-            # @@ -line_number,length_in_lines +line_number,length_in_lines @@
-            diff_hunks = raw_diffs.lines[2..-1].join('').split(/(?=^@@)/)
-            diff_hunks.each do |d|
-              # lexical sorting of numbers requires that they all be the same length
-              base_linenumber_string = ('00000' + d.match(/\A@@ -(\d+)/)[1])[-6..-1]
-              unless diffpoints.has_key?(base_linenumber_string)
-                diffpoints[base_linenumber_string] = []
-              end
-              length_match = d.match(/\A@@ -\d+,(\d+)/)
-              # if the hunk length is 1, the comma and second number are not present
-              length_match = (length_match == nil ? 1 : length_match[1].to_i)
-              line_group = []
-              # we want the lines that are either unchanged between files or only found in the non‐
-              # basis file; and to shave off the leading ‘+’ or ‘ ’
-              d.lines { |line| line_group << line[1..-1] if line =~ /^[+ ]/ }
-              diffpoints[base_linenumber_string] << {
-                :arch => a,
-                :displacement => length_match,
-                :hunk_lines => line_group
-              }
-            end # each diff hunk |d|
-          end # each arch |a|
-          # Ideally, the logic would account for overlapping and/or different-displacement hunks at
-          # this point; but since most packages do not seem to generate such in the first place, it
-          # can wait.  That said, packages exist (e.g. both Python 2 and Python 3) which can and do
-          # generate quad fat binaries, so it can’t be ignored forever.
-          basis_lines = []
-          File.open(basis_file, 'r') { |text| basis_lines = text.read.lines[0..-1] }
-          # Bear in mind that the line-array indices are one less than the line numbers.
-          #
-          # Start with the last diff point so the insertions don’t screw up our line numbering:
-          diffpoints.keys.sort.reverse.each do |index_string|
-            diff_start = index_string.to_i - 1
-            diff_end = index_string.to_i + diffpoints[index_string][0][:displacement] - 2
-            adjusted_lines = [
-              "\#if defined (__#{archs[0]}__)\n",
-              basis_lines[diff_start..diff_end],
-              *(diffpoints[index_string].map { |dp|
-                  [ "\#elif defined (__#{dp[:arch]}__)\n", *(dp[:hunk_lines]) ]
-                }),
-              "\#endif\n"
-            ]
-            basis_lines[diff_start..diff_end] = adjusted_lines
-          end # each key |index_string|
-          File.new("#{include_dir}/#{spb}", 'w').syswrite(basis_lines.join(''))
-        end # if not a directory
-      end # each |basis_file|
-    end # c_headers
-
-    # install_prefix expects a Pathname object, not just a string
-    def mach_o(install_prefix, stash_root, archs, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      # generate a full list of files, even if some are not present on all architectures; bear in
-      # mind that the current _directory_ may not even exist on all archs
-      basename_list = []
-      arch_dirs = archs.map {|a| "#{a}-bin"}
-      arch_dir_list = arch_dirs.join(',')
-      Dir["#{stash_root}/{#{arch_dir_list}}/#{s_p}*"].map { |f|
-        File.basename(f)
-      }.each { |b|
-        basename_list << b unless basename_list.count(b) > 0
-      }
-      basename_list.each do |b|
-        spb = s_p + b
-        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{stash_root}/#{ad}/#{spb}") }
-        pn = Pathname("#{stash_root}/#{the_arch_dir}/#{spb}")
-        if pn.directory?
-          mach_o(install_prefix, stash_root, archs, spb)
-        else
-          arch_files = Dir["#{stash_root}/{#{arch_dir_list}}/#{spb}"]
-          if arch_files.length > 1
-            system 'lipo', '-create', *arch_files, '-output', install_prefix/spb
-          else
-            # presumably there's a reason this only exists for one architecture, so no error;
-            # the same rationale would apply if it only existed in, say, two out of three
-            cp arch_files.first, install_prefix/spb
-          end # if > 1 file?
-        end # if directory?
-      end # each basename |b|
-    end # mach_o
-
-    def pkg_cfg(pc_dir)
-      pc_dir.children.select { |f|
-        (not f.symlink?) and f.file? and f.fnmatch('*.pc')
-      }.each do |f|
-        fdata = f.read.gsub(/-arch \S+|-m32|-m64/, '')
-        f.open('w') { |io| io.write(fdata) }
-      end # each dir child |f|
-    end # pkg_cfg
-  end # << self
-end # Merge
+__END__
+--- old/libguile/dynstack.h	2019-08-02 05:41:06 -0700
++++ new/libguile/dynstack.h	2024-08-21 11:02:08 -0700
+@@ -29,12 +29,12 @@
+ 
+ 
+ 
+-typedef struct scm_dynstack
++struct scm_dynstack
+ {
+   scm_t_bits *base;
+   scm_t_bits *top;
+   scm_t_bits *limit;
+-} scm_t_dynstack;
++};
+ 
+ 
+ 
+--- old/libguile/posix.c	2024-06-19 11:54:50 -0700
++++ new/libguile/posix.c	2024-08-21 11:48:18 -0700
+@@ -1609,7 +1609,7 @@
+       default:    /* ENOENT, etc. */
+         /* Report the error on the console (before switching to
+            'posix_spawn', the child process would do exactly that.)  */
+-        dprintf (err, "In execvp of %s: %s\n", exec_file,
++        fprintf (stderr, "In execvp of %s: %s\n", exec_file,
+                  strerror (errno_save));
+       }
+ 
+--- old/libguile/print.h	2021-09-30 05:30:38 -0700
++++ new/libguile/print.h	2024-08-21 11:00:45 -0700
+@@ -61,7 +61,7 @@
+   SCM_MAKE_VALIDATE_MSG(pos, a, PRINT_STATE_P, "print-state")
+ 
+ #define SCM_PRINT_STATE_LAYOUT "pwuwuwuwuwuwpwuwuwuwpwpw"
+-typedef struct scm_print_state {
++struct scm_print_state {
+   SCM handle;			/* Struct handle */
+   int revealed;                 /* Has the state escaped to Scheme? */
+   unsigned long writingp;	/* Writing? */
+@@ -76,7 +76,7 @@
+ 				   circular reference detection;
+ 				   a vector. */
+   SCM highlight_objects;        /* List of objects to be highlighted */
+-} scm_print_state;
++};
+ 
+ SCM_API SCM scm_print_state_vtable;
+ 
