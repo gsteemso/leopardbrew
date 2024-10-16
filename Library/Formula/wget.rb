ea777a8f009d1543ed704e89b984d5c7e38aa752
Gordon Steemson <gsteemso@gmail.com>
2024-10-15 22:36:41 -0700

    wget (untested):  v1.21.3 → 1.24.5; :universal; " → '

--- Library/Formula/wget.rb
+++ Library/Formula/wget.rb
@@ -2,61 +2,71 @@
 # Upstream issue: https://savannah.gnu.org/bugs/index.php?37063
 
 class Wget < Formula
-  desc "Internet file retriever"
-  homepage "https://www.gnu.org/software/wget/"
-  url "https://ftpmirror.gnu.org/wget/wget-1.21.3.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/wget/wget-1.21.3.tar.gz"
-  sha256 "5726bb8bc5ca0f6dc7110f6416e4bb7019e2d2ff5bf93d1ca2ffcc6656f220e5"
-  revision 1
+  desc 'Internet file retriever'
+  homepage 'https://www.gnu.org/software/wget/'
+  url 'http://ftpmirror.gnu.org/wget/wget-1.24.5.tar.gz'
+  mirror 'https://ftp.gnu.org/gnu/wget/wget-1.24.5.tar.gz'
+  sha256 'fa2dc35bab5184ecbc46a9ef83def2aaaa3f4c9f3c97d4bd19dcb07d4da637de'
 
   head do
-    url "git://git.savannah.gnu.org/wget.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "gettext"
+    url 'https://git.savannah.gnu.org/wget.git'
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
   end
 
-  bottle do
-    sha256 "81581a3befe8af2552a4f6b3a574b712ae9fb0f85b2d1bbe1f1de2e96d4d1a9f" => :tiger_altivec
-  end
+  deprecated_option 'enable-iri' => nil
+  deprecated_option 'with-iri' => nil
+  deprecated_option 'enable-debug' => 'with-debug'
 
-  deprecated_option "enable-iri" => "with-iri"
-  deprecated_option "enable-debug" => "with-debug"
+  option :universal
+  option 'with-debug', 'Build with debug support'
+  option 'with-libressl', 'Build with LibreSSL security (replaces gnutls)'
+  option 'with-openssl3', 'Build with OpenSSL security (replaces gnutls)'
 
-  option "with-iri", "Enable iri support"
-  option "with-debug", "Build with debug support"
-
-  depends_on "openssl3" => :recommended
-  depends_on "libressl" => :optional
-  depends_on "libidn" if build.with? "iri"
-  depends_on "pcre2" => :optional
-  depends_on "zlib"
+  depends_on 'libpsl'   => :recommended
+  depends_on 'pcre2'    => :recommended
+  depends_on 'c-ares'       => :optional
+  depends_on 'gpgme'        => :optional
+  depends_on 'libmetalink'  => :optional
+  depends_on 'libressl'     => :optional
+  depends_on 'openssl3'     => :optional
+  depends_on 'gnutls' if build.without? 'libressl' and build.without? 'openssl3'
+  depends_on 'gettext'
+  depends_on 'libidn2'
+  depends_on 'libunistring'
+  depends_on 'pkg-config'
+  depends_on 'zlib'
 
   def install
-  ENV.append "CFLAGS", "-std=gnu99"
+    ENV.universal_binary if build.universal?
+    raise 'Only one SSL backend can be used.  OpenSSL and LibreSSL have both been specified.' \
+                                                if build.with? 'libressl' and build.with? 'openssl3'
     args = %W[
       --prefix=#{prefix}
-      --sysconfdir=#{etc}
-      --with-ssl=openssl
+      --disable-dependency-tracking
+      --disable-silent-rules
     ]
-
-    if build.with? "libressl"
-      args << "--with-libssl-prefix=#{Formula["libressl"].opt_prefix}"
-    else
-      args << "--with-libssl-prefix=#{Formula["openssl"].opt_prefix}"
-    end
-
-    args << "--disable-debug" if build.without? "debug"
-    args << "--disable-iri" if build.without? "iri"
-    args << "--disable-pcre" if build.without? "pcre"
-
-    system "./bootstrap" if build.head?
-    system "./configure", *args
-    system "make", "install"
-  end
+    args << '--disable-debug' if build.without? 'debug'
+    if build.with? 'libressl' or build.with? 'openssl3'
+      args << '--with-ssl=openssl'
+      args << "--with-libssl-prefix=#{Formula[build.with?('libressl') \
+                                               ? 'libressl' \
+                                               : 'openssl3' \
+                                             ].opt_prefix}"
+    end # with libressl or openssl3
+    args << '--with-cares' if build.with? 'c-ares'
+    # gpgme gets picked up unaided if specified
+    args << '--with-metalink' if build.with? 'libmetalink'
+    args << '--disable-psl' if build.without? 'libpsl'
+    args << '--disable-pcre2' if build.without? 'pcre2'
+    system './bootstrap' if build.head?
+    system './configure', *args
+    system 'make', 'install'
+  end # install
 
   test do
-    system bin/"wget", "-O", "-", "https://github.com"
+    system bin/'wget', '-O', '-', 'https://github.com'
   end
 end
+
+__END__
