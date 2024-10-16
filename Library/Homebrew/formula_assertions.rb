3530f59cf499bc4834798c828ca0a861ca9ee46c
Gordon Steemson <gsteemso@gmail.com>
2024-10-15 22:15:32 -0700

    formula_assertions:  reverted (scrapped `arch_outputs`)

--- Library/Homebrew/formula_assertions.rb
+++ Library/Homebrew/formula_assertions.rb
@@ -25,31 +25,7 @@ module Homebrew
       puts output if VERBOSE
       assert_equal result, $?.exitstatus
       output
-    end
-
-    # Returns the outputs of running each arch of cmd, and asserts the exit statuses
-    def arch_outputs(cmd, argstr, result = 0)
-      outputs = []
-      cmd = which(cmd) unless cmd.to_s =~ %r{/}
-      cmd = Pathname.new(cmd) unless cmd.class == Pathname
-      if cmd.universal?
-        if (arch_cmd = which 'arch')
-          cmd.archs.select { |a| Hardware::CPU.can_run?(a) }.each do |a|
-            cmdstr = "#{arch_cmd} -arch #{a.to_s} #{cmd} #{argstr}"
-            ohai cmdstr
-            outputs << `#{cmdstr}` 
-            puts outputs[-1] if VERBOSE
-            assert_equal result, $?.exitstatus
-          end
-        else
-          opoo "Can’t find the “arch” command.  Running #{cmd} with the default architecture only:"
-          outputs << shell_output("#{cmd} #{argstr}", result)
-        end
-      else
-        outputs << shell_output("#{cmd} #{argstr}", result)
-      end
-      outputs
-    end
+    end # shell_output
 
     # Returns the output of running the cmd with the optional input, and
     # optionally asserts the exit status
@@ -63,6 +39,6 @@ module Homebrew
       puts output if VERBOSE
       assert_equal result, $?.exitstatus unless result.nil?
       output
-    end
-  end
-end
+    end # pipe_output
+  end # Assertions
+end # Homebrew
