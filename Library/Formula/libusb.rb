2e9c887e50f3cc28e827ee9bd2ee657589aede0c
Gordon Steemson <gsteemso@gmail.com>
2024-10-15 22:23:38 -0700

    libusb:  v1.0.23 → 1.0.27; :universal; needs C11; " →'

--- Library/Formula/libusb.rb
+++ Library/Formula/libusb.rb
@@ -1,37 +1,44 @@
 class Libusb < Formula
-  desc "Library for USB device access"
-  homepage "https://libusb.info/"
-  url "https://github.com/libusb/libusb/releases/download/v1.0.23/libusb-1.0.23.tar.bz2"
-  sha256 "db11c06e958a82dac52cf3c65cb4dd2c3f339c8a988665110e0d24d19312ad8d"
-  license "LGPL-2.1-or-later"
-
-  bottle do
-    sha256 "73b540f092915bee23b2afd8735ccc94b3fa150efbcdd7921f7ef3d4da9984e1" => :tiger_altivec
-  end
+  desc 'Cross‐platform library for USB device access'
+  homepage 'https://libusb.info/'
+  url 'https://github.com/libusb/libusb/releases/download/v1.0.27/libusb-1.0.27.tar.bz2'
+  sha256 'ffaa41d741a8a3bee244ac8e54a72ea05bf2879663c098c82fc5757853441575'
+  license 'LGPL-2.1-or-later'
 
   head do
-    url "https://github.com/libusb/libusb.git", branch: "master"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
+    url 'https://github.com/libusb/libusb.git', branch: 'master'
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool'  => :build
   end
 
-  def install
-    args = %W[--disable-dependency-tracking --prefix=#{prefix}]
+  option :universal
 
-    system "./autogen.sh" if build.head?
-    system "./configure", *args
-    system "make", "install"
-    (pkgshare/"examples").install Dir["examples/*"] - Dir["examples/Makefile*"]
+  depends_on C11Requirement.new
+
+  def install
+    ENV.universal_binary if build.universal?
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-system-log
+      --enable-tests-build
+    ]
+    system './autogen.sh' if build.head?
+    system './configure', *args
+    system 'make'
+    system 'make', 'check'
+    system 'make', 'install'
+    (pkgshare/'examples').install Dir['examples/*'] - Dir['examples/Makefile*']
   end
 
   test do
-    cp_r (pkgshare/"examples"), testpath
-    cd "examples" do
-      system ENV.cc, "listdevs.c", "-L#{lib}", "-I#{include}/libusb-1.0",
-             "-lusb-1.0", "-o", "test"
-      system "./test"
+    cp_r (pkgshare/'examples'), testpath
+    cd 'examples' do
+      system ENV.cc, 'listdevs.c', "-L#{lib}", "-I#{include}/libusb-1.0",
+             '-lusb-1.0', '-o', 'test'
+      system './test'
     end
   end
 end
