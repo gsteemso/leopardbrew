0a3eae71d422d6118c1bd389d18fa29211bcb6d3
Gordon Steemson <gsteemso@gmail.com>
2024-10-27 01:10:17 -0700

    cmd/install:  help text; checks; global consts; →Leopardbrew; endtags; reformat

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -1,3 +1,26 @@
+#:
+#:  Usage:  brew install [/options/] /formula/ [...]
+#:
+#:Install each listed /formula/, applying the /options/ to each one.
+#:
+#:  --build-bottle   - Build a bottled version of the software.  Specify what
+#:                     platform to build it for with “--bottle-arch=____”.
+#:  --build-from-source - Build from source code, even if a bottle is available.
+#:  --cc=/compiler/  - Use /compiler/ to build the software.
+#:  --debug (-d)     - See messages for debugging the software installation.
+#:  --devel, --HEAD  - Install the development version or the source repository,
+#:                     if available.  These are mutually exclusive.
+#:  --force (-f)     - Install the formula even if it might cause problems.
+#:  --force-bottle   - Use the bottled version.  Overrides --build-from-source.
+#:  --git (-g)       - Install by making a Git repository.  Implies -i.
+#:  --ignore-dependencies - Assume that the software’s dependencies are present.
+#:  --interactive (-i) - Install the formula manually, starting right after any
+#:                       patches are applied.
+#:  --only-dependencies - Install its dependencies, but not the formula itself.
+#:  --quieter (-q)   - Don’t be verbose when brewing dependencies.  Implies -v.
+#:  --verbose (-v)   - See lots of progress messages as the software builds.
+#:
+
 require 'blacklist'
 require 'cmd/doctor'
 require 'cmd/search'
@@ -8,12 +31,16 @@ require 'hardware'
 module Homebrew
   def install
     raise FormulaUnspecifiedError if ARGV.named.empty?
-
     raise 'Specify “--HEAD” in uppercase to build from the latest source code.' if ARGV.include? '--head'
+    raise '--ignore-dependencies and --only-dependencies are mutually exclusive.' \
+                                                           if ARGV.ignore_deps? and ARGV.only_deps?
+    # if the user's flags will prevent bottle only-installations when no
+    # developer tools are available, we need to stop them early on
+    FormulaInstaller.prevent_build_flags unless MacOS.has_apple_developer_tools?
 
     ARGV.named.each do |name|
-      if !File.exist?(name) && (name !~ HOMEBREW_CORE_FORMULA_REGEX) \
-              && (name =~ HOMEBREW_TAP_FORMULA_REGEX || name =~ HOMEBREW_CASK_TAP_FORMULA_REGEX)
+      if !File.exists?(name) and (name !~ HOMEBREW_CORE_FORMULA_REGEX) \
+                and (name =~ HOMEBREW_TAP_FORMULA_REGEX or name =~ HOMEBREW_CASK_TAP_FORMULA_REGEX)
         install_tap $1, $2
       end
     end unless ARGV.force?
@@ -36,13 +63,8 @@ module Homebrew
         end
       end
 
-      # if the user's flags will prevent bottle only-installations when no
-      # developer tools are available, we need to stop them early on
-      FormulaInstaller.prevent_build_flags unless MacOS.has_apple_developer_tools?
-
       ARGV.formulae.each do |f|
         requested_spec = (ARGV.build_head? ? :head : (ARGV.build_devel? ? :devel : :stable))
-
         case requested_spec
           when :stable
             if f.stable.nil?
@@ -54,18 +76,14 @@ module Homebrew
                 raise "#{f.full_name} has no stable download, please choose --devel or --HEAD"
               end
             end
-          when :head
-            raise "No head is defined for #{f.full_name}" if f.head.nil?
-          when :devel
-            raise "No devel block is defined for #{f.full_name}" if f.devel.nil?
+          when :head then raise "No head is defined for #{f.full_name}" if f.head.nil?
+          when :devel then raise "No devel block is defined for #{f.full_name}" if f.devel.nil?
         end
 
         if f.installed?(requested_spec)
           msg = "#{f.full_name}|#{f.send(requested_spec).version} is already installed"
-          msg << ', it’s just not linked' unless \
-            (f.linked_keg.symlink? and
-             f.linked_keg.readlink.realpath == f.installed_prefix(requested_spec)) or
-            f.keg_only?
+          msg << ', it’s just not linked' unless f.keg_only? or (f.linked_keg.symlink? and
+                              f.linked_keg.readlink.realpath == f.prefix(requested_spec))
           opoo msg
         elsif f.oldname_installed? and not ARGV.force?
           # Check if the formula we try to install is the same as installed
@@ -76,7 +94,7 @@ module Homebrew
         else
           formulae << f
         end
-      end
+      end # each ARGV formula |f|
 
       perform_preinstall_checks
 
@@ -92,18 +110,17 @@ module Homebrew
         ohai 'Searching taps...'
         puts_columns(search_taps(query))
 
-        # If they haven't updated in a week (604800 seconds), that
-        # might explain the error
+        # If they haven't updated in a while, that might explain the error
         master = HOMEBREW_REPOSITORY/'.git/refs/heads/master'
-        if master.exist? && (Time.now.to_i - File.mtime(master).to_i) > 604800
-          ohai 'You haven’t updated Homebrew in a while.', <<-EOS.undent
+        if master.exists? and (Time.now.to_i - File.mtime(master).to_i) > HOMEBREW_OUTDATED_LIMIT
+          ohai 'You haven’t updated Leopardbrew in a while.', <<-EOS.undent
             A formula for #{e.name} might have been added recently.
-            Run “brew update” to get the latest Homebrew updates!
+            Run “brew update” to get the latest Leopardbrew updates!
           EOS
-        end
-      end
-    end
-  end
+        end # outdated?
+      end # not blacklisted
+    end # rescue Formula Unavailable
+  end # install
 
   def check_writable_install_location
     raise "Cannot write to #{HOMEBREW_CELLAR}" unless HOMEBREW_CELLAR.writable_real?
@@ -122,14 +139,15 @@ module Homebrew
       out = checks.send(check)
       opoo out unless out.nil?
     end
-  end
+  end # check_xcode
 
   def check_macports
     opoo 'It appears you have MacPorts or Fink installed.',
-      'Software installed with other package managers causes known problems for', "\n",
-      '’brewing. If a formula fails to build, uninstall MacPorts/Fink and try again.' \
-        unless MacOS.macports_or_fink.empty?
-  end
+      <<-_.undent unless MacOS.macports_or_fink.empty?
+        Software installed with other package managers causes known problems for
+        ’brewing. If a formula fails to build, uninstall MacPorts/Fink and try again.
+      _
+  end # check_macports
 
   def check_cellar
     FileUtils.mkdir_p HOMEBREW_CELLAR unless HOMEBREW_CELLAR.exists?
@@ -138,13 +156,21 @@ module Homebrew
       Could not create #{HOMEBREW_CELLAR}
       Check you have permission to write to #{HOMEBREW_CELLAR.parent}
     EOS
-  end
+  end # check_cellar
 
   def perform_preinstall_checks
     check_writable_install_location
     check_xcode if MacOS.has_apple_developer_tools?
     check_cellar
   end
+
+  def remove_failed_install(f)
+    # Leave no trace of the failed installation.
+    if f.prefix.exists?
+      oh1 "Cleaning up failed #{f.prefix}" if DEBUG
+      ignore_interrupts { f.prefix.rmtree }
+    end
+  end # remove_failed_install
 
   def install_formula(f)
     f.print_tap_action
@@ -156,39 +182,28 @@ module Homebrew
     fi.build_from_source   = ARGV.build_from_source?
     fi.build_bottle        = ARGV.build_bottle?
     fi.force_bottle        = ARGV.force_bottle?
-    fi.interactive         = ARGV.interactive?
+    fi.interactive         = ARGV.interactive? or ARGV.git?
     fi.git                 = ARGV.git?
-    fi.verbose             = VERBOSE
-    fi.quieter             = ARGV.quieter?
+    fi.verbose             = VERBOSE or QUIETER
+    fi.quieter             = QUIETER
     fi.debug               = DEBUG
     fi.prelude
-    fi.install
-    fi.finish
-    fi.insinuate
+    fi.install  # if successful, this renames the new keg to its installed prefix
   rescue FormulaInstallationAlreadyAttemptedError
-    # We already attempted to install f as part of the dependency tree of
-    # another formula. In that case, don't generate an error, just move on.
+    # next
   rescue CannotInstallFormulaError => e
-    # leave no trace of the failed installation
-    if f.prefix.exists?
-      oh1 "Deleting failed install at #{f.prefix}" if DEBUG
-      f.prefix.rmtree
-    end
+    remove_failed_install(f)
     ofail e.message
   rescue BuildError
-    # leave no trace of the failed installation
-    if f.prefix.exists?
-      oh1 "Deleting failed install at #{f.prefix}" if DEBUG
-      f.prefix.rmtree
-    end
+    remove_failed_install(f)
     check_macports
     raise
-  rescue Exception
-    # leave no trace of the failed installation
-    if f.prefix.exists?
-      oh1 "Deleting failed install at #{f.prefix}" if DEBUG
-      f.prefix.rmtree
-    end
+  rescue Exception => e
+    puts e.inspect if DEBUG
+    remove_failed_install(f)
     raise
-  end
-end
+  else
+    fi.finish
+    fi.insinuate
+  end # install_formula
+end # Homebrew
