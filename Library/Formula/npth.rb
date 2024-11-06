45c7f4e5348c1fb0f01ef46f8d1e90beef733dad
Gordon Steemson <gsteemso@gmail.com>
2024-11-06 15:15:29 -0800

    npth:  trivial tweak to integrate the patch better

--- Library/Formula/npth.rb
+++ Library/Formula/npth.rb
@@ -30,69 +30,47 @@ end
 
 __END__
 --- old/src/npth.c	2024-02-05 03:09:26 -0800
-+++ new/src/npth.c	2024-10-03 18:02:25 -0700
-@@ -29,42 +29,45 @@
- #include <pthread.h>
- #include <fcntl.h>
- #include <sys/stat.h>
--#ifdef HAVE_LIB_DISPATCH
--# include <dispatch/dispatch.h>
--typedef dispatch_semaphore_t sem_t;
--
--/* This glue code is for macOS which does not have full implementation
--   of POSIX semaphore.  On macOS, using semaphore in Grand Central
--   Dispatch library is better than using the partial implementation of
--   POSIX semaphore where sem_init doesn't work well.
-- */
-+/* This glue code is for versions of Mac OS predating Grand Central Dispatch. */
-+#include <CoreServices/CoreServices.h>
-+#include <CarbonCore/Multiprocessing.h>
-+typedef int sem_t;
- 
-+/* Note that the MultiProcessing Services API tracks semaphores by ID number, not by address. */
- static int
- sem_init (sem_t *sem, int is_shared, unsigned int value)
- {
-+  OSStatus result;
-   (void)is_shared;
--  if ((*sem = dispatch_semaphore_create (value)) == NULL)
--    return -1;
--  else
-+  result = MPCreateSemaphore ((MPSemaphoreCount)UINT_MAX, (MPSemaphoreCount)value, (MPSemaphoreID *)sem);
-+  if (result == noErr)
-     return 0;
-+  else
-+    return -1;
++++ new/src/npth.c	2024-10-31 11:18:20 -0700
+@@ -63,7 +63,41 @@
+   return 0;
  }
- 
- static int
- sem_post (sem_t *sem)
- {
--  dispatch_semaphore_signal (*sem);
--  return 0;
-+  OSStatus result;
-+  result = MPSignalSemaphore ((MPSemaphoreID) *sem);
-+  if (result == noErr)
-+    return 0;
-+  else
-+    return -1;
- }
- 
- static int
- sem_wait (sem_t *sem)
- {
--  dispatch_semaphore_wait (*sem, DISPATCH_TIME_FOREVER);
--  return 0;
-+  OSStatus result;
-+  result = MPWaitOnSemaphore ((MPSemaphoreID) *sem, kDurationForever);
-+  if (result == noErr)
-+    return 0;
-+  else
-+    return -1;
- }
--#else
+ #else
 -# include <semaphore.h>
--#endif
++# ifdef __MAC_10_0  /* i.e., if we’re on a Mac at all */
++   /* As noted above, Mac OS only partially implements POSIX semaphores
++      – but Grand Central Dispatch only exists from Mac OS 10.6 onward.
++      This glue code is for versions of Mac OS older than that.
++    */
++#  include <CoreServices/CoreServices.h>
++#  include <CarbonCore/Multiprocessing.h>
++   typedef MPSemaphoreID sem_t;
++
++   static int
++   sem_init (sem_t *sem, int is_shared, unsigned int value)
++   {
++     (void)is_shared;
++     if (MPCreateSemaphore ((MPSemaphoreCount)UINT_MAX, (MPSemaphoreCount)value, sem) == noErr)
++       return 0;
++     else
++       return -1;
++   }
++
++   static int
++   sem_post (sem_t *sem)
++   {
++     MPSignalSemaphore (*sem);
++     return 0;
++   }
++
++   static int
++   sem_wait (sem_t *sem)
++   {
++     MPWaitOnSemaphore (*sem, kDurationForever);
++     return 0;
++   }
++# else
++#  include <semaphore.h>
++# endif
+ #endif
  #ifdef HAVE_UNISTD_H
  # include <unistd.h>
- #endif
