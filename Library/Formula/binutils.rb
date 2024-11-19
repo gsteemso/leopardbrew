7699e66b1efb4d9828bd8d0cec19264275188231
Gordon Steemson <gsteemso@gmail.com>
2024-11-18 21:54:27 -0800

    binutils:  v2.25.1→2.43.1; `needs :tls`; trivially :universal; better config

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -1,40 +1,57 @@
 class Binutils < Formula
-  desc "FSF Binutils for native development"
-  homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.25.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.gz"
-  sha256 "82a40a37b13a12facb36ac7e87846475a1d80f2e63467b1b8d63ec8b6a2b63fc"
+  # The only sub‐package applicable to Mac OS (Darwin) is the actual binutils.
+  # gas, ld, gold, and all of the other user‐visible sub‐packages are unable to
+  # handle Mach-O binaries.
+  desc 'GNU Binary Utilities for native development'
+  homepage 'https://www.gnu.org/software/binutils/binutils.html'
+  url 'https://ftpmirror.gnu.org/binutils/binutils-2.43.1.tar.lz'
+  mirror 'https://ftp.gnu.org/gnu/binutils/binutils-2.43.1.tar.lz'
+  sha256 '1bd0b4604a7b1f65737c8ad0ce5191daf233843c73c6bf5429eb4e95895bd136'
+
+  # Unfortunately, the binutils sub‐package requires the bfd one, which is
+  # designed around thread-local storage and thus cannot be built by GCC 4.x.
+  needs :tls
 
   # No --default-names option as it interferes with Homebrew builds.
+  option :universal
+  option 'with-zstd', 'Allow debugging‐data compression in ZStandard format'
+  option 'without-nls', 'Build without natural‐language support (internationalization)'
 
-  bottle do
-    sha256 "ab393fd01e8bb2f9ff697efa3e14765eb32468a46380ce8c2db0d5baf1b588b5" => :el_capitan
-    sha256 "299f9bbdf522b68b803ce6cdb109a34b018459f3815771b3530ba95e06ee19f7" => :yosemite
-    sha256 "30b13f1b4ddfe09f5140c2f00d0d34d42ed471ed2ab241405d8f5b7d3fb8eb41" => :mavericks
-    sha256 "689d5c651fba64a18fed5f8f66ba9d156af7f3b8787e383afe8979f535432d64" => :mountain_lion
-  end
-
-  # Fixes build on 10.6. Committed upstream; will be in the next release:
-  # https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;a=commitdiff;h=31593e1b96c792abba3c5268d6423975aefa56b2
-  patch :p1 do
-    url "https://gist.githubusercontent.com/mistydemeo/f14c03c0c9ddfce97cf7/raw/2107e53002c34e41ade9b54629e670184e438d80/binutils_10.6.diff"
-    sha256 "5ff40b9d7ad174d3c24edd8381471b8f5c408703111f0c90135ad0fffba452c2"
-  end
+  depends_on 'pkg-config' => :build if build.with? 'zstd'
+  depends_on 'isl'
+  depends_on 'zlib'
+  depends_on 'gettext' if build.with? 'nls'
+  depends_on 'zstd' => :optional
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--program-prefix=g",
-                          "--prefix=#{prefix}",
-                          "--infodir=#{info}",
-                          "--mandir=#{man}",
-                          "--disable-werror",
-                          "--enable-interwork",
-                          "--enable-multilib",
-                          "--enable-64-bit-bfd",
-                          "--enable-targets=all"
-    system "make"
-    system "make", "install"
+    ENV.universal_binary if build.universal?
+    ENV.prepend_path ENV['PKG_CONFIG_PATH'], Formula['zstd'].opt_prefix if build.with? 'zstd'
+    args = [
+      "--prefix=#{prefix}",
+      '--disable-debug',
+      '--disable-dependency-tracking',
+      '--disable-silent-rules',
+      '--program-prefix=g',
+      '--enable-64-bit-bfd',  # Without this, even “all targets” can’t do 64‐bit ones.
+      '--enable-build-warnings',
+      '--enable-checking=all',
+      '--enable-colored-disassembly',  # Sets a default behaviour for objdump.
+      '--enable-compressed-debug-sections=all',
+      '--enable-deterministic-archives',  # Makes ar and ranlib default to -D behaviour.
+      '--enable-f-for-ifunc-symbols',  # Makes nm use F and f for global and local ifunc symbols.
+      '--enable-follow-debug-links',  # Makes readelf & objdump follow debug links by default.
+      '--enable-install-libbfd',
+      '--enable-plugins',
+      '--with-system-zlib',
+      '--enable-targets=all',  # Means “all target ISAs”, not “all makefile targets”.
+      '--enable-werror',
+    ]
+    args << '--disable-nls' if build.without? 'nls'
+    args << '--with-zstd' if build.with? 'zstd'
+    system './configure', *args
+    system 'make', 'all-binutils'
+    system 'make', 'check-binutils'
+    system 'make', 'install-binutils'
   end
 
   test do
