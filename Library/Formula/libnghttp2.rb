28789120c985248390f652aad176409b03e5adaf
Gordon Steemson <gsteemso@gmail.com>
2024-11-18 22:24:32 -0800

    libnghttp2:  update scour+merge code/no “make check”/:universal “test”/"→'

--- Library/Formula/libnghttp2.rb
+++ Library/Formula/libnghttp2.rb
@@ -1,35 +1,31 @@
 class Libnghttp2 < Formula
-  desc "HTTP/2 C Library"
-  homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.62.0/nghttp2-1.62.1.tar.xz"
-  mirror "http://fresh-center.net/linux/www/nghttp2-1.62.1.tar.xz"
-  sha256 "2345d4dc136fda28ce243e0bb21f2e7e8ef6293d62c799abbf6f633a6887af72"
-  license "MIT"
-
-  bottle do
-    sha256 "b5561ecc87f62759d6915200d7a3bd9e6cebf97842a825116e5871a2a0952b7d" => :tiger_altivec
-  end
+  desc 'HTTP/2 C Library'
+  homepage 'https://nghttp2.org/'
+  url 'https://github.com/nghttp2/nghttp2/releases/download/v1.62.0/nghttp2-1.62.1.tar.xz'
+  mirror 'http://fresh-center.net/linux/www/nghttp2-1.62.1.tar.xz'
+  sha256 '2345d4dc136fda28ce243e0bb21f2e7e8ef6293d62c799abbf6f633a6887af72'
+  license 'MIT'
 
   head do
-    url "https://github.com/nghttp2/nghttp2.git", branch: "master"
+    url 'https://github.com/nghttp2/nghttp2.git', branch: 'master'
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
   end
 
   option :universal
 
-  depends_on "pkg-config" => :build
+  depends_on 'pkg-config' => :build
 
   # These used to live in `nghttp2`.
-  link_overwrite "include/nghttp2"
-  link_overwrite "lib/libnghttp2.a"
-  link_overwrite "lib/libnghttp2.dylib"
-  link_overwrite "lib/libnghttp2.14.dylib"
-  link_overwrite "lib/libnghttp2.so"
-  link_overwrite "lib/libnghttp2.so.14"
-  link_overwrite "lib/pkgconfig/libnghttp2.pc"
+  link_overwrite 'include/nghttp2'
+  link_overwrite 'lib/libnghttp2.a'
+  link_overwrite 'lib/libnghttp2.dylib'
+  link_overwrite 'lib/libnghttp2.14.dylib'
+  link_overwrite 'lib/libnghttp2.so'
+  link_overwrite 'lib/libnghttp2.so.14'
+  link_overwrite 'lib/pkgconfig/libnghttp2.pc'
 
   # Apple GCC thinks forward declarations in a different file are redefinitions
   patch :p1, :DATA
@@ -37,51 +33,38 @@ class Libnghttp2 < Formula
   def install
     if build.universal?
       ENV.permit_arch_flags if superenv?
-      ENV.un_m64 if Hardware::CPU.family == :g5_64
       archs = Hardware::CPU.universal_archs
-      mkdir 'arch-stashes'
-      dirs = []
+      stashdir = buildpath/'arch-stashes'
     else
       archs = [MacOS.preferred_arch]
-    end
+    end # universal?
 
-    bitness_stash = ENV['HOMEBREW_PREFER_64_BIT']
     archs.each do |arch|
-      if build.universal?
-        case arch
-          when :i386, :ppc then ENV.delete 'HOMEBREW_PREFER_64_BIT'
-          when :x86_64, :ppc64 then ENV['HOMEBREW_PREFER_64_BIT'] = '1'
-        end
-        ENV.setup_build_environment(self)
-        mkdir "arch-stashes/#{arch}-bin"
-      end
+      ENV.append_to_cflags "-arch #{arch}" if build.universal?
 
-      system "autoreconf", "-ivf" if build.head?
-      system "./configure", "--prefix=#{prefix}",
+      system 'autoreconf', '-ivf' if build.head?
+      system './configure', "--prefix=#{prefix}",
                             '--disable-dependency-tracking',
                             '--disable-silent-rules',
-                            "--enable-lib-only"
+                            '--enable-lib-only'
       cd 'lib' do
-        system "make"
-        system "make", "install"
-      end
-
-      if build.universal?
-        system 'make', 'clean'
-        Merge.scour_keg(prefix, "arch-stashes/#{arch}-bin")
-      end # universal?
+        system 'make'
+        # `make check` does nothing
+        system 'make', 'install'
+        if build.universal?
+          system 'make', 'distclean'
+          Merge.scour_keg(prefix, stashdir/"bin-#{arch}")
+          # undo architecture-specific tweak before next run
+          ENV.remove_from_cflags "-arch #{arch}"
+        end # universal?
+      end # cd 'lib'
     end # archs.each
 
-    if build.universal?
-      Merge.mach_o(prefix, 'arch-stashes', archs)
-      # undo architecture-specific tweak
-      ENV['HOMEBREW_PREFER_64_BIT'] = bitness_stash
-      ENV.setup_build_environment(self)
-    end # universal?
+    Merge.binaries(prefix, stashdir, archs) if build.universal?
   end # install
 
   test do
-    (testpath/"test.c").write <<~EOS
+    (testpath/'test.c').write <<-EOS.undent
       #include <nghttp2/nghttp2.h>
       #include <stdio.h>
 
@@ -91,61 +74,46 @@ class Libnghttp2 < Formula
         return 0;
       }
     EOS
-
     ENV.universal_binary if build.universal?
-    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lnghttp2", "-o", "test"
-    assert_equal version.to_s, shell_output("./test")
+    system ENV.cc, 'test.c', "-I#{include}", "-L#{lib}", '-lnghttp2', '-o', 'test'
+    for_archs './test' do |a|
+      arch_cmd = (a.nil? ? [] : ['arch', '-arch', "#{a.to_s} "])
+      assert_equal version.to_s, shell_output("#{arch_cmd * ' '}./test")
+    end
   end # test
 end # Libnghttp2
 
 class Merge
-  module Pathname_extension
-    def is_bare_mach_o?
-      # header word 0, magic signature:
-      #   MH_MAGIC    = 'feedface' – value with lowest‐order bit clear
-      #   MH_MAGIC_64 = 'feedfacf' – same value with lowest‐order bit set
-      # low‐order 24 bits of header word 1, CPU type:  7 is x86, 12 is ARM, 18 is PPC
-      # header word 3, file type:  no types higher than 10 are defined
-      # header word 5, net size of load commands, is far smaller than the filesize
-      if (self.file? and self.size >= 28 and mach_header = self.binread(24).unpack('N6'))
-        raise('Fat binary found where bare Mach-O file expected') if mach_header[0] == 0xcafebabe
-        ((mach_header[0] & 0xfffffffe) == 0xfeedface and
-          [7, 12, 18].detect { |item| (mach_header[1] & 0x00ffffff) == item } and
-          mach_header[3] < 11 and
-          mach_header[5] < self.size)
-      else
-        false
-      end
-    end unless method_defined?(:is_bare_mach_o?)
-  end # Pathname_extension
-
   class << self
     include FileUtils
 
-    def scour_keg(keg_prefix, stash, sub_path = '')
+    # The stash_root is expected to be a Pathname object.
+    # The keg_prefix and the sub_path are just strings.
+    def scour_keg(keg_prefix, stash_root, sub_path = '')
       # don’t suffer a double slash when sub_path is null:
       s_p = (sub_path == '' ? '' : sub_path + '/')
+      stash_p = stash_root/s_p
+      mkdir_p stash_p unless stash_p.directory?
       Dir["#{keg_prefix}/#{s_p}*"].each do |f|
-        pn = Pathname(f).extend(Pathname_extension)
+        pn = Pathname.new(f)
         spb = s_p + pn.basename
         if pn.directory?
-          Dir.mkdir "#{stash}/#{spb}"
-          scour_keg(keg_prefix, stash, spb)
+          scour_keg(keg_prefix, stash_root, spb)
         # the number of things that look like Mach-O files but aren’t is horrifying, so test
-        elsif ((not pn.symlink?) and pn.is_bare_mach_o?)
-          cp pn, "#{stash}/#{spb}"
-        end
-      end
-    end # scour_keg
+        elsif not(pn.symlink?) and (pn.mach_o_signature_at?(0) or pn.ar_sigseek_from 0)
+          cp pn, stash_root/spb
+        end # what is pn?
+      end # each pathname
+    end # Merge.scour_keg
 
-    # install_prefix expects a Pathname object, not just a string
-    def mach_o(install_prefix, stash_root, archs, sub_path = '')
+    # The keg_prefix is expected to be a Pathname object.  The rest are just strings.
+    def binaries(keg_prefix, stash_root, archs, sub_path = '')
       # don’t suffer a double slash when sub_path is null:
       s_p = (sub_path == '' ? '' : sub_path + '/')
       # generate a full list of files, even if some are not present on all architectures; bear in
       # mind that the current _directory_ may not even exist on all archs
       basename_list = []
-      arch_dirs = archs.map {|a| "#{a}-bin"}
+      arch_dirs = archs.map {|a| "bin-#{a}"}
       arch_dir_list = arch_dirs.join(',')
       Dir["#{stash_root}/{#{arch_dir_list}}/#{s_p}*"].map { |f|
         File.basename(f)
@@ -157,19 +125,19 @@ class Merge
         the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{stash_root}/#{ad}/#{spb}") }
         pn = Pathname("#{stash_root}/#{the_arch_dir}/#{spb}")
         if pn.directory?
-          mach_o(install_prefix, stash_root, archs, spb)
+          binaries(keg_prefix, stash_root, archs, spb)
         else
           arch_files = Dir["#{stash_root}/{#{arch_dir_list}}/#{spb}"]
           if arch_files.length > 1
-            system 'lipo', '-create', *arch_files, '-output', install_prefix/spb
+            system 'lipo', '-create', *arch_files, '-output', keg_prefix/spb
           else
             # presumably there's a reason this only exists for one architecture, so no error;
             # the same rationale would apply if it only existed in, say, two out of three
-            cp arch_files.first, install_prefix/spb
+            cp arch_files.first, keg_prefix/spb
           end # if > 1 file?
         end # if directory?
       end # each basename |b|
-    end # mach_o
+    end # Merge.binaries
   end # << self
 end # Merge
 
