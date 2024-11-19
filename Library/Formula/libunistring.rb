01a660f78ce6d998f8e8382ea296ae967dc38847
Gordon Steemson <gsteemso@gmail.com>
2024-11-18 22:23:39 -0800

    libunistring:  v1.2 → 1.3; `make check`

--- Library/Formula/libunistring.rb
+++ Library/Formula/libunistring.rb
@@ -1,14 +1,9 @@
 class Libunistring < Formula
   desc 'C string library for manipulating Unicode strings'
   homepage 'https://www.gnu.org/software/libunistring/'
-  url 'http://ftpmirror.gnu.org/libunistring/libunistring-1.2.tar.xz'
-  mirror 'https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.xz'
-  sha256 '632bd65ed74a881ca8a0309a1001c428bd1cbd5cd7ddbf8cedcd2e65f4dcdc44'
-
-  bottle do
-    cellar :any
-    sha256 '8074558a34abdb2121379dd2df80fc1572d62983f39e750b37e69f20cc8fa35c' => :tiger_altivec
-  end
+  url 'http://ftpmirror.gnu.org/libunistring/libunistring-1.3.tar.xz'
+  mirror 'https://ftp.gnu.org/gnu/libunistring/libunistring-1.3.tar.xz'
+  sha256 'f245786c831d25150f3dfb4317cda1acc5e3f79a5da4ad073ddca58886569527'
 
   option :universal
 
@@ -19,6 +14,8 @@ class Libunistring < Formula
                           '--disable-dependency-tracking',
                           '--disable-silent-rules'
     system 'make'
+    # `make check` hardcodes bit widths, causing failure with 32b/64b universal builds
+    system 'make', 'check' unless build.universal?
     system 'make', 'install'
   end
 end
