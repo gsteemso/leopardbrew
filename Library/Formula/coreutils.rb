b1ce1ca7f97537f415a188cf2762df61054cb3bb
Gordon Steemson <gsteemso@gmail.com>
2024-11-25 23:02:43 -0800

    coreutils:  gmp recommended, vs optional; patched some tests

--- Library/Formula/coreutils.rb
+++ Library/Formula/coreutils.rb
@@ -16,16 +16,19 @@ class Coreutils < Formula
     depends_on "wget" => :build
   end
 
-  option 'without-nls',   'Build without internationalization'
+  option 'without-gmp',   'Build with default (inferior) math handling'
+  option 'without-nls',   'Build without internationalization support'
   option 'without-tests', 'Build without running the unit tests'
 
   depends_on "gettext" if build.with? 'nls'
-  depends_on "gmp" => :optional
+  depends_on "gmp"     => :recommended
 
   conflicts_with "ganglia", :because => "both install `gstat` binaries"
   conflicts_with "gegl", :because => "both install `gcut` binaries"
   conflicts_with "idutils", :because => "both install `gid` and `gid.1`"
   conflicts_with "aardvark_shell_utils", :because => "both install `realpath` binaries"
+
+  patch :DATA
 
   def install
     if MacOS.version == :el_capitan
@@ -54,7 +57,7 @@ class Coreutils < Formula
     args << "--without-gmp" if build.without? "gmp"
     system "./configure", *args
     system "make"
-    system "make", "check" if build.with? 'tests'
+    bombproof_system "make", "check" if build.with? 'tests'
     system "make", "install"
 
     # Symlink all commands into libexec/gnubin without the 'g' prefix
@@ -71,15 +74,15 @@ class Coreutils < Formula
     man1.install_symlink "grealpath.1" => "realpath.1"
   end
 
-  def caveats; <<~EOS
-    All commands are installed with the prefix 'g'.
-    If you really need to use these commands with their normal names, you
-    can add the "gnubin" directory to your PATH from your bashrc:
-        PATH="#{opt_libexec}/gnubin:$PATH"
-    You can access their man pages with normal names if you also add the
-    "gnuman" directory to your MANPATH from your bashrc:
-        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
-  EOS
+  def caveats; <<-EOS.undent
+      All commands are installed with the prefix ‘g’.
+      If you really need to use these commands with their normal names, you
+      can add the “gnubin” directory to your PATH from your bashrc:
+          PATH="#{opt_libexec}/gnubin:$PATH"
+      You can likewise access their man pages with normal names if you also add
+      the “gnuman” directory to your MANPATH from your bashrc:
+          MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+    EOS
   end
 
   def coreutils_filenames(dir)
@@ -98,3 +101,34 @@ class Coreutils < Formula
     system bin/"gln", "-f", "test", "test.sha1"
   end
 end
+
+__END__
+--- old/tests/ls/dired.sh
++++ new/tests/ls/dired.sh
+@@ -37,13 +37,13 @@
+ 
+ # Check with varying positions (due to usernames etc.)
+ # Also use multibyte characters to show --dired counts bytes not characters
+-touch dir/1a dir/2á || framework_failure_
++touch dir/1a dir/2æ || framework_failure_
+ mkdir -p dir/3dir || framework_failure_
+ 
+ ls -l --dired dir > out || fail=1
+ 
+ dired_values=$(grep "//DIRED//" out| cut -d' ' -f2-)
+-expected_files="1a 2á 3dir"
++expected_files="1a 2æ 3dir"
+ 
+ dired_count=$(printf '%s\n' $dired_values | wc -l)
+ expected_count=$(printf '%s\n' $expected_files | wc -l)
+--- old/src/iopoll.c
++++ new/src/iopoll.c
+@@ -23,7 +23,7 @@
+    a readable event).  Also use poll(2) on systems we know work
+    and/or are already using poll (linux).  */
+ 
+-#if defined _AIX || defined __sun || defined __APPLE__ || \
++#if defined _AIX || defined __sun || \
+     defined __linux__ || defined __ANDROID__
+ # define IOPOLL_USES_POLL 1
+   /* Check we've not enabled gnulib's poll module
