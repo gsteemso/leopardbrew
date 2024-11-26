c6ed308d8d90c76b48250391dd57e7714d81e885
Gordon Steemson <gsteemso@gmail.com>
2024-11-25 23:04:52 -0800

    automake:  optional tests; patched a test; " → '

--- Library/Formula/automake.rb
+++ Library/Formula/automake.rb
@@ -1,34 +1,59 @@
 class Automake < Formula
-  desc "Tool for generating GNU Standards-compliant Makefiles"
-  homepage "https://www.gnu.org/software/automake/"
-  url "http://ftpmirror.gnu.org/automake/automake-1.16.5.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/automake/automake-1.16.5.tar.xz"
-  sha256 "f01d58cd6d9d77fbdca9eb4bbd5ead1988228fdb73d6f7a201f5f8d6b118b469"
+  desc 'Tool for generating GNU Standards-compliant Makefiles'
+  homepage 'https://www.gnu.org/software/automake/'
+  url 'http://ftpmirror.gnu.org/automake/automake-1.17.tar.xz'
+  mirror 'https://ftp.gnu.org/gnu/automake/automake-1.17.tar.xz'
+  sha256 '8920c1fc411e13b90bf704ef9db6f29d540e76d232cb3b2c9f4dc4cc599bd990'
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "8b0b74e874319bca40c049d01e0816845dfedae4a4607ee5cd91f23170f94996" => :tiger_altivec
+  option 'with-tests', 'Run the build‐time unit tests'
+
+  depends_on 'autoconf' => [:build, :run]
+
+  if build.with? 'tests'
+    depends_on :python
+    enhanced_by 'libtool'
   end
-
-  depends_on "autoconf"
 
   keg_only :provided_until_xcode43
 
-  def install
-    ENV["PERL"] = "/usr/bin/perl"
+  # Superenv argument refurbishment messes up the compiler‐flag jiggery‐pokery
+  # used by one of the `make check` tests.  This makes that test skip itself
+  # when argument refurbishment is active.
+  patch :DATA
 
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+  def install
+    ENV['PERL'] = '/usr/bin/perl'
+
+    system './configure', "--prefix=#{prefix}"
+    system 'make'
+    bombproof_system 'make', 'check' if build.with? 'tests'
+    system 'make', 'install'
 
     # Our aclocal must go first. See:
     # https://github.com/Homebrew/homebrew/issues/10618
-    (share/"aclocal/dirlist").write <<-EOS.undent
+    (share/'aclocal/dirlist').write <<-EOS.undent
       #{HOMEBREW_PREFIX}/share/aclocal
       /usr/share/aclocal
     EOS
   end
 
   test do
-    system "#{bin}/automake", "--version"
+    system bin/'automake', '--version'
   end
 end
+
+__END__
+--- old/t/amhello-cflags.sh
++++ new/t/amhello-cflags.sh
+@@ -22,6 +22,11 @@
+ required=gcc
+ . test-init.sh
+ 
++case "$HOMEBREW_CCCFG" in
++  *O*) skip_ 'Homebrew argument refurbishment precludes meaningful results' ;;
++  *) ;;
++esac
++
+ cp "$am_docdir"/amhello-1.0.tar.gz . \
+   || fatal_ "cannot get amhello tarball"
+ 
