061a677197184573114a3acaec1dd004e5568219
Gordon Steemson <gsteemso@gmail.com>
2024-12-04 21:18:15 -0800

    libtool:  update v.2.4.7 → 2.5.4; " → '

--- Library/Formula/libtool.rb
+++ Library/Formula/libtool.rb
@@ -1,40 +1,37 @@
-# Xcode 4.3 provides the Apple libtool.
-# This is not the same so as a result we must install this as glibtool.
-
 class Libtool < Formula
-  desc "Generic library support script"
-  homepage "https://www.gnu.org/software/libtool/"
-  url "http://ftpmirror.gnu.org/libtool/libtool-2.4.7.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/libtool/libtool-2.4.7.tar.xz"
-  sha256 "4f7f217f057ce655ff22559ad221a0fd8ef84ad1fc5fcb6990cecc333aa1635d"
-
-  bottle do
-    cellar :any
-    sha256 "438bff78afd08066e5393c71ddb35875d380016b6e7a1f6f8b381b3635951995" => :tiger_altivec
-  end
-
-  depends_on "m4" if MacOS.version < :leopard
+  desc 'Generic library support script'
+  homepage 'https://www.gnu.org/software/libtool/'
+  url 'http://ftpmirror.gnu.org/libtool/libtool-2.5.4.tar.xz'
+  mirror 'https://ftp.gnu.org/gnu/libtool/libtool-2.5.4.tar.xz'
+  sha256 'f81f5860666b0bc7d84baddefa60d1cb9fa6fceb2398cc3baca6afaa60266675'
 
   keg_only :provided_until_xcode43
 
   option :universal
+  option 'with-tests', 'Run the build‐time unit tests (very slow)'
+
+  depends_on 'autoconf' => :run
+  depends_on 'automake' => :run
+  depends_on 'gettext' if build.with? 'tests'
 
   def install
     ENV.universal_binary if build.universal?
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--program-prefix=g",
-                          "--enable-ltdl-install"
-    system "make", "install"
+    system './configure', "--prefix=#{prefix}",
+                          '--program-prefix=g',
+                          '--disable-dependency-tracking',
+                          '--enable-ltdl-install'
+    system 'make'
+    bombproof_system 'make', 'check' if build.with? 'tests'
+    system 'make', 'install'
   end
 
   def caveats; <<-EOS.undent
-    In order to prevent conflicts with Apple's own libtool we have prepended a "g"
-    so, you have instead: glibtool and glibtoolize.
+      In order to prevent conflicts with Apple’s stock libtool, we prepend a “g” to
+      get “glibtool” and “glibtoolize”.
     EOS
   end
 
   test do
-    system "#{bin}/glibtool", "execute", "/usr/bin/true"
+    system bin/'glibtool', 'execute', '/usr/bin/true'  # glibtool is a script – no archs!
   end
 end
