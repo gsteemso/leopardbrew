ea59ecee819e2d8339da1904f827609e31d776b9
Gordon Steemson <gsteemso@gmail.com>
2025-01-13 16:21:31 -0800

    cmd/desc:  added help text; " → '

--- Library/Homebrew/cmd/desc.rb
+++ Library/Homebrew/cmd/desc.rb
@@ -1,12 +1,29 @@
-require "descriptions"
-require "cmd/search"
+#:
+#:Search names and/or descriptions of formulæ, or simply show descriptions.
+#:
+#:Usage:
+#:
+#:    brew desc /flag/ /search term/
+#:
+#:    brew desc /formula/ [...]
+#:
+#:In the first form, /flag/ may be any one of “--name” (to search formula names),
+#:“--desc” (to search formula descriptions), or just “--search” (to search both).
+#:These may be abbreviated as “-n”, “-d”, or “-s”, respectively.
+#:
+#:In the second form, the description for each formula named on the command line
+#:is printed out.
+#:
+
+require 'descriptions'
+require 'cmd/search'
 
 module Homebrew
   def desc
     search_type = []
-    search_type << :either if ARGV.flag? "--search"
-    search_type << :name   if ARGV.flag? "--name"
-    search_type << :desc   if ARGV.flag? "--description"
+    search_type << :either if ARGV.flag? '--search'
+    search_type << :name   if ARGV.flag? '--name'
+    search_type << :desc   if ARGV.flag? '--description'
 
     if search_type.empty?
       raise FormulaUnspecifiedError if ARGV.named.empty?
@@ -15,14 +32,14 @@ module Homebrew
       results = Descriptions.new(desc)
       results.print
     elsif search_type.size > 1
-      odie "Pick one, and only one, of -s/--search, -n/--name, or -d/--description."
+      odie 'Pick one, and only one, of -s/--search, -n/--name, or -d/--description.'
     else
       if arg = ARGV.named.first
         regex = Homebrew::query_regexp(arg)
         results = Descriptions.search(regex, search_type.first)
         results.print
       else
-        odie "You must provide a search term."
+        odie 'You must provide a search term.'
       end
     end
   end
