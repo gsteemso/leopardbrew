aed8e9d6b8da2464b114d22d27045e1a4398ec47
Gordon Steemson <gsteemso@gmail.com>
2025-01-16 23:04:41 -0800

    extend/fileutils:  Add .cp_mkp(); delete .scons(); compact; endtags.

--- Library/Homebrew/extend/fileutils.rb
+++ Library/Homebrew/extend/fileutils.rb
@@ -10,15 +10,19 @@ class File
 end
 
 module FileUtils
+  def cp_mkp(src, dst, opts = {})
+    dp = File.dirname(dst)
+    mkdir_p(dp) unless File.exists?(dp)
+    cp_r src, dst, opts
+  end
+
   # Create a temporary directory then yield. When the block returns,
   # recursively delete the temporary directory.
   def mktemp(prefix = name)
     prev = pwd
     tmp  = Dir.mktmpdir(prefix, HOMEBREW_TEMP)
-
     begin
       cd(tmp)
-
       begin
         yield
       ensure
@@ -32,7 +36,6 @@ module FileUtils
 
   # @private
   alias_method :old_mkdir, :mkdir
-
   # A version of mkdir that also changes to that folder in a block.
   def mkdir(name, &_block)
     old_mkdir(name)
@@ -92,15 +95,8 @@ module FileUtils
     end
   end
 
-  # Run `scons` using a Homebrew-installed version rather than whatever is in the `PATH`.
-  def scons(*args)
-    system Formulary.factory("scons").opt_bin/"scons", *args
-  end
-
   # Run the `rake` from the `ruby` Homebrew is using rather than whatever is in the `PATH`.
-  def rake(*args)
-    system CONFIG_RUBY_BIN/"rake", *args
-  end
+  def rake(*args); system CONFIG_RUBY_BIN/"rake", *args; end
 
   # Run `make` 3.81 or newer.  Uses brewed make if available, or system make from Leopard onward.
   # Adapts as appropriate to stdenv/superenv (required for superenv argument refurbishment).
@@ -119,17 +115,12 @@ module FileUtils
     else
       system _make, *args
     end
-  end
+  end # make
 
-  if method_defined?(:ruby)
-    # @private
-    alias_method :old_ruby, :ruby
-  end
-
+  # @private
+  alias_method :old_ruby, :ruby if method_defined?(:ruby)
   # Run the `ruby` Homebrew is using rather than whatever is in the `PATH`.
-  def ruby(*args)
-    system CONFIG_RUBY_PATH, *args
-  end
+  def ruby(*args); system CONFIG_RUBY_PATH, *args; end
 
   # Run `xcodebuild` without Homebrew's compiler environment variables set.
   def xcodebuild(*args)
@@ -137,5 +128,5 @@ module FileUtils
     system "xcodebuild", *args
   ensure
     ENV.update(removed)
-  end
-end
+  end # xcodebuild
+end # FileUtils
