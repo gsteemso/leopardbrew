039f0dbeb5d3366ec8f32fc1cb20935e79cdb635
Gordon Steemson <gsteemso@gmail.com>
2025-01-24 23:23:16 -0800

    compat/hardware:  CPU#is_nn_bit? → #_nnb?; compact; endtags

--- Library/Homebrew/compat/hardware.rb
+++ Library/Homebrew/compat/hardware.rb
@@ -1,33 +1,27 @@
 class Hardware
   class << self
+    CPU = ::CPU
+
     # We won't change the name because of backward compatibility.
     # So disable rubocop here.
     def is_32_bit? # rubocop:disable Style/PredicateName
-      CPU.is_32_bit?
+      CPU._32b?
     end
 
     # We won't change the name because of backward compatibility.
     # So disable rubocop here.
     def is_64_bit? # rubocop:disable Style/PredicateName
-      CPU.is_64_bit?
+      CPU._64b?
     end
 
-    def bits
-      Hardware::CPU.bits
-    end
+    def bits; CPU.bits; end
 
-    def cpu_type
-      Hardware::CPU.type
-    end
+    def cpu_type; CPU.type; end
 
-    def cpu_family
-      Hardware::CPU.model
-    end
+    def cpu_family; CPU.model; end
     alias_method :intel_family, :cpu_family
     alias_method :ppc_family, :cpu_family
 
-    def processor_count
-      Hardware::CPU.cores
-    end
-  end
-end
+    def processor_count; CPU.cores; end
+  end # << self
+end # Hardware
