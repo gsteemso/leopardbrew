b0d67e5280d9a260f0904fbb11c5b1ed5021bcc5
Gordon Steemson <gsteemso@gmail.com>
2025-01-24 21:03:03 -0800

    libtasn1:  Simplify to use Merge library instead of inline code

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -1,3 +1,5 @@
+require 'merge'
+
 class Libtasn1 < Formula
   desc "ASN.1 structure parser library"
   homepage "https://www.gnu.org/software/libtasn1/"
@@ -13,7 +15,7 @@ class Libtasn1 < Formula
 
   def install
     if build.universal?
-      archs = Hardware::CPU.universal_archs
+      archs = CPU.local_archs
       stashdir = buildpath/'arch-stashes'
       the_binaries = %w[
         bin/asn1Coding
@@ -27,7 +29,7 @@ class Libtasn1 < Formula
     end # universal?
 
     archs.each do |arch|
-      ENV.append_to_cflags "-arch #{arch}" if build.universal?
+      ENV.set_build_archs(arch) if build.universal?
 
       system "./configure", "--prefix=#{prefix}",
                             "--disable-dependency-tracking",
@@ -38,12 +40,13 @@ class Libtasn1 < Formula
       if build.universal?
         system 'make', 'distclean'
         Merge.prep(prefix, stashdir/"bin-#{arch}", the_binaries)
-        # undo architecture-specific tweak before next run
-        ENV.remove_from_cflags "-arch #{arch}"
       end # universal?
-    end # archs.each
+    end # each |arch|
 
-    Merge.binaries(prefix, stashdir, archs) if build.universal?
+    if build.universal?
+      ENV.set_build_archs(archs)
+      Merge.binaries(prefix, stashdir, archs)
+    end # universal?
   end # install
 
   test do
@@ -77,67 +80,3 @@ class Libtasn1 < Formula
     end
   end # test
 end # Libtasn1
-
-class Merge
-  class << self
-    include FileUtils
-
-    # The destination is expected to be a Pathname object.
-    # The source is just a string.
-    def cp_mkp(source, destination)
-      if destination.exists?
-        if destination.is_directory?
-          cp source, destination
-        else
-          raise "File exists at destination:  #{destination}"
-        end # directory?
-      else
-        mkdir_p destination.parent unless destination.parent.exists?
-        cp source, destination
-      end # destination exists?
-    end # Merge.cp_mkp
-
-    # The keg_prefix and stash_root are expected to be Pathname objects.
-    # The list members are just strings.
-    def prep(keg_prefix, stash_root, list)
-      list.each do |item|
-        source = keg_prefix/item
-        dest = stash_root/item
-        cp_mkp source, dest
-      end # each binary
-    end # Merge.prep
-
-    # The keg_prefix is expected to be a Pathname object.  The rest are just strings.
-    def binaries(keg_prefix, stash_root, archs, sub_path = '')
-      # don’t suffer a double slash when sub_path is null:
-      s_p = (sub_path == '' ? '' : sub_path + '/')
-      # generate a full list of files, even if some are not present on all architectures; bear in
-      # mind that the current _directory_ may not even exist on all archs
-      basename_list = []
-      arch_dirs = archs.map {|a| "bin-#{a}"}
-      arch_dir_list = arch_dirs.join(',')
-      Dir["#{stash_root}/{#{arch_dir_list}}/#{s_p}*"].map { |f|
-        File.basename(f)
-      }.each { |b|
-        basename_list << b unless basename_list.count(b) > 0
-      }
-      basename_list.each do |b|
-        spb = s_p + b
-        the_arch_dir = arch_dirs.detect { |ad| File.exist?("#{stash_root}/#{ad}/#{spb}") }
-        pn = Pathname("#{stash_root}/#{the_arch_dir}/#{spb}")
-        if pn.directory?
-          binaries(keg_prefix, stash_root, archs, spb)
-        else
-          arch_files = Dir["#{stash_root}/{#{arch_dir_list}}/#{spb}"]
-          if arch_files.length > 1
-            system 'lipo', '-create', *arch_files, '-output', keg_prefix/spb
-          else
-            # presumably there's a reason this only exists for one architecture, so no error;
-            # the same rationale would apply if it only existed in, say, two out of three
-            cp arch_files.first, keg_prefix/spb
-          end # if > 1 file?
-        end # if directory?
-      end # each basename |b|
-    end # Merge.binaries
-  end # << self
-end # Merge
