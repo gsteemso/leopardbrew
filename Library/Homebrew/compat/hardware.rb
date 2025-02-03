c316d325473bdd35318f275669312e86dd5791f9
Gordon Steemson <gsteemso@gmail.com>
2025-02-02 21:02:52 -0800

    compat/hardware:  Add several items from cpu.rb

--- Library/Homebrew/compat/hardware.rb
+++ Library/Homebrew/compat/hardware.rb
@@ -1,7 +1,16 @@
 class Hardware
-  class << self
-    CPU = ::CPU
+  module CPU
+    def arch_32_bit;     CPU._32b_arch;   end
+    def arch_64_bit;     CPU._64b_arch;   end
+    def intel?;          CPU.intel?;      end
+    def is_32_bit?;      CPU._32b?;       end
+    def is_64_bit?;      CPU._64b?;       end
+    def ppc?;            CPU.powerpc?;    end
+    def type;            CPU.type;        end
+    def universal_archs; CPU.local_archs; end
+  end # Hardware⸬CPU
 
+  class << self
     # We won't change the name because of backward compatibility.
     # So disable rubocop here.
     def is_32_bit? # rubocop:disable Style/PredicateName
@@ -21,6 +30,8 @@ class Hardware
     def cpu_family; CPU.model; end
     alias_method :intel_family, :cpu_family
     alias_method :ppc_family, :cpu_family
+
+    def oldest_cpu; CPU.oldest; end
 
     def processor_count; CPU.cores; end
   end # << self
