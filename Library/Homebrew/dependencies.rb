1cbb3a9fb87a1d0bfc964bf3f19c1a3bab1949ba
Gordon Steemson <gsteemso@gmail.com>
2025-03-11 12:23:51 -0700

    dependencies.rb:  compact the code; endtags

--- Library/Homebrew/dependencies.rb
+++ Library/Homebrew/dependencies.rb
@@ -1,83 +1,52 @@
 class Dependencies
   include Enumerable
 
-  def initialize
-    @deps = []
-  end
+  def initialize; @deps = []; end
 
-  def each(*args, &block)
-    @deps.each(*args, &block)
-  end
+  def each(*args, &block); @deps.each(*args, &block); end
 
-  def <<(o)
-    @deps << o
-    self
-  end
+  def <<(o); @deps << o; self; end
 
-  def empty?
-    @deps.empty?
-  end
+  def empty?; @deps.empty?; end
 
-  def *(arg)
-    @deps * arg
-  end
+  def *(arg); @deps * arg; end
 
   alias_method :to_ary, :to_a
 
-  def optional
-    select(&:optional?)
-  end
+  def optional; select(&:optional?); end
 
-  def recommended
-    select(&:recommended?)
-  end
+  def recommended; select(&:recommended?); end
 
-  def build
-    select(&:build?)
-  end
+  def build; select(&:build?); end
 
-  def required
-    select(&:required?)
-  end
+  def required; select(&:required?); end
 
-  def default
-    build + required + recommended
-  end
+  def default; build + required + recommended; end
 
   attr_reader :deps
   protected :deps
 
-  def ==(other)
-    deps == other.deps
-  end
+  def ==(other); deps == other.deps; end
   alias_method :eql?, :==
 
-  def inspect
-    "#<#{self.class.name}: #{to_a.inspect}>"
-  end
-end
+  def inspect; "#<#{self.class.name}:  #{to_a.inspect}>"; end
+end # Dependencies
 
 class Requirements
   include Enumerable
 
-  def initialize
-    @reqs = Set.new
-  end
+  def initialize; @reqs = Set.new; end
 
-  def each(*args, &block)
-    @reqs.each(*args, &block)
-  end
+  def each(*args, &block); @reqs.each(*args, &block); end
 
   def <<(other)
-    if Comparable === other
-      @reqs.grep(other.class) do |req|
-        return self if req > other
-        @reqs.delete(req)
-      end
-    end
+    @reqs.grep(other.class) do |req|
+      return self if req > other
+      @reqs.delete(req)
+    end if Comparable === other
     @reqs << other
     self
-  end
+  end # Requirements#<<
 
   alias_method :to_ary, :to_a
-end
+end # Requirements
