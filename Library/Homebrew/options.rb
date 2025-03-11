df53c08d5fd948836de9244d14b812485ac750fc
Gordon Steemson <gsteemso@gmail.com>
2025-03-11 11:36:06 -0700

    options.rb:  compacted the code; minor streamlining; " → '

--- Library/Homebrew/options.rb
+++ Library/Homebrew/options.rb
@@ -1,36 +1,25 @@
-require "set"
+require 'set'
 
 class Option
   attr_reader :name, :description, :flag
 
-  def initialize(name, description = "")
+  def initialize(name, description = '')
     @name = name
     @flag = "--#{name}"
     @description = description
   end
 
-  def to_s
-    flag
-  end
+  def to_s; flag; end
 
-  def <=>(other)
-    return unless Option === other
-    name <=> other.name
-  end
+  def <=>(o); return unless Option === o; name <=> o.name; end
 
-  def ==(other)
-    instance_of?(other.class) and name == other.name
-  end
+  def ==(o); instance_of?(o.class) and name == o.name; end
   alias_method :eql?, :==
 
-  def hash
-    name.hash
-  end
+  def hash; name.hash; end
 
-  def inspect
-    "#<#{self.class.name}: #{flag.inspect}>"
-  end
-end
+  def inspect; "#<#{self.class.name}: #{flag.inspect}>"; end
+end # Option
 
 class DeprecatedOption
   attr_reader :old, :current
@@ -40,75 +29,41 @@ class DeprecatedOption
     @current = current
   end
 
-  def old_flag
-    "--#{old}"
-  end
+  def old_flag; "--#{old}"; end
 
-  def current_flag
-    "--#{current}"
-  end
+  def current_flag; "--#{current}"; end
 
-  def ==(other)
-    instance_of?(other.class) and old == other.old and current == other.current
-  end
+  def ==(o); instance_of?(o.class) and old == o.old and current == o.current; end
   alias_method :eql?, :==
-end
+end # DeprecatedOption
 
 class Options
   include Enumerable
 
-  def self.create(array)
-    new array.map { |e| Option.new(e[/^--([^=]+=?)(.+)?$/, 1] || e) }
-  end
+  def self.create(array); new array.map{ |e| Option.new(e[/^--([^=]+=?)(.+)?$/, 1] || e) }; end
 
-  def initialize(*args)
-    @options = Set.new(*args)
-  end
+  def initialize(*args); @options = Set.new(*args); end
 
-  def each(*args, &block)
-    @options.each(*args, &block)
-  end
+  def each(*args, &block); @options.each(*args, &block); end
 
-  def <<(o)
-    @options << o
-    self
-  end
+  def <<(o); @options << o; self; end
 
-  def +(o)
-    self.class.new(@options + o)
-  end
+  def -(o); self.class.new(@options - o); end
 
-  def -(o)
-    self.class.new(@options - o)
-  end
+  def &(o); self.class.new(@options & o); end
 
-  def &(o)
-    self.class.new(@options & o)
-  end
+  def |(o); self.class.new(@options | o); end
+  alias_method :+, :|
 
-  def |(o)
-    self.class.new(@options | o)
-  end
+  def *(arg); @options.to_a * arg; end
 
-  def *(arg)
-    @options.to_a * arg
-  end
+  def empty?; @options.empty?; end
 
-  def empty?
-    @options.empty?
-  end
+  def as_flags; map(&:flag); end
 
-  def as_flags
-    map(&:flag)
-  end
-
-  def include?(o)
-    any? { |opt| opt == o or opt.name == o or opt.flag == o }
-  end
+  def include?(other); any?{ |o| [o, o.name, o.flag].any?{ |opt| opt == other } }; end
 
   alias_method :to_ary, :to_a
 
-  def inspect
-    "#<#{self.class.name}: #{to_a.inspect}>"
-  end
-end
+  def inspect; "#<#{self.class.name}: #{to_a.inspect}>"; end
+end # Options
