6ae2b1d1c06158d3ba055f649fe9161a9bfb5871
Gordon Steemson <gsteemso@gmail.com>
2025-03-17 21:48:39 -0700

    “Tigerbrew” → “Leopardbrew”

--- Library/Homebrew/exceptions.rb
+++ Library/Homebrew/exceptions.rb
@@ -348,7 +348,7 @@ class OperationInProgressError < RuntimeError
   def initialize(name)
     message = <<-EOS.undent
         Operation already in progress for #{name}
-        Another active Tigerbrew process is already using #{name}.
+        Another active Leopardbrew process is already using #{name}.
         Please wait for it to finish or terminate it to continue.
       EOS
     super message
