7a6b31e1add8abccbb0d53329943966b3462c487
Gordon Steemson <gsteemso@gmail.com>
2025-04-24 21:29:10 -0700

    pcre2:  10.43 → 10.45; use CPU.powerpc?(); test all runnable built architectures; " → '

--- Library/Formula/pcre2.rb
+++ Library/Formula/pcre2.rb
@@ -1,31 +1,24 @@
 class Pcre2 < Formula
-  desc "Perl-compatible regular expressions library with revised API"
-  homepage "https://www.pcre.org/"
-  url "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.43/pcre2-10.43.tar.bz2"
-  sha256 "e2a53984ff0b07dfdb5ae4486bbb9b21cca8e7df2434096cc9bf1b728c350bcb"
+  desc 'Perl-compatible regular expressions library with revised API'
+  homepage 'https://www.pcre.org/'
+  url 'https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.45/pcre2-10.45.tar.bz2'
+  sha256 '21547f3516120c75597e5b30a992e27a592a31950b5140e7b8bfde3f192033c4'
 
   head do
-    url "https://github.com/PCRE2Project/pcre2"
+    url 'https://github.com/PCRE2Project/pcre2'
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool"  => :build
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool'  => :build
   end
 
   option :universal
-
-  # Fix JIT support on Mac OSes before 11.0
-  # (previous patch for Tiger compatibility now incorporated upstream)
-  patch :p0 do
-   url "https://raw.githubusercontent.com/macports/macports-ports/master/devel/pcre/files/MAP_JIT.patch"
-   sha256 "abbf0ece0c75581d653d4556eee4c5d27ef4505a8a6298f79c7f87f4a72da49d"
-  end
 
   def install
     ENV.universal_binary if build.universal?
     ENV.deparallelize
 
-    system "./autogen.sh" if build.head?
+    system './autogen.sh' if build.head?
 
     args = %W[
       --disable-dependency-tracking
@@ -38,16 +31,16 @@ class Pcre2 < Formula
       --enable-pcre2test-libedit
     ]
     # PPC64 JIT is explicitly supported in the package’s source code, but for reasons yet to be
-    # determined, fails to build properly under Mac OS 10.5
-    args << "--enable-jit" unless Hardware::CPU.ppc? and MacOS.prefer_64_bit?
+    # determined, fails to build properly under Mac OS 10.5.
+    args << '--enable-jit' unless CPU.powerpc? and MacOS.prefer_64_bit?
 
-    system "./configure", *args
-    system "make"
-    system "make", "check"
-    system "make", "install"
-  end
+    system './configure', *args
+    system 'make'
+    system 'make', 'check'
+    system 'make', 'install'
+  end # install
 
   test do
-    system bin/"pcre2grep", "regular expression", share/"doc/pcre2/README"
+    arch_system bin/'pcre2grep', 'regular expression', share/'doc/pcre2/README'
   end
-end
+end # Pcre2
