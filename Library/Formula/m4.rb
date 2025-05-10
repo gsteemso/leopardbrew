5242561bfbcb9b410eb52e234be58623736fe896
Gordon Steemson <gsteemso@gmail.com>
2025-05-10 09:06:02 -0700

    m4:  update v1.4.18 → 1.4.19; " → '

--- Library/Formula/m4.rb
+++ Library/Formula/m4.rb
@@ -1,26 +1,20 @@
 class M4 < Formula
-  desc "Macro processing language"
-  homepage "https://www.gnu.org/software/m4"
-  url "http://ftpmirror.gnu.org/m4/m4-1.4.18.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz"
-  sha256 "f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07"
-
-  bottle do
-    cellar :any_skip_relocation
-    revision 1
-    sha256 "a6780807d2598f2e36d923d675262505d097954900ce7293e617aafe8e4b9075" => :tiger_g4e
-  end
+  desc 'Macro processing language'
+  homepage 'https://www.gnu.org/software/m4'
+  url 'http://ftpmirror.gnu.org/m4/m4-1.4.19.tar.xz'
+  mirror 'https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.xz'
+  sha256 '63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96'
 
   keg_only :provided_by_osx
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make"
-    system "make", "install"
+    system './configure', '--disable-dependency-tracking', "--prefix=#{prefix}"
+    system 'make'
+    system 'make', 'install'
   end
 
   test do
-    assert_match "Homebrew",
+    assert_match 'Homebrew',
       pipe_output("#{bin}/m4", "define(TEST, Homebrew)\nTEST\n")
   end
 end
