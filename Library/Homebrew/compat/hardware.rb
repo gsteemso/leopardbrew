48c75a76b95dc29566fb30da7dc92620dcd3ac18
Gordon Steemson <gsteemso@gmail.com>
2025-05-09 23:00:57 -0700

    compat/hardware:  “CPU” → “::CPU”, so it actually works

--- Library/Homebrew/compat/hardware.rb
+++ Library/Homebrew/compat/hardware.rb
@@ -1,38 +1,39 @@
 class Hardware
   module CPU
-    def arch_32_bit;     CPU._32b_arch;   end
-    def arch_64_bit;     CPU._64b_arch;   end
-    def intel?;          CPU.intel?;      end
-    def is_32_bit?;      CPU._32b?;       end
-    def is_64_bit?;      CPU._64b?;       end
-    def ppc?;            CPU.powerpc?;    end
-    def type;            CPU.type;        end
-    def universal_archs; CPU.local_archs; end
+    extend self;
+    def arch_32_bit;     ::CPU._32b_arch;   end
+    def arch_64_bit;     ::CPU._64b_arch;   end
+    def intel?;          ::CPU.intel?;      end
+    def is_32_bit?;      ::CPU._32b?;       end
+    def is_64_bit?;      ::CPU._64b?;       end
+    def ppc?;            ::CPU.powerpc?;    end
+    def type;            ::CPU.type;        end
+    def universal_archs; ::CPU.local_archs; end
   end # Hardware⸬CPU
 
   class << self
     # We won't change the name because of backward compatibility.
     # So disable rubocop here.
     def is_32_bit? # rubocop:disable Style/PredicateName
-      CPU._32b?
+      ::CPU._32b?
     end
 
     # We won't change the name because of backward compatibility.
     # So disable rubocop here.
     def is_64_bit? # rubocop:disable Style/PredicateName
-      CPU._64b?
+      ::CPU._64b?
     end
 
-    def bits; CPU.bits; end
+    def bits; ::CPU.bits; end
 
-    def cpu_type; CPU.type; end
+    def cpu_type; ::CPU.type; end
 
-    def cpu_family; CPU.model; end
+    def cpu_family; ::CPU.model; end
     alias_method :intel_family, :cpu_family
     alias_method :ppc_family, :cpu_family
 
-    def oldest_cpu; CPU.oldest; end
+    def oldest_cpu; ::CPU.oldest; end
 
-    def processor_count; CPU.cores; end
+    def processor_count; ::CPU.cores; end
   end # << self
 end # Hardware
