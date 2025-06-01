4c99cde80d0dcf455586c65b1ac3373c55c09bd8
Gordon Steemson <gsteemso@gmail.com>
2025-06-01 00:19:11 -0700

    perl:  fix for latest Merge update; → v5.40.2/5.41.12; fix my Darwin-test patch

--- Library/Formula/p/perl.rb
+++ Library/Formula/p/perl.rb
@@ -1,18 +1,14 @@
 require 'merge'
 
 class Perl < Formula
-  extend Merge;
+  include Merge
 
   desc 'Highly capable, feature-rich programming language'
   homepage 'https://www.perl.org/'
   url 'https://www.cpan.org/src/5.0/perl-5.40.2.tar.xz'
-  sha256 '10d4647cfbb543a7f9ae3e5f6851ec49305232ea7621aed24c7cfbb0bef4b70d'
+  sha256 '0551c717458e703ef7972307ab19385edfa231198d88998df74e12226abf563b'
 
   head 'https://github.com/Perl/perl5.git', :branch => 'blead'
-
-  bottle do
-    sha256 '0743dbdaa87cc72cc5f206ade56c68d4f5e2ebacad8f047872b8c3827bfa724c' => :tiger_altivec
-  end
 
   devel do
     url 'https://www.cpan.org/src/5.0/perl-5.41.12.tar.xz'
@@ -20,27 +16,30 @@ class Perl < Formula
   end
 
   keg_only :provided_by_osx,
-    'OS X ships Perl and overriding that can cause unintended issues'
+    'Mac OS ships Perl and overriding that can cause unintended issues.'
 
   option :universal
-  option 'with-dtrace', 'Build with DTrace probes' if (MacOS.version >= :leopard and not MacOS.prefer_64_bit?) \
-                                                      or MacOS.version >= :lion
-  option 'with-tests', 'Run the build-time unit tests (fails on ppc64 when built with older GCCs)'
+  option 'with-dtrace', 'Build with DTrace probes' if (MacOS.version >= '10.5' and not MacOS.prefer_64_bit?) \
+                                                      or MacOS.version >= '10.7'
+  option 'with-tests', 'Run the build-time unit tests (fails on 64‐bit when built with older compilers)'
 
   enhanced_by 'curl'  # The obsolete stock curl on older Mac OSes causes
                       # extension modules reliant on it to fail messily.
 
-  if (build.with?('tests') or build.bottle?) and (build.universal? or
-                                                     (MacOS.prefer_64_bit? and Hardware::CPU.ppc?))
-    fails_with :gcc
-    fails_with :gcc_4_0
+  if (build.with?('tests') or build.bottle?) and
+    (build.universal? or MacOS.prefer_64_bit?) and
+    not ARGV.force?
+      fails_with [:gcc, :gcc_4_0, :llvm]
   end
 
-  # installperl:  .packlist files are sometimes created without write permissions (undocumented)
+  # installperl:  rarely, .packlist files are created without write permissions (undocumented)
   # t/04-xs-rpath-darwin.t:  Need Darwin 9 minimum
   #   see https://github.com/Perl-Toolchain-Gang/ExtUtils-MakeMaker/pull/446
-  # (same file):  Dummy library build needs to match bit width(s) of Perl build (undocumented)
+  # (same file):  Dummy library build needs to match bit width of Perl build (undocumented)
   patch :DATA unless build.head?
+
+  # shared location for pure‐Perl extensions
+  def site_perl; HOMEBREW_PREFIX/'site_perl'; end
 
   def install
     if build.universal?
@@ -49,8 +48,6 @@ class Perl < Formula
       archs = [MacOS.preferred_arch]
     end # universal?
 
-    # set installation directories for pure‐Perl extensions to the shared
-    # location $HOMEBREW_PREFIX/site_perl
     args = %W[
       -des
       -Dprefix=#{prefix}
@@ -60,12 +57,12 @@ class Perl < Formula
       -Dman1dir=#{man1}
       -Dman3dir=#{man3}
       -Dman3ext=3pl
-      -Dsitebin=#{HOMEBREW_PREFIX}/site_perl/bin
-      -Dsitescript=#{HOMEBREW_PREFIX}/site_perl/bin
-      -Dsitelib=#{HOMEBREW_PREFIX}/site_perl/lib
+      -Dsitebin=#{site_perl}/bin
+      -Dsitescript=#{site_perl}/bin
+      -Dsitelib=#{site_perl}/lib
       -Dsitearch=#{lib}
-      -Dsiteman1dir=#{HOMEBREW_PREFIX}/site_perl/man1
-      -Dsiteman3dir=#{HOMEBREW_PREFIX}/site_perl/man3
+      -Dsiteman1dir=#{site_perl}/man1
+      -Dsiteman3dir=#{site_perl}/man3
       -Dperladmin=none
       -Dstartperl='\#!#{opt_bin}/perl'
       -Duseshrplib
@@ -83,34 +80,32 @@ class Perl < Formula
       if arch.to_s =~ %r{64} then arch_args << '-Duse64bitall'
       elsif CPU._64b? then arch_args << '-Duse64bitint'; end
 
-      system './Configure', *args, *arch_args
-      system 'make'
-      system 'make', 'test' if build.with?('tests') or build.bottle?
-      system 'make', 'install'
+      Homebrew.system './Configure', *args, *arch_args
+      Homebrew.system 'make'
+      Homebrew.system 'make', 'test' rescue nil if build.with?('tests') or build.bottle?
+      Homebrew.system 'make', 'install'
 
       if build.universal?
-        ENV.deparallelize { system 'make', 'veryclean' }
-        Merge.scour_keg(prefix, stashdir/"bin-#{arch}")
+        ENV.deparallelize{ system 'make', 'veryclean' }
+        scour_keg(arch)
       end # universal?
     end # each |arch|
 
     if build.universal?
       ENV.set_build_archs(archs)
-      Merge.binaries(prefix, stashdir, archs)
+      merge_binaries(archs)
     end # universal?
   end # install
 
   def caveats; <<-EOS.undent
-      By default Perl installs modules in your HOME dir. If this is an issue run:
-        `#{bin}/cpan o conf init`
-      and tell it to put them in, for example, #{HOMEBREW_PREFIX}/site_perl instead.
+      By default Perl installs modules in your HOME dir.  If this is an issue, run:
+          `#{bin}/cpan o conf init`
+      and tell it to put them in, for example, #{site_perl} instead.
 
-      Perl will take advantage of brewed cURL, if it is present.  If it is _not_
-      present, the system `curl` will be used by those extension modules that require
-      it; on older Mac OSes, this will fail messily in use, due to its obsolescence.
-      (Due to a circular dependency, a newer cURL cannot be automatically brewed for
-      you.  If you have extension modules which require a newer cURL, it must be
-      brewed separately, and then Perl reïnstalled.)
+      If brewed cURL is not present, the system `curl` will be used by those extension
+      modules that require it; on older Mac OSes, this will fail messily in use due to
+      its extreme obsolescence.  (Due to a circular dependency, a newer cURL cannot be
+      automatically brewed.)
     EOS
   end # caveats
 
@@ -166,17 +161,18 @@ __END__
          plan skip_all => 'Not darwin platform';
      }
 +    elsif ($^O eq 'darwin' && $osmajmin < 9) {
-+	plan skip_all => 'For OS X Leopard and newer'
++        plan skip_all => 'For Mac OS Leopard and newer'
 +    }
      else {
          plan skip_all => 'Dynaloading not enabled'
              if !$Config{usedl} or $Config{usedl} ne 'define';
-@@ -195,7 +199,7 @@
+@@ -195,7 +199,8 @@
      my $libext = $Config{so};
  
      my $libname = 'lib' . $self->{mylib_lib_name} . '.' . $libext;
 -    my @cmd = ($cc, '-I.', '-dynamiclib', '-install_name',
-+    my @cmd = ($cc, split(' ', $ENV{'CFLAGS'}), '-I.', '-dynamiclib', '-install_name',
++    my $m64 = defined $Config::Config{use64bitall};
++    my @cmd = ($cc, $m64 ? '-m64' : '-m32', '-I.', '-dynamiclib', '-install_name',
               '@rpath/' . $libname,
               'mylib.c', '-o', $libname);
      _run_system_cmd(\@cmd);
