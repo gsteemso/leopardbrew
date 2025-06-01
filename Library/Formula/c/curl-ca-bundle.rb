c65fad795b43d8c02569b2ae790521a81ea92c52
Gordon Steemson <gsteemso@gmail.com>
2025-06-01 00:39:14 -0700

    curl-ca-bundle:  update to v. 2025-05-20

--- Library/Formula/c/curl-ca-bundle.rb
+++ Library/Formula/c/curl-ca-bundle.rb
@@ -1,9 +1,9 @@
 class CurlCaBundle < Formula
   desc 'Modern certificate-authority bundle from the Curl project'
   homepage 'http://curl.haxx.se/docs/caextract.html'
-  url 'https://curl.se/ca/cacert-2025-02-25.pem', :using => :nounzip
-  version '2025-02-25'
-  sha256 '50a6277ec69113f00c5fd45f09e8b97a4b3e32daa35d3a95ab30137a55386cef'
+  url 'https://curl.se/ca/cacert-2025-05-20.pem', :using => :nounzip
+  version '2025-05-20'
+  sha256 'ab3ee3651977a4178a702b0b828a4ee7b2bbb9127235b0ab740e2e15974bf5db'
 
   bottle do
     cellar :any
