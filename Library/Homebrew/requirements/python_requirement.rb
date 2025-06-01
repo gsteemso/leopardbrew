6389ab01c5a1b37c8a2d43faf720d1b582218994
Gordon Steemson <gsteemso@gmail.com>
2025-06-01 00:33:55 -0700

    reqs/python_req:  de‐contort logic flow; compact code; endtags; " → '

--- Library/Homebrew/requirements/python_requirement.rb
+++ Library/Homebrew/requirements/python_requirement.rb
@@ -1,9 +1,9 @@
-require "language/python"
+require 'language/python'
 
 class PythonRequirement < Requirement
   fatal true
-  default_formula "python"
-  cask "python"
+  default_formula 'python'
+  cask 'python'
 
   satisfy :build_env => false do
     python = which_python
@@ -11,56 +11,52 @@ class PythonRequirement < Requirement
     version = python_short_version
     next unless version
     # Always use Python 2.7 for consistency on older versions of OSX.
-    version == Version.new("2.7")
+    version == Version.new('2.7')
   end
 
   env do
     short_version = python_short_version
 
-    if !system_python? && short_version == Version.new("2.7")
-      ENV.prepend_path "PATH", which_python.dirname
+    if short_version == Version.new('2.7')
+      ENV.prepend_path 'PATH', which_python.dirname unless system_python?
     # Homebrew Python should take precedence over older Pythons in the PATH
-    elsif short_version != Version.new("2.7")
-      ENV.prepend_path "PATH", Formula["python"].opt_bin
-    end
+    else ENV.prepend_path 'PATH', Formula['python'].opt_bin; end
 
-    ENV["PYTHONPATH"] = "#{HOMEBREW_PREFIX}/lib/python#{short_version}/site-packages"
-  end
+    ENV['PYTHONPATH'] = "#{HOMEBREW_PREFIX}/lib/python#{short_version}/site-packages"
+  end # env
 
   def python_short_version
     @short_version ||= Language::Python.major_minor_version which_python
   end
 
   def which_python
-    python = which python_binary
-    return unless python
-    Pathname.new Utils.popen_read(python, "-c", "import sys; print(sys.executable)").strip
+    @which_python ||= if (python = which python_binary)
+        Pathname.new Utils.popen_read(python, '-c', 'import sys; print(sys.executable)').strip
+      end
   end
 
-  def system_python
-    "/usr/bin/#{python_binary}"
-  end
+  def system_python; "/usr/bin/#{python_binary}"; end
 
-  def system_python?
-    system_python == which_python.to_s
-  end
+  def system_python?; system_python == which_python.to_s; end
 
-  def python_binary
-    "python"
-  end
+  def python_binary; 'python'; end
 
   # Deprecated
   alias_method :to_s, :python_binary
-end
+end # PythonRequirement
 
 class Python3Requirement < PythonRequirement
   fatal true
-  default_formula "python3"
-  cask "python3"
+  default_formula 'python3'
+  cask 'python3'
 
-  satisfy(:build_env => false) { which_python }
+  satisfy(:build_env => false) { which_python3 }
 
-  def python_binary
-    "python3"
+  def which_python3
+    @which_python3 ||= if (python = which python_binary)
+        Pathname.new Utils.popen_read(python, '-c', 'import sys; print(sys.executable)').strip
+      end
   end
-end
+
+  def python_binary; 'python3'; end
+end # Python3Requirement
