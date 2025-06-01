12caa65d526ddc955740532e480afa4ee8477a14
Gordon Steemson <gsteemso@gmail.com>
2025-06-01 00:25:21 -0700

    cmd/uninstall:  uninsinuate correctly; compact code; tweak messaging

--- Library/Homebrew/cmd/uninstall.rb
+++ Library/Homebrew/cmd/uninstall.rb
@@ -11,15 +11,18 @@ module Homebrew
         was_linked = keg.linked?
         keg.lock do
           puts "Uninstalling #{keg}... (#{keg.abv})"
+          if f = attempt_from_keg(keg)
+            f.unpin rescue nil
+            f.uninsinuate if f.uninsinuate_defined?  # Repeat this later, so helper scripts can
+                                                     # self‐delete if the rack has gone.
+          end
           keg.unlink
           keg.uninstall  # this also deletes the whole rack, if it’s empty
-          if f = attempt_from_keg(keg) then f.unpin rescue nil; end
-          f.uninsinuate if f  # Do after the rack has gone, so helper scripts can self‐delete.
           rack = keg.rack
           if rack.directory?
             if (dirs = rack.subdirs) != []
               # hook up the next keg in line
-              next_keg = dirs.map { |d| Keg.new(d) }.max_by(&:version)
+              next_keg = dirs.map{ |d| Keg.new(d) }.max_by(&:version)
               next_keg.optlink
               next_keg.link if was_linked
               if f = attempt_from_keg(next_keg) then f.insinuate; end
@@ -35,42 +38,33 @@ module Homebrew
         end # keg lock
       end # each ARGV |keg|
     else # --force in effect; remove all versions
-      ARGV.named.each do |name|
-        if name =~ VERSIONED_NAME_REGEX then name = $1; end  # nuking ’em all; ignore given version
-        rack = Formulary.to_rack(name)
-        name = rack.basename
-        if f = attempt_from_rack(rack) then f.unpin rescue nil; end
+      ARGV.racks.each do |rack|
+        if f = attempt_from_rack(rack)
+          f.unpin rescue nil
+          f.uninsinuate if f.uninsinuate_defined?  # Repeat this later, so helper scripts can self‐
+                                                   # delete if the rack has gone.
+        end
         if rack.directory?
-          puts "Uninstalling #{name}... (#{rack.abv})"
+          puts "Uninstalling #{rack.basename}... (#{rack.abv})"
           rack.subdirs.each do |d|
             keg = Keg.new(d)
             keg.unlink
             keg.uninstall  # this also deletes the whole rack when it’s empty
           end
         end
-        f.uninsinuate if f  # Do after the rack has gone, so helper scripts can self‐delete.
       end
     end # --force?
+    f.uninsinuate if f and f.uninsinuate_defined?  # This repetition lets helper scripts self‐
+                                                   # delete if the rack has gone.
   rescue MultipleVersionsInstalledError => e
     ofail e
     puts "Use `brew uninstall --force #{e.name}` to remove all versions."
   ensure
-    # If we delete Cellar/newname, then Cellar/oldname symlink
-    # can become broken and we have to remove it.
-    HOMEBREW_CELLAR.children.each do |rack|
-      rack.unlink if rack.symlink? and not rack.resolved_path_exists?
-    end
+    # If we delete Cellar/newname and Cellar/oldname symlink breaks, we have to remove it.
+    HOMEBREW_CELLAR.children.each{ |r| r.unlink if r.symlink? and not r.resolved_path_exists? }
   end # uninstall
 
-  def attempt_from_keg(k)
-    Formulary.from_keg(k)
-  rescue FormulaUnavailableError
-    return nil
-  end
+  def attempt_from_keg(k); Formulary.from_keg(k); rescue FormulaUnavailableError; return nil; end
 
-  def attempt_from_rack(r)
-    Formulary.from_rack(r)
-  rescue FormulaUnavailableError
-    return nil
-  end
+  def attempt_from_rack(r); Formulary.from_rack(r); rescue FormulaUnavailableError; return nil; end
 end # Homebrew
