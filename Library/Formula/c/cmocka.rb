689177220206ba2c5bbaa7efe705f9f1915505bb
Gordon Steemson <gsteemso@gmail.com>
2025-06-24 22:13:33 -0700

    cmocka:  v1.0.1 → 1.1.7; use the unit tests it asks for; test block for all archs; " → '

--- Library/Formula/c/cmocka.rb
+++ Library/Formula/c/cmocka.rb
@@ -1,29 +1,25 @@
 class Cmocka < Formula
-  desc "Unit testing framework for C"
-  homepage "https://cmocka.org/"
-  url "https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/c/cmocka/cmocka_1.0.1.orig.tar.xz"
-  sha256 "b36050d7a1224296803d216cba1a9d4c58c31bf308b2d6d6649d61aa5a36753b"
+  desc 'Unit testing framework for C'
+  homepage 'https://cmocka.org/'
+  url 'https://cmocka.org/files/1.1/cmocka-1.1.7.tar.xz'
+  sha256 '810570eb0b8d64804331f82b29ff47c790ce9cd6b163e98d47a4807047ecad82'
 
-  bottle do
-    cellar :any
-    sha256 "33765424588cf149679e394842f6132dbb003913e774bc30d0115294952c3cad" => :yosemite
-    sha256 "28ea3d6de51e920dae544f9b4f36288a04f1ee6215dd34f833c6e98bc43de0f9" => :mavericks
-    sha256 "544f9a2bd42b2a868322cc9942afb191733260bb33e8a2731a53311b6c538d7d" => :mountain_lion
-  end
+  option :universal
 
-  depends_on "cmake" => :build
+  depends_on 'cmake' => :build
 
   def install
-    mkdir "build" do
-      system "cmake", "..", "-DUNIT_TESTING=On", *std_cmake_args
-      system "make"
-      system "make", "install"
+    ENV.universal_binary if build.universal?
+    mkdir 'build' do
+      system 'cmake', '..', '-DUNIT_TESTING=On', *std_cmake_args
+      system 'make'
+      system 'make', 'test'
+      system 'make', 'install'
     end
-  end
+  end # install
 
   test do
-    (testpath/"test.c").write <<-EOS.undent
+    (testpath/'test.c').write <<-EOS.undent
       #include <stdarg.h>
       #include <stddef.h>
       #include <setjmp.h>
@@ -40,7 +36,7 @@ class Cmocka < Formula
         return cmocka_run_group_tests(tests, NULL, NULL);
       }
     EOS
-    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lcmocka", "-o", "test"
-    system "./test"
-  end
-end
+    system ENV.cc, 'test.c', "-I#{include}", "-L#{lib}", '-lcmocka', '-o', 'test'
+    arch_system './test'
+  end # test
+end # Cmocka
