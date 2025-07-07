b7e4a80e30435a4226d84846348878960519b99c
Gordon Steemson <gsteemso@gmail.com>
2025-07-06 22:45:45 -0700

    libxml2:  2.12.7 → 2.14.4; " → '; Python 2 _&_ 3

--- Library/Formula/l/libxml2.rb
+++ Library/Formula/l/libxml2.rb
@@ -1,66 +1,97 @@
 class Libxml2 < Formula
-  desc "GNOME XML library"
-  homepage "http://xmlsoft.org"
-  url "https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.7.tar.xz"
-  sha256 "24ae78ff1363a973e6d8beba941a7945da2ac056e19b53956aeb6927fd6cfb56"
-
-  head do
-    url "https://git.gnome.org/browse/libxml2", :using => :git
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
-  option :universal
-  option 'with-python', 'Build with Python 2.7 language bindings'
-
-  depends_on 'python' => :optional
-  depends_on 'python3' if build.with? 'python'
-  depends_on 'libiconv'
-  depends_on "xz"
-  depends_on "zlib"
-  depends_on 'pkg-config' => :build
+  desc 'C XML library developed for GNOME'
+  homepage 'https://gitlab.gnome.org/GNOME/libxml2/-/wikis/home'
+  url 'https://download.gnome.org/sources/libxml2/2.14/libxml2-2.14.4.tar.xz'
+  sha256 '24175ec30a97cfa86bdf9befb7ccf4613f8f4b2713c5103e0dd0bc9c711a2773'
 
   keg_only :provided_by_osx
 
-  def install
-    ENV.deparallelize
-    ENV.universal_binary if build.universal?
-    if build.head?
-      inreplace "autogen.sh", "libtoolize", "glibtoolize"
-      system "./autogen.sh"
-    end
+  head do
+    url 'https://gitlab.gnome.org/GNOME/libxml2.git'
 
-    args = %W[
-      --prefix=#{prefix}
-      --disable-dependency-tracking
-      --without-debug
-      --with-iconv=#{Formula['libiconv'].opt_prefix}
-      --with-lzma=#{Formula["xz"].opt_prefix}
-      --with-zlib=#{Formula["zlib"].opt_prefix}
-    ]
-    # the package builds the python bindings by default
-    args << "--without-python" if build.without? "python"
-
-    system "./configure", *args
-    inreplace ['Makefile', 'python/Makefile'], '-lpython2.7', '-undefined dynamic_lookup'
-    system "make"
-    system "make", "check" if (build.without?('python') or
-                               (build.universal? == Tab.for_name('python').universal?) )
-    system "make", "install"
-
-    if build.with? "python"
-      cd "python" do
-        # We need to insert our include dir first
-        inreplace "setup.py", "includes_dir = [", "includes_dir = ['#{include}', '#{MacOS.sdk_path}/usr/include',"
-        system "python3", "setup.py", "install", "--prefix=#{prefix}"
-      end
-    end
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool'  => :build
   end
 
+  option :universal
+  option 'with-python', 'Build with Python 2.7 and Python 3 language bindings'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'readline'
+  depends_on 'xz'
+  depends_on :python => :optional
+  depends_on :python3 if build.with? 'python'
+
+  def install
+    ENV.universal_binary if build.universal?
+    ENV.delete 'PYTHONPATH'
+    mktemp do
+      maintemp = pwd
+      if build.head?
+        ENV['NOCONFIGURE'] = 'yes'
+        system "#{buildpath}/autogen.sh"
+      end
+      args = %W[
+          --prefix=#{prefix}
+          --disable-dependency-tracking
+          --disable-silent-rules
+          --without-debug
+          --with-history
+          --with-lzma=#{Formula['xz'].opt_prefix}
+          --with-readline
+          --enable-static
+          --with-zlib
+        ]
+#      args << '--with-docs' if build.head?  # docs require Doxygen
+      args << (build.with?('python') ? '--with-python' : '--without-python')
+      system "#{buildpath}/configure", *args
+      inreplace ['Makefile', 'python/Makefile'], '-lpython2.7', '-undefined dynamic_lookup' if build.with? 'python'
+      system 'make'
+      system 'make', 'check'
+      system 'make', 'install'
+      if build.with? 'python'
+        ENV.delete 'PYTHONPATH'
+        mktemp do
+          old_path = ENV['PATH']
+          begin
+            # replace the unversioned `python` 2 in $PATH with the unversioned `python` 3 in Python3/libexec/bin
+            ENV['PATH'] = ENV['PATH'].sub(Formula['python'].opt_bin.to_s, "#{Formula['python3'].opt_prefix}/libexec/bin")
+            system "#{buildpath}/configure", *args
+            Pathname.new(pwd).install_symlink_to "#{maintemp}/libxml2.la", "#{maintemp}/.libs"
+            system 'make', '-C', 'python'
+            system 'make', '-C', 'python', 'check'
+            system 'make', '-C', 'python', 'install'
+          ensure
+            ENV['PATH'] = old_path
+          end
+        end # secondary temporary directory
+      end # build with python?
+    end # main temporary directory
+  end # install
+
+  def post_install
+    if build.with? 'python'
+      # There are no system Python bindings to LibXML2, so we can install our own even though the
+      # library itself has to be keg‐only.
+      # Our Python will be missing if system Python was deemed adequate, but even if site_packages is
+      # not there, Pathname⸬binwrite will simply create it before writing to the file.
+      (Formula['python'].site_packages/'libxml2.pth').binwrite "#{opt_lib}/python2.7/site-packages\n"
+      py3 = Formula['python3']
+      (py3.site_packages/'libxml2.pth').binwrite "#{opt_lib}/python#{py3.xy}/site-packages\n"
+    end # build with python?
+  end # post_install
+
+  def caveats; <<-_.undent if build.with? 'python'
+      Python bindings for LibXML2 were installed.  The library itself already exists
+      in Mac OS, and needs to be kept aside; but Python bindings to it do not, and are
+      therefore not a problem.  The `libxml2.pth` file described in the automated
+      announcement below has already been written, and the one for Python 3 as well.
+    _
+  end # caveats
+
   test do
-    (testpath/"test.c").write <<-EOS.undent
+    (testpath/'test.c').write <<-EOS.undent
       #include <libxml/tree.h>
 
       int main()
@@ -72,10 +103,9 @@ class Libxml2 < Formula
         return 0;
       }
     EOS
-    ENV.universal_binary if build.universal?
     args = `#{bin}/xml2-config --cflags --libs`.split
     args += %w[test.c -o test]
     system ENV.cc, *args
-    system "./test"
+    arch_system './test'
   end
 end
