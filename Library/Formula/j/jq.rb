8b146b67a076fbbe8d4804321334e3b117b981a0
Gordon Steemson <gsteemso@gmail.com>
2025-07-09 14:56:17 -0700

    jq:  made :universal, no bison, no HEAD, " → '
    - Universal builds work fine, but you have to patch ./configure.  There is a
      heavily-used M4 macro out there somewhere that calls endianness "universal"
      when more than one -arch flag is present, even if they are both for platforms
      with the same endianness.  The fix is straightforward but a bit long.
    
    - Bison is only required in maintainer mode.  Set ./configure accordingly and
      removed the dependency.
    
    - HEAD (and indeed any recent version of JQ) segfaults as soon as it finishes
      verifying its command line, at least on Leopard PPC.  Removed the option; it
      can always be put back if someone figures out a patch for newer versions.

--- Library/Formula/j/jq.rb
+++ Library/Formula/j/jq.rb
@@ -1,38 +1,102 @@
 class Jq < Formula
-  desc "Lightweight and flexible command-line JSON processor"
-  homepage "https://stedolan.github.io/jq/"
-  url "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz"
-  sha256 "c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c"
+  desc 'Lightweight and flexible command-line JSON processor'
+  homepage 'https://jqlang.org/'
+  url 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz'
+  sha256 'c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c'
 
   bottle do
     cellar :any
     revision 1
-    sha256 "d969487931abc27767a3435f5a1b2d06ed61aab0916de187ed894b6137baceee" => :el_capitan
-    sha256 "8529bc1edac66bdeec82afe80ce671b9d015b02959fe9f37efd2887fd975faf1" => :yosemite
-    sha256 "89a32fb53e7f4330d6db84ba526133228189ea3ba3b15adf7fc743787c8ef645" => :mavericks
-    sha256 "d817dec8745f52802b4ac2fbcd2a7a76a647b2000f43ba9a842f59a4363da55d" => :mountain_lion
+    sha256 'd969487931abc27767a3435f5a1b2d06ed61aab0916de187ed894b6137baceee' => :el_capitan
+    sha256 '8529bc1edac66bdeec82afe80ce671b9d015b02959fe9f37efd2887fd975faf1' => :yosemite
+    sha256 '89a32fb53e7f4330d6db84ba526133228189ea3ba3b15adf7fc743787c8ef645' => :mavericks
+    sha256 'd817dec8745f52802b4ac2fbcd2a7a76a647b2000f43ba9a842f59a4363da55d' => :mountain_lion
   end
 
-  head do
-    url "https://github.com/stedolan/jq.git"
+  option :universal
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  # only depends on 'bison' if using maintainer mode
+  depends_on 'oniguruma'
 
-  depends_on "oniguruma"  # jq depends > 1.5
-  depends_on "bison" => :build # jq depends on bison > 2.5
+  patch :DATA
 
   def install
-    system "autoreconf", "-iv" unless build.stable?
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
-  end
+    ENV.universal_binary if build.universal?
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-maintainer-mode',
+                          '--disable-silent-rules'
+    system 'make'
+    # `make check` fails messily, dying with segfaults when processing inputs above a certain level
+    # of complexity.  It doesn’t help that some of the tests absolutely require Valgrind, which was
+    # never produced for Power Macs; or that the documentation tests require a great many Ruby gems,
+    # none of which are available to Leopardbrew’s internal Ruby installation.
+    system 'make', 'install'
+  end # install
 
   test do
-    assert_equal "2\n", pipe_output("#{bin}/jq .bar", '{"foo":1, "bar":2}')
+    for_archs(bin/'jq') do |_, cmd|
+      assert_equal "2\n", pipe_output("#{cmd * ' '} .bar", '{"foo":1, "bar":2}')
+    end
   end
-end
+end # Jq
+
+__END__
+# Allow installing :universal as long as the endianness is consistent.
+--- old/configure
++++ new/configure
+@@ -18214,25 +18214,37 @@
+ 
+ 	# Check for potential -arch flags.  It is not universal unless
+ 	# there are at least two -arch flags with different values.
++	# Even then, endianness is only ambiguous if the values represent
++	# architectures with differing endianness.
+-	ac_arch=
+ 	ac_prev=
++	ac_arch_be=
++	ac_arch_el=
+ 	for ac_word in $CC $CFLAGS $CPPFLAGS $LDFLAGS; do
+-	 if test -n "$ac_prev"; then
+-	   case $ac_word in
+-	     i?86 | x86_64 | ppc | ppc64)
+-	       if test -z "$ac_arch" || test "$ac_arch" = "$ac_word"; then
+-		 ac_arch=$ac_word
+-	       else
+-		 ac_cv_c_bigendian=universal
+-		 break
+-	       fi
+-	       ;;
+-	   esac
+-	   ac_prev=
+-	 elif test "x$ac_word" = "x-arch"; then
+-	   ac_prev=arch
+-	 fi
+-       done
++	  if test "x$ac_word" = "x-arch"; then
++	    ac_prev=arch
++	  elif test -n "$ac_prev"; then
++	    case $ac_word in
++	      arm* | *86*)
++		ac_arch_el=true
++		;;
++	      ppc*)
++		ac_arch_be=true
++		;;
++	    esac
++	    ac_prev=
++	  fi
++	done
++	if test -n "$ac_arch_be"; then
++	  if test -n "$ac_arch_el"; then
++	    ac_cv_c_bigendian=universal
++	  else
++	    ac_cv_c_bigendian=yes
++	  fi
++	elif test -n "$ac_arch_el"; then
++	  ac_cv_c_bigendian=no
++	else
++	  ac_cv_c_bigendian=unknown
++	fi
+ fi
+ rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
+     if test $ac_cv_c_bigendian = unknown; then
