06c8cf1212fba7f35fbd158718c95e029daea5c7
gsteemso <gsteemso@gmail.com>
2025-08-14 23:03:16 -0700

    libiconv:  release/checked dates; v1.17 → 1.18; " → '

--- Library/Formula/l/libiconv.rb
+++ Library/Formula/l/libiconv.rb
@@ -1,40 +1,39 @@
+# stable release 2024-12-15; checked 2025-08-01
 class Libiconv < Formula
-  desc "Conversion library"
-  homepage "https://www.gnu.org/software/libiconv/"
-  url "http://ftpmirror.gnu.org/libiconv/libiconv-1.17.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libiconv/libiconv-1.17.tar.gz"
-  sha256 "8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313"
-
-  bottle do
-    sha256 "533c88e9e63c7f9b98919951d1aae09a0ac385919cb53957b78ee0eb65f615fc" => :tiger_altivec
-  end
+  desc 'Conversion library'
+  homepage 'https://www.gnu.org/software/libiconv/'
+  url 'http://ftpmirror.gnu.org/libiconv/libiconv-1.18.tar.gz'
+  mirror 'https://ftp.gnu.org/gnu/libiconv/libiconv-1.18.tar.gz'
+  sha256 '3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8'
 
   keg_only :provided_by_osx
 
   option :universal
 
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
   depends_on 'gettext'
 
+  # Add definitions for “utf8mac”.
   patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/9be2793af/libiconv/patch-utf8mac.diff"
-    sha256 "e8128732f22f63b5c656659786d2cf76f1450008f36bcf541285268c66cabeab"
+    url 'https://raw.githubusercontent.com/Homebrew/patches/9be2793af/libiconv/patch-utf8mac.diff'
+    sha256 'e8128732f22f63b5c656659786d2cf76f1450008f36bcf541285268c66cabeab'
   end
-
   patch :DATA
 
   def install
     ENV.universal_binary if build.universal?
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--enable-extra-encodings",
-                          "--enable-static",
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-debug',
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules',
+                          '--enable-extra-encodings',
+                          '--enable-static',
                           "--docdir=#{doc}"
-    system "make", "-f", "Makefile.devel", "CFLAGS=#{ENV.cflags}", "CC=#{ENV.cc}"
+    system 'make', '-f', 'Makefile.devel', "CFLAGS=#{ENV.cflags}", "CC=#{ENV.cc}"
     system 'make', 'check'
-    system "make", "install"
+    system 'make', 'install'
   end
 
   def caveats; <<-_.undent
@@ -51,7 +50,7 @@ class Libiconv < Formula
   end
 
   test do
-    arch_system bin/"iconv", "--help"
+    arch_system bin/'iconv', '--help'
   end
 end
 
