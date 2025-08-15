a1da7695f8f2cfd2d1fadb6ef7ff375750787258
gsteemso <gsteemso@gmail.com>
2025-08-14 22:50:50 -0700

    utils/git:  Utils#self.⟨…⟩ → module_function / Utils⸬⟨…⟩

--- Library/Homebrew/utils/git.rb
+++ Library/Homebrew/utils/git.rb
@@ -1,23 +1,25 @@
 module Utils
-  def self.git_available?
+  module_function
+
+  def git_available?
     return @git if instance_variable_defined?(:@git)
-    # check git in original path in case it's the wrapper script of Library/ENV/scm
+    # check git in original path in case it’s the wrapper script of Library/ENV/scm
     git = which("git", ORIGINAL_PATHS.join(File::PATH_SEPARATOR))
-    # git isn't installed by older Xcodes
+    # git isn’t installed by older Xcodes
     return @git = false if git.nil?
     # /usr/bin/git is a popup stub when Xcode/CLT aren't installed, so bail out
     return @git = false if git == "/usr/bin/git" && !MacOS.has_apple_developer_tools?
     @git = true
   end # Utils⸬git_available?
 
-  def self.ensure_git_installed!
+  def ensure_git_installed!
     return if git_available?
 
     require "cmd/install"
     begin
       oh1 "Installing git"
       Homebrew.perform_preinstall_checks
-      Homebrew.install_formula(Formulary.factory("git"))
+      Homebrew.install_formula(Formula['git'])
     rescue
       raise "Git is unavailable"
     end
@@ -25,7 +27,7 @@ module Utils
     raise "Git is unavailable" unless git_available?
   end # Utils⸬ensure_git_installed!
 
-  def self.clear_git_available_cache
+  def clear_git_available_cache
     remove_instance_variable(:@git) if instance_variable_defined?(:@git)
   end
 end # Utils
