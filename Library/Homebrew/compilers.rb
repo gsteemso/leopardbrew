dd0ff3c23b04c0bde98445158c779562a9ef770f
gsteemso <gsteemso@gmail.com>
2025-08-14 22:45:34 -0700

    compilers.rb:  constants re compiler C/C++ defaults

--- Library/Homebrew/compilers.rb
+++ Library/Homebrew/compilers.rb
@@ -1,11 +1,42 @@
+# The four C revisions are [:c89, :c99, :c11, :c23].
+# (GCC support for :c99 was so lacklustre that the default implementation went from :c89 in GCC 4.9
+#  to :c11 in GCC 5.)
+# The six C++ revisions are [:cxx98, :cxx11, :cxx14, :cxx17, :cxx20, :cxx23].
+# (GCC support for C++11 was delayed long enough that the default implementation went from :cxx98
+#  in GCC 5 to :cxx14 in GCC 6.)
+# Other nominal revisions of each language exist, but are effectively redundant.
+
 # @private
 module CompilerConstants
   GNU_GCC_VERSIONS = %w[4.3 4.4 4.5 4.6 4.7 4.8 4.9 5 6 7 8]
   GNU_GCC_REGEXP = /^gcc-(4\.[3-9]|[5-8])$/
+
+  GNU_C11_REGEXP = /^gcc-(4\.9|[5-8])$/
+  # C23 is not yet stable as of GCC 15.
   GNU_CXX11_REGEXP = /^gcc-(4\.[89]|[5-8])$/
   GNU_CXX14_REGEXP = /^gcc-([5-8])$/
-  GNU_C11_REGEXP = /^gcc-(4\.9|[5-8])$/
-  CLANG_CXX11_MIN = '5'
+  # C++17 is stable from GCC 9.  C++20 is mostly stable as of GCC 13 and C++23 as of GCC 15.
+# CLANG_C99_MIN = ???
+# CLANG_C11_MIN = ???
+# CLANG_C23_MIN = ???
+  CLANG_CXX11_MIN = '5.0'
+# CLANG_CXX14_MIN = ???
+# CLANG_CXX17_MIN = ???
+# CLANG_CXX20_MIN = ???
+# CLANG_CXX23_MIN = ???
+
+  GNU_C11_DEFAULT_REGEXP   = /^gcc-([5-8])$/  # Older versions defaulted to C89.
+  GNU_CXX14_DEFAULT_REGEXP = /^gcc-([6-8])$/  # Older versions defaulted to C++98.
+  # C++17 is the default from GCC 11 through 15.
+# CLANG_C99_DEFAULT_MIN = ???
+# CLANG_C11_DEFAULT_MIN = ???
+# CLANG_C23_DEFAULT_MIN = ???
+# CLANG_CXX11_DEFAULT_MIN = ???
+# CLANG_CXX14_DEFAULT_MIN = ???
+# CLANG_CXX17_DEFAULT_MIN = ???
+# CLANG_CXX20_DEFAULT_MIN = ???
+# CLANG_CXX23_DEFAULT_MIN = ???
+
   COMPILER_SYMBOL_MAP = {
     "gcc-4.0"  => :gcc_4_0,
     "gcc-4.2"  => :gcc,
