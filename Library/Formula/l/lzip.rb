b6f54afd0a901a8ef74d384e013bddc67055ee8a
gsteemso <gsteemso@gmail.com>
2025-08-15 17:11:55 -0700

    lzip:  release/checked dates; v1.24 → 1.25; " → '

--- Library/Formula/l/lzip.rb
+++ Library/Formula/l/lzip.rb
@@ -1,28 +1,30 @@
+# stable release 2025-01-17; checked 2025-08-01
 class Lzip < Formula
-  desc "LZMA-based compression program similar to gzip or bzip2"
-  homepage "http://www.nongnu.org/lzip/lzip.html"
-  url 'http://download.savannah.gnu.org/releases/lzip/lzip-1.24.tar.gz'
-  sha256 'd42659229b10e066eeb6e81eb673cdd893b672e512d26719c2d95975556ca56c'
+  desc 'LZMA-based compression program similar to gzip or bzip2'
+  homepage 'http://www.nongnu.org/lzip/lzip.html'
+  url 'http://download.savannah.gnu.org/releases/lzip/lzip-1.25.tar.gz'
+  sha256 '09418a6d8fb83f5113f5bd856e09703df5d37bae0308c668d0f346e3d3f0a56f'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
+    system './configure', "--prefix=#{prefix}",
                           "CXX=#{ENV.cxx}",
                           "CXXFLAGS=#{ENV.cflags}"
-    system "make", "check"
-    ENV.deparallelize { system "make", "install" }
+    system 'make'
+    system 'make', 'check'
+    ENV.deparallelize { system 'make', 'install' }
   end
 
   test do
-    path = testpath/"data.txt"
-    original_contents = "." * 1000
+    path = testpath/'data.txt'
+    original_contents = '.' * 1000
     path.write original_contents
 
     # compress: data.txt -> data.txt.lz
     system "#{bin}/lzip", path
-    assert !path.exist?
+    assert !path.exists?
 
     # decompress: data.txt.lz -> data.txt
-    system "#{bin}/lzip", "-d", "#{path}.lz"
+    system "#{bin}/lzip", '-d', "#{path}.lz"
     assert_equal original_contents, path.read
   end
 end
