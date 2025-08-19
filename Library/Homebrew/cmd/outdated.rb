bc1ece35fd708c64563736ca58a59a8aea49d8e9
Gordon Steemson <gsteemso@gmail.com>
2025-08-18 20:00:54 -0700

    cmd/outdated:  three improvements:
    - Rewrote the help text to be simpler and more accurate.
    
    - Revised the version-checking logic to not spend all that time faffing around
      with details of taps that it then never used.
    
    - Added the previously purported – but actually nonexistent – functionality of
      checking :devel version outdatedness, including two important details:
    
      • No current stable version should prompt a claim of outdatedness.
    
      • The output, in both available formats, shows when a :devel version was the
        standard for comparison.
    
      (Checking for :head‐version currency, as was also incorrectly advertised, is
      unlikely to happen until we have better HEAD versioning in general, & either
      way, upstream repositories will need to be accessed directly in order to get
      meaningful results.)

--- Library/Homebrew/cmd/outdated.rb
+++ Library/Homebrew/cmd/outdated.rb
@@ -1,15 +1,15 @@
-#:  Usage:  brew outdated [ [/--HEAD/ | /--devel/] /installed formula/ [...] ]
+#:  Usage:  brew outdated [ [--devel] <installed formula> [...] ]
 #:
-#:Determine which, if any, of the named /installed formulæ/ are out of date
-#:(i.e., would have newer versions if brewed today).
+#:Determine which, if any, of the named installed formulæ are out of date – i.e.,
+#:would have newer versions if brewed today.
 #:
-#:If no individual formulæ are named, all installed formulæ are examined.
+#:If no specific installed formula is named, all installed formulæ are examined.
 #:
-#:Formulæ may be tagged “--HEAD” or “--devel” to check those versions – though
-#:specifying “--HEAD” will generally provide an incorrect result.  (Whether old
-#:or recent, a formula installed as --HEAD will always look up‐to‐date, as its
-#:version is always “HEAD”.  This is unaffected by whether the upstream
-#:repository has in fact been updated since the local version was installed).
+#:If “--devel” is specified, the target formulæ are examined to see whether they
+#:name a development version.  For those which do, `brew outdated` uses that for
+#:testing outdatedness instead of the stable release’s version.  No similar test
+#:yet exists for HEAD formulæ; it would involve mucking about with remote source-
+#:code repositories.
 
 require 'formula'
 require 'keg'
@@ -28,25 +28,26 @@ module Homebrew
 
   def outdated_brews(formulae)
     formulae.map do |f|
-      all_versions = []
-      older_or_same_tap_versions = []
+      versions = []
       if f.oldname and not f.rack.exists? and (dir = HOMEBREW_CELLAR/f.oldname).exists?
         if f.tap == Tab.for_keg(dir.subdirs.first).tap
           raise Migrator::MigrationNeededError.new(f)
         end
       end
-      f.rack.subdirs.each do |keg_dir|
-        keg = Keg.new keg_dir
-        version = keg.version
-        all_versions << version
-        older_version = f.pkg_version > version
-        tap = Tab.for_keg(keg).tap
-        if tap.nil? or f.tap == tap or older_version
-          older_or_same_tap_versions << version
-        end
+      if ARGV.build_devel? and f.devel
+        check_version = PkgVersion.new(f.devel.version, f.revision)
+        false_positive_version = f.pkg_version
+      else
+        check_version = f.pkg_version
+        false_positive_version = nil
       end
-      if older_or_same_tap_versions.all? { |version| f.pkg_version > version }
-        yield f, all_versions if block_given?
+      f.rack.subdirs.each do |keg_dir|
+        version = Keg.new(keg_dir).version
+        if version != false_positive_version then versions << version; end
+      end
+      versions = versions.compact
+      if versions and not versions.empty? and versions.all?{ |version| version < check_version }
+        yield f, versions, check_version if block_given?
         f
       end
     end.compact
@@ -54,9 +55,9 @@ module Homebrew
 
   def print_outdated(formulae)
     verbose = ($stdout.tty? or VERBOSE) and not QUIETER
-    outdated_brews(formulae) do |f, versions|
+    outdated_brews(formulae) do |f, versions, check_version|
       if verbose
-        puts "#{f.full_name} (#{versions * ', '} < #{f.pkg_version})"
+        puts "#{f.full_name} (#{versions * ', '} < #{check_version}#{' [devel.]' if ARGV.build_devel? and f.devel})"
       else
         puts f.full_name
       end
@@ -65,10 +66,10 @@ module Homebrew
 
   def print_outdated_json(formulae)
     json = []
-    outdated = outdated_brews(formulae) do |f, versions|
+    outdated = outdated_brews(formulae) do |f, versions, check_version|
       json << { :name => f.full_name,
                 :installed_versions => versions.collect(&:to_s),
-                :current_version => f.pkg_version.to_s }
+                :current_version => "#{check_version}#{' (devel.)' if ARGV.build_devel? and f.devel}" }
     end
     puts Utils::JSON.dump(json)
     outdated
