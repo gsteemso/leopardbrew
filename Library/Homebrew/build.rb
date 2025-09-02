aabca54ba3e23d8167d9c61e123e7a738395ef38
gsteemso <gsteemso@gmail.com>
2025-09-01 20:07:42 -0700

    build.rb:  let ENV set default archset

--- Library/Homebrew/build.rb
+++ Library/Homebrew/build.rb
@@ -84,7 +84,7 @@ class Build
       ENV.x11 = reqs.any? { |rq| rq.is_a?(X11Requirement) }
     end
 
-    ENV.setup_build_environment(formula, MacOS.preferred_arch_as_list)
+    ENV.setup_build_environment(formula)
 
     post_superenv_hacks if superenv?
 
