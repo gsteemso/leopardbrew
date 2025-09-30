74a8da9110f8f7d825358ba65655f871c80ba9f4
gsteemso <gsteemso@gmail.com>
2025-09-29 17:57:02 -0700

    test/test_cpu:  Update lists of CPU models, architectures, & types.

--- Library/Homebrew/test/test_cpu.rb
+++ Library/Homebrew/test/test_cpu.rb
@@ -3,15 +3,16 @@ require 'cpu'
 
 class CPUTests < Homebrew::TestCase
   def test_cpu_type
-    assert_includes [:intel, :powerpc], CPU.type
+    assert_includes [:arm, :intel, :powerpc], CPU.type
   end
 
   def test_cpu_arch
-    assert_includes [:i386, :ppc, :ppc64, :x86_64], CPU.arch
+    assert_includes [:arm64, :arm64e, :i386, :ppc, :ppc64, :x86_64, :x86_64h], CPU.arch
   end
 
   def test_cpu_intel_model
-    models = [:core, :core2, :penryn, :nehalem, :arrandale, :sandybridge, :ivybridge, :haswell, :broadwell]
+    models = [:core, :core2, :penryn, :nehalem, :arrandale, :sandybridge, :ivybridge,
+              :haswell, :broadwell, :skylake, :kabylake, :icelake, :cometlake]
     assert_includes models, CPU.model
   end if CPU.intel?
 end
