727fb5f72c9a0f3c20b92b42cccdcd53a1cdb4fe
gsteemso <gsteemso@gmail.com>
2025-09-29 17:58:19 -0700

    compilers.rb:  Rerepresent compiler vs. language/standard support.

--- Library/Homebrew/compilers.rb
+++ Library/Homebrew/compilers.rb
@@ -1,41 +1,58 @@
-# The four C revisions are [:c89, :c99, :c11, :c23].
-# (GCC support for :c99 was so lacklustre that the default implementation went from :c89 in GCC 4.9
-#  to :c11 in GCC 5.)
-# The six C++ revisions are [:cxx98, :cxx11, :cxx14, :cxx17, :cxx20, :cxx23].
-# (GCC support for C++11 was delayed long enough that the default implementation went from :cxx98
-#  in GCC 5 to :cxx14 in GCC 6.)
-# Other nominal revisions of each language exist, but are effectively redundant.
+# The four substantive C revisions are [:c89, :c99, :c11, :c23].
+#   (GCC support for C99 was so laid‐back that the default went from :c89 in GCC 4.9 to :c11 in GCC
+#   5, and C17 didn’t really change much that was noticeable.)
+# The six substantive C++ revisions are [:cxx98, :cxx11, :cxx14, :cxx17, :cxx20, :cxx23].
+#   (GCC’s C++11 support took so long that the default implementation went from C++98 in GCC 6.0 to
+#   C++14 in GCC 6.1.)  Preliminary support for the expected C++26 standard exists, but will not be
+#   ABI‐stable any time soon.
+# According to the GCC Fortran wiki, revisions of that language date all the way back to 1956 – but
+#   GNU Fortran only supports a selection from the middle of that range.  The nine most‐substantive
+#   revisions are probably [:f57, :f66, :f77, :f90, :f95, :f2003, :f2008, :f2018, :f2023].  Support
+#   in LLVM for Fortran is via the third‐party “Flang”, which is its own bizarre beast.
+# Other nominal revisions of each language exist, but are largely redundant.
 
 # @private
 module CompilerConstants
   GNU_GCC_VERSIONS = %w[4.3 4.4 4.5 4.6 4.7 4.8 4.9 5 6 7 8]
-  GNU_GCC_REGEXP = /^gcc-(4\.[3-9]|[5-8])$/
+  GNU_GCC_REGEXP = /^gcc-(4\.[3-9]|[5-9]|1[0-5])$/
 
-  GNU_C11_REGEXP = /^gcc-(4\.9|[5-8])$/
-  # C23 is not yet stable as of GCC 15.
-  GNU_CXX11_REGEXP = /^gcc-(4\.[89]|[5-8])$/
-  GNU_CXX14_REGEXP = /^gcc-([5-8])$/
-  # C++17 is stable from GCC 9.  C++20 is mostly stable as of GCC 13 and C++23 as of GCC 15.
-# CLANG_C99_MIN = ???
-# CLANG_C11_MIN = ???
-# CLANG_C23_MIN = ???
-  CLANG_CXX11_MIN = '5.0'
-# CLANG_CXX14_MIN = ???
-# CLANG_CXX17_MIN = ???
-# CLANG_CXX20_MIN = ???
-# CLANG_CXX23_MIN = ???
+  COMPILER_SUPPORT = {
+    :clang => {
+      :c   => {  :c89 =>  0.0,   :c99 => 0.0,   :c11 => nil,   :c23 => nil,                                 },
+      :cxx => {:cxx98 =>  0.0, :cxx11 => 5.0, :cxx14 => nil, :cxx17 => nil,  :cxx20 => nil,  :cxx23 => nil, },
+      :f   => {  :f57 => nil,    :f66 => nil,   :f77 => nil,   :f90 => nil,    :f95 => nil,  :f2003 => nil,
+               :f2008 => nil,  :f2018 => nil, :f2023 => nil,                                                },
+    },
+    :gcc => {
+      :c   => {  :c89 =>  0.0,   :c99 => 4.0,   :c11 => 4.9,   :c23 => 13.0,                                },
+      :cxx => {:cxx98 =>  0.0, :cxx11 => 4.8, :cxx14 => 5.2, :cxx17 =>  9.0, :cxx20 => 13.0, :cxx23 => 15.0,},
+      :f   => {  :f57 => nil,    :f66 => nil,   :f77 => 0.0,   :f90 =>  4.0,   :f95 =>  4.0, :f2003 => 12.0,
+               :f2008 => 12.0, :f2018 => nil, :f2023 => nil,                                                },
+    },
+  }
 
-  GNU_C11_DEFAULT_REGEXP   = /^gcc-([5-8])$/  # Older versions defaulted to C89.
-  GNU_CXX14_DEFAULT_REGEXP = /^gcc-([6-8])$/  # Older versions defaulted to C++98.
-  # C++17 is the default from GCC 11 through 15.
-# CLANG_C99_DEFAULT_MIN = ???
-# CLANG_C11_DEFAULT_MIN = ???
-# CLANG_C23_DEFAULT_MIN = ???
-# CLANG_CXX11_DEFAULT_MIN = ???
-# CLANG_CXX14_DEFAULT_MIN = ???
-# CLANG_CXX17_DEFAULT_MIN = ???
-# CLANG_CXX20_DEFAULT_MIN = ???
-# CLANG_CXX23_DEFAULT_MIN = ???
+  COMPILER_DEFAULT = {
+    :clang => {
+      :c       => [{0.0 =>   :c89},],
+      :cxx     => [{0.0 => :cxx98},],
+      :fortran => nil,
+    },
+    :gcc => {
+      :c       => [{0.0 =>   :c89}, {5.0 =>   :c11}, { 8.0 =>   :c17}, {15.0 =>   :c23},],
+      :cxx     => [{0.0 => :cxx98}, {6.1 => :cxx14}, {11.0 => :cxx17},                  ],
+      :fortran => [{0.0 =>   :f77}, {4.0 =>   :f95},                                    ],
+    },
+  }
+
+  ARCH_COMPILER_MINIMUM = {
+    :arm64   => {:gcc => nil, :clang => nil},
+    :arm64e  => {:gcc => nil, :clang => nil},
+    :i386    => {:gcc => 4.0, :clang => 0.0},
+    :ppc     => {:gcc => 4.0, :clang => 0.0},
+    :ppc64   => {:gcc => 4.0, :clang => nil},
+    :x86_64  => {:gcc => 4.0, :clang => 0.0},
+    :x86_64h => {:gcc => nil, :clang => nil},
+  }
 
   COMPILER_SYMBOL_MAP = {
     "gcc-4.0"  => :gcc_4_0,
