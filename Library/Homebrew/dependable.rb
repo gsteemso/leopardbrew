612b15e4594bb5195d69dca316b45696ad471135
Gordon Steemson <gsteemso@gmail.com>
2025-10-11 00:47:34 -0700

    dependable.rb:  Compact; add endtag.

--- Library/Homebrew/dependable.rb
+++ Library/Homebrew/dependable.rb
@@ -3,27 +3,15 @@ require "options"
 module Dependable
   RESERVED_TAGS = [:build, :optional, :recommended, :run]
 
-  def build?
-    tags.include? :build
-  end
+  def build?; tags.include? :build; end
 
-  def optional?
-    tags.include? :optional
-  end
+  def optional?; tags.include? :optional; end
 
-  def recommended?
-    tags.include? :recommended
-  end
+  def recommended?; tags.include? :recommended; end
 
-  def run?
-    tags.include? :run
-  end
+  def run?; tags.include? :run; end
 
-  def required?
-    !build? && !optional? && !recommended?
-  end
+  def required?; !build? && !optional? && !recommended?; end
 
-  def options
-    Options.create(tags - RESERVED_TAGS)
-  end
-end
+  def options; Options.create(tags - RESERVED_TAGS); end
+end # Dependable
