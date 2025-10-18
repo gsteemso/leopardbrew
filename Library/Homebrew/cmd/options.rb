806017937dfe8d04876782d3a8ab9bb66e68b08e
Gordon Steemson <gsteemso@gmail.com>
2025-10-17 19:31:17 -0700

    cmd/options:  Compact one method.

--- Library/Homebrew/cmd/options.rb
+++ Library/Homebrew/cmd/options.rb
@@ -26,9 +26,7 @@ module Homebrew
   end
 
   def dump_options_for_formula(f)
-    f.options.sort_by(&:flag).each do |opt|
-      puts "#{opt.flag}\n\t#{opt.description}"
-    end
+    f.options.sort_by(&:flag).each{ |opt| puts "#{opt.flag}\n\t#{opt.description}" }
     puts "--devel\n\tInstall development version #{f.devel.version}" if f.devel
     puts "--HEAD\n\tInstall HEAD version" if f.head
   end
