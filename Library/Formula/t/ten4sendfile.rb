60ce6e738acd1a7635d69cf08169fa503189e44c
Gordon Steemson <gsteemso@gmail.com>
2025-10-25 21:46:15 -0700

    ten4sendfile:  Fix keg_only & caveats

--- Library/Formula/t/ten4sendfile.rb
+++ Library/Formula/t/ten4sendfile.rb
@@ -1,21 +1,13 @@
 class Ten4sendfile < Formula
-  desc 'Sendfile(2) implementation for Mac OS X Tiger'
+  desc 'Sendfile(2) implementation for Mac OS 10.4 Tiger'
   homepage 'https://github.com/gsteemso/ten4sendfile/'
   head 'https://github.com/gsteemso/ten4sendfile.git', :branch => 'main'
+
+  keg_only 'installs an overriding <sys/socket.h> header'
 
   depends_on MaximumMacOSRequirement.new(:tiger)
 
   def install
     system 'make', "prefix=#{prefix}", 'install'
   end
-
-  def caveats
-    <<-_.undent
-      Software which needs to use the sendfile function must add
-          -isystem #{opt_include}
-      to CPPFLAGS whenever compiling a software unit that uses sendfile(2).  Doing so
-      will interpose Ten4Sendfile’s <sys/socket.h> shim, making the sendfile function
-      visible to your software.
-    _
-  end # caveats
 end # Ten4sendfile
