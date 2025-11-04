ab50451cbc29aefd674fe3fb22c6aa09ab7003c7
Gordon Steemson <gsteemso@gmail.com>
2025-11-03 18:10:28 -0800

    extend.misc:  compact; new Symbol#to__s; fix Array#list with it

--- Library/Homebrew/extend/misc.rb
+++ Library/Homebrew/extend/misc.rb
@@ -2,43 +2,31 @@ class Array
   def choke; self unless flatten.compact.empty?; end
 
   def list
+    _self = map{ |item| Symbol === item ? item.to__s : item.to_s }
     case length
       when 0 then ''
-      when 1 then self[0].to_s
-      when 2 then "#{self[0].to_s} and #{self[1].to_s}"
-      else "#{self[0..-2] * ', '}, and #{self[-1].to_s}"
+      when 1 then _self[0]
+      when 2 then "#{_self[0]} and #{_self[1]}"
+      else "#{_self[0..-2] * ', '}, and #{_self[-1]}"
     end
   end # Array#list
 
   alias_method :includes?, :include? unless method_defined? :includes?
 end # Array
 
-module Enumerable
-  alias_method :includes?, :include? unless method_defined? :includes?
-end
+module Enumerable; alias_method :includes?, :include? unless method_defined? :includes?; end
 
-class Hash
-  alias_method :includes?, :include? unless method_defined? :includes?
-end
+class Hash; alias_method :includes?, :include? unless method_defined? :includes?; end
 
 class Module
   def attr_rw(*attrs)
     file, line, = caller.first.split(":")
-    line = line.to_i
-    attrs.each do |attr|
-      module_eval <<-EOS, file, line
-        def #{attr}(val=nil)
-          val.nil? ? @#{attr} : @#{attr} = val
-        end
-      EOS
-    end # each |attr|
+    attrs.each{ |attr| module_eval "def #{attr}(val=nil); val.nil? ? @#{attr} : @#{attr} = val; end", file, line.to_i }
   end # attr_rw
 end # Module
 
-class Numeric
-  def nope; self unless self == 0; end  # return self, but nil when zero
-end
+class Numeric; def nope; self unless self == 0; end; end  # return self, but nil when zero
 
-class Object
-  alias_method :responds_to?, :respond_to? unless method_defined? :responds_to?
-end
+class Object; alias_method :responds_to?, :respond_to? unless method_defined? :responds_to?; end
+
+class Symbol; def to__s; ":#{to_s}"; end; end
