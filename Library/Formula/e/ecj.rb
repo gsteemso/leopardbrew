217409eaa10fbb3ff24a23a200b8a7b7bf91c902
Gordon Steemson <gsteemso@gmail.com>
2025-11-24 19:43:23 -0800

    ecj:  Add release dates, endtags, & “test”; " → '

--- Library/Formula/e/ecj.rb
+++ Library/Formula/e/ecj.rb
@@ -1,13 +1,15 @@
-# This formula is intended to be used by gcc formulae with java support.
-
+# Stable release 2013-12-10; discontinued? (checked 2025-11-13).
+# This formula is intended to be used by gcc formulæ with java support.
 class Ecj < Formula
-  desc "Standalone version of the Eclipse Java compiler"
-  homepage "https://gcc.gnu.org/"
-  url "ftp://sourceware.org/pub/java/ecj-4.9.jar"
-  mirror "https://mirrors.kernel.org/sources.redhat.com/java/ecj-4.9.jar"
-  sha256 "9506e75b862f782213df61af67338eb7a23c35ff425d328affc65585477d34cd"
+  desc 'Standalone version of the Eclipse Java compiler'
+  homepage 'https://gcc.gnu.org/'
+  url 'ftp://sourceware.org/pub/java/ecj-4.9.jar'
+  mirror 'https://mirrors.kernel.org/sources.redhat.com/java/ecj-4.9.jar'
+  sha256 '9506e75b862f782213df61af67338eb7a23c35ff425d328affc65585477d34cd'
 
   def install
-    (share/"java").install "ecj-#{version}.jar" => "ecj.jar"
+    (share/'java').install "ecj-#{version}.jar" => 'ecj.jar'
   end
-end
+
+  test { :does_not_apply }
+end # Ecj
