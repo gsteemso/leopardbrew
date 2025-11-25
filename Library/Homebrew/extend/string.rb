84bd044618310cc1b2512a28a165c53992eb0895
Gordon Steemson <gsteemso@gmail.com>
2025-11-24 19:39:01 -0800

    extend/string:  Tweak #rewrap:  spacing after “.”

--- Library/Homebrew/extend/string.rb
+++ Library/Homebrew/extend/string.rb
@@ -3,10 +3,10 @@ class String
   # eg:
   #   if foo then <<-EOS.undent_________________________________________________________72
   #               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-  #               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  #               eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad
   #               minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-  #               ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-  #               voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+  #               ex ea commodo consequat.  Duis aute irure dolor in reprehenderit in
+  #               voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur
   #               sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
   #               mollit anim id est laborum.
   #               EOS
@@ -18,7 +18,7 @@ class String
     lines = []
     paras = split("\n\n")
     paras.each do |p|
-      p.gsub!("\n", ' ')
+      p = p.gsub(".\n", '.  ').gsub("\n", ' ')
       while p.length > width
         pos = width
         while pos > 0 and p[pos] !~ %r{\s} do pos -= 1; end
