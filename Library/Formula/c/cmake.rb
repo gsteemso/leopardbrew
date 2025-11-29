daa28f9256082a5d95c5e9b6b0a6b553339e79aa
Gordon Steemson <gsteemso@gmail.com>
2025-11-28 16:02:38 -0800

    cmake:  “python” → python2; compact; " → '; dates

--- Library/Formula/c/cmake.rb
+++ Library/Formula/c/cmake.rb
@@ -1,149 +1,143 @@
+# Stable release 2017-11-10; Frozen.  Latest release (4.2.0) 2025-11-19 requires C++11(?); checked 2025-11-28.
 class Cmake < Formula
-  desc "Cross-platform make"
-  homepage "http://www.cmake.org/"
-  url "https://cmake.org/files/v3.9/cmake-3.9.6.tar.gz"
-  sha256 "7410851a783a41b521214ad987bb534a7e4a65e059651a2514e6ebfc8f46b218"
+  desc 'Cross-platform make'
+  homepage 'http://www.cmake.org/'
+  url 'https://cmake.org/files/v3.9/cmake-3.9.6.tar.gz'
+  sha256 '7410851a783a41b521214ad987bb534a7e4a65e059651a2514e6ebfc8f46b218'
 
-  head "https://cmake.org/cmake.git"
+  head 'https://cmake.org/cmake.git'
 
-  bottle do
-    sha256 "7ed83fa411209917e876cd0a1396593ee78af1201e4ec952ab32884cf00e0c50" => :tiger_altivec
-  end
+  bottle { sha256 '7ed83fa411209917e876cd0a1396593ee78af1201e4ec952ab32884cf00e0c50' => :tiger_altivec }
 
-  option "without-docs", "Don't build man pages"
-  option "with-completion", "Install Bash completion (Has potential problems with system bash)"
+  option 'with-completion', 'Install Bash completion (Has potential problems with system bash)'
+  option 'without-docs', 'Don’t build man pages'
 
-  depends_on :python => :build if MacOS.version <= :snow_leopard && build.with?("docs")
-  depends_on "xz" => :build
+  depends_on :python2 => :build if MacOS.version <= :snow_leopard and build.with?('docs')
+  depends_on 'xz'     => :build
 
-  depends_on "curl"
-  depends_on "expat"
-  depends_on "libarchive"
+  depends_on 'curl'
+  depends_on 'expat'
+  depends_on 'libarchive'
   depends_on 'libuv'
-  depends_on "rhash"
-  depends_on "zlib"
+  depends_on 'rhash'
+  depends_on 'zlib'
 
   # The `with-qt` GUI option was removed due to circular dependencies if
   # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
   # For the GUI application please instead use brew install caskroom/cask/cmake.
 
-  resource "sphinx_rtd_theme" do
-    url "https://files.pythonhosted.org/packages/35/b4/40faec6790d4b08a6ef878feddc6ad11c3872b75f52273f1418c39f67cd6/sphinx_rtd_theme-1.2.0.tar.gz"
-    sha256 "a0d8bd1a2ed52e0b338cbe19c4b2eef3c5e7a048769753dac6a9f059c7b641b8"
+  resource 'sphinx_rtd_theme' do
+    url 'https://files.pythonhosted.org/packages/35/b4/40faec6790d4b08a6ef878feddc6ad11c3872b75f52273f1418c39f67cd6/sphinx_rtd_theme-1.2.0.tar.gz'
+    sha256 'a0d8bd1a2ed52e0b338cbe19c4b2eef3c5e7a048769753dac6a9f059c7b641b8'
   end
 
-  resource "snowballstemmer" do
-    url "https://files.pythonhosted.org/packages/a3/3d/d305c9112f35df6efb51e5acd0db7009b74d86f35580e033451b5994a0a9/snowballstemmer-2.1.0.tar.gz"
-    sha256 "e997baa4f2e9139951b6f4c631bad912dfd3c792467e2f03d7239464af90e914"
+  resource 'snowballstemmer' do
+    url 'https://files.pythonhosted.org/packages/a3/3d/d305c9112f35df6efb51e5acd0db7009b74d86f35580e033451b5994a0a9/snowballstemmer-2.1.0.tar.gz'
+    sha256 'e997baa4f2e9139951b6f4c631bad912dfd3c792467e2f03d7239464af90e914'
   end
 
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
-    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  resource 'six' do
+    url 'https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz'
+    sha256 '1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926'
   end
 
-  resource "pygments" do
-    url "https://files.pythonhosted.org/packages/cb/9f/27d4844ac5bf158a33900dbad7985951e2910397998e85712da03ce125f0/Pygments-2.5.2.tar.gz"
-    sha256 "98c8aa5a9f778fcd1026a17361ddaf7330d1b7c62ae97c3bb0ae73e0b9b6b0fe"
+  resource 'pygments' do
+    url 'https://files.pythonhosted.org/packages/cb/9f/27d4844ac5bf158a33900dbad7985951e2910397998e85712da03ce125f0/Pygments-2.5.2.tar.gz'
+    sha256 '98c8aa5a9f778fcd1026a17361ddaf7330d1b7c62ae97c3bb0ae73e0b9b6b0fe'
   end
 
-  resource "docutils" do
-    url "https://files.pythonhosted.org/packages/4c/17/559b4d020f4b46e0287a2eddf2d8ebf76318fd3bd495f1625414b052fdc9/docutils-0.17.1.tar.gz"
-    sha256 "686577d2e4c32380bb50cbb22f575ed742d58168cee37e99117a854bcd88f125"
+  resource 'docutils' do
+    url 'https://files.pythonhosted.org/packages/4c/17/559b4d020f4b46e0287a2eddf2d8ebf76318fd3bd495f1625414b052fdc9/docutils-0.17.1.tar.gz'
+    sha256 '686577d2e4c32380bb50cbb22f575ed742d58168cee37e99117a854bcd88f125'
   end
 
-  resource "pytz" do
-    url "https://files.pythonhosted.org/packages/5e/32/12032aa8c673ee16707a9b6cdda2b09c0089131f35af55d443b6a9c69c1d/pytz-2023.3.tar.gz"
-    sha256 "1d8ce29db189191fb55338ee6d0387d82ab59f3d00eac103412d64e0ebd0c588"
+  resource 'pytz' do
+    url 'https://files.pythonhosted.org/packages/5e/32/12032aa8c673ee16707a9b6cdda2b09c0089131f35af55d443b6a9c69c1d/pytz-2023.3.tar.gz'
+    sha256 '1d8ce29db189191fb55338ee6d0387d82ab59f3d00eac103412d64e0ebd0c588'
   end
 
-  resource "babel" do
-    url "https://files.pythonhosted.org/packages/41/1b/5ed6e564b9ca54318df20ebe5d642ab25da4118df3c178247b8c4b26fa13/Babel-2.9.0.tar.gz"
-    sha256 "da031ab54472314f210b0adcff1588ee5d1d1d0ba4dbd07b94dba82bde791e05"
+  resource 'babel' do
+    url 'https://files.pythonhosted.org/packages/41/1b/5ed6e564b9ca54318df20ebe5d642ab25da4118df3c178247b8c4b26fa13/Babel-2.9.0.tar.gz'
+    sha256 'da031ab54472314f210b0adcff1588ee5d1d1d0ba4dbd07b94dba82bde791e05'
   end
 
-  resource "markupsafe" do
-    url "https://files.pythonhosted.org/packages/b9/2e/64db92e53b86efccfaea71321f597fa2e1b2bd3853d8ce658568f7a13094/MarkupSafe-1.1.1.tar.gz"
-    sha256 "29872e92839765e546828bb7754a68c418d927cd064fd4708fab9fe9c8bb116b"
+  resource 'markupsafe' do
+    url 'https://files.pythonhosted.org/packages/b9/2e/64db92e53b86efccfaea71321f597fa2e1b2bd3853d8ce658568f7a13094/MarkupSafe-1.1.1.tar.gz'
+    sha256 '29872e92839765e546828bb7754a68c418d927cd064fd4708fab9fe9c8bb116b'
   end
 
-  resource "jinja2" do
-    url "https://files.pythonhosted.org/packages/4f/e7/65300e6b32e69768ded990494809106f87da1d436418d5f1367ed3966fd7/Jinja2-2.11.3.tar.gz"
-    sha256 "a6d58433de0ae800347cab1fa3043cebbabe8baa9d29e668f1c768cb87a333c6"
+  resource 'jinja2' do
+    url 'https://files.pythonhosted.org/packages/4f/e7/65300e6b32e69768ded990494809106f87da1d436418d5f1367ed3966fd7/Jinja2-2.11.3.tar.gz'
+    sha256 'a6d58433de0ae800347cab1fa3043cebbabe8baa9d29e668f1c768cb87a333c6'
   end
 
-  resource "alabaster" do
-    url "https://files.pythonhosted.org/packages/cc/b4/ed8dcb0d67d5cfb7f83c4d5463a7614cb1d078ad7ae890c9143edebbf072/alabaster-0.7.12.tar.gz"
-    sha256 "a661d72d58e6ea8a57f7a86e37d86716863ee5e92788398526d58b26a4e4dc02"
+  resource 'alabaster' do
+    url 'https://files.pythonhosted.org/packages/cc/b4/ed8dcb0d67d5cfb7f83c4d5463a7614cb1d078ad7ae890c9143edebbf072/alabaster-0.7.12.tar.gz'
+    sha256 'a661d72d58e6ea8a57f7a86e37d86716863ee5e92788398526d58b26a4e4dc02'
   end
 
-  resource "typing" do
-    url "https://files.pythonhosted.org/packages/b0/1b/835d4431805939d2996f8772aca1d2313a57e8860fec0e48e8e7dfe3a477/typing-3.10.0.0.tar.gz"
-    sha256 "13b4ad211f54ddbf93e5901a9967b1e07720c1d1b78d596ac6a439641aa1b130"
+  resource 'typing' do
+    url 'https://files.pythonhosted.org/packages/b0/1b/835d4431805939d2996f8772aca1d2313a57e8860fec0e48e8e7dfe3a477/typing-3.10.0.0.tar.gz'
+    sha256 '13b4ad211f54ddbf93e5901a9967b1e07720c1d1b78d596ac6a439641aa1b130'
   end
 
-  resource "packaging" do
-    url "https://files.pythonhosted.org/packages/86/3c/bcd09ec5df7123abcf695009221a52f90438d877a2f1499453c6938f5728/packaging-20.9.tar.gz"
-    sha256 "5b327ac1320dc863dca72f4514ecc086f31186744b84a230374cc1fd776feae5"
+  resource 'packaging' do
+    url 'https://files.pythonhosted.org/packages/86/3c/bcd09ec5df7123abcf695009221a52f90438d877a2f1499453c6938f5728/packaging-20.9.tar.gz'
+    sha256 '5b327ac1320dc863dca72f4514ecc086f31186744b84a230374cc1fd776feae5'
   end
 
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz"
-    sha256 "68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61"
+  resource 'requests' do
+    url 'https://files.pythonhosted.org/packages/60/f3/26ff3767f099b73e0efa138a9998da67890793bfa475d8278f84a30fec77/requests-2.27.1.tar.gz'
+    sha256 '68d7c56fd5a8999887728ef304a6d12edc7be74f1cfa47714fc8b414525c9a61'
   end
 
-  resource "imagesize" do
-    url "https://files.pythonhosted.org/packages/a7/84/62473fb57d61e31fef6e36d64a179c8781605429fd927b5dd608c997be31/imagesize-1.4.1.tar.gz"
-    sha256 "69150444affb9cb0d5cc5a92b3676f0b2fb7cd9ae39e947a5e11a36b4497cd4a"
+  resource 'imagesize' do
+    url 'https://files.pythonhosted.org/packages/a7/84/62473fb57d61e31fef6e36d64a179c8781605429fd927b5dd608c997be31/imagesize-1.4.1.tar.gz'
+    sha256 '69150444affb9cb0d5cc5a92b3676f0b2fb7cd9ae39e947a5e11a36b4497cd4a'
   end
 
-  resource "pyparsing" do
-    url "https://files.pythonhosted.org/packages/c1/47/dfc9c342c9842bbe0036c7f763d2d6686bcf5eb1808ba3e170afdb282210/pyparsing-2.4.7.tar.gz"
-    sha256 "c203ec8783bf771a155b207279b9bccb8dea02d8f0c9e5f8ead507bc3246ecc1"
+  resource 'pyparsing' do
+    url 'https://files.pythonhosted.org/packages/c1/47/dfc9c342c9842bbe0036c7f763d2d6686bcf5eb1808ba3e170afdb282210/pyparsing-2.4.7.tar.gz'
+    sha256 'c203ec8783bf771a155b207279b9bccb8dea02d8f0c9e5f8ead507bc3246ecc1'
   end
 
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/ea/b7/e0e3c1c467636186c39925827be42f16fee389dc404ac29e930e9136be70/idna-2.10.tar.gz"
-    sha256 "b307872f855b18632ce0c21c5e45be78c0ea7ae4c15c828c20788b26921eb3f6"
+  resource 'idna' do
+    url 'https://files.pythonhosted.org/packages/ea/b7/e0e3c1c467636186c39925827be42f16fee389dc404ac29e930e9136be70/idna-2.10.tar.gz'
+    sha256 'b307872f855b18632ce0c21c5e45be78c0ea7ae4c15c828c20788b26921eb3f6'
   end
 
-  resource "chardet" do
-    url "https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz"
-    sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
+  resource 'chardet' do
+    url 'https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz'
+    sha256 '0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa'
   end
 
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/b8/e2/a3a86a67c3fc8249ed305fc7b7d290ebe5e4d46ad45573884761ef4dea7b/certifi-2020.4.5.1.tar.gz"
-    sha256 "51fcb31174be6e6664c5f69e3e1691a2d72a1a12e90f872cbdb1567eb47b6519"
+  resource 'certifi' do
+    url 'https://files.pythonhosted.org/packages/b8/e2/a3a86a67c3fc8249ed305fc7b7d290ebe5e4d46ad45573884761ef4dea7b/certifi-2020.4.5.1.tar.gz'
+    sha256 '51fcb31174be6e6664c5f69e3e1691a2d72a1a12e90f872cbdb1567eb47b6519'
   end
 
-  resource "urllib" do
-    url "https://files.pythonhosted.org/packages/21/79/6372d8c0d0641b4072889f3ff84f279b738cd8595b64c8e0496d4e848122/urllib3-1.26.15.tar.gz"
-    sha256 "8a388717b9476f934a21484e8c8e61875ab60644d29b9b39e11e4b9dc1c6b305"
+  resource 'urllib' do
+    url 'https://files.pythonhosted.org/packages/21/79/6372d8c0d0641b4072889f3ff84f279b738cd8595b64c8e0496d4e848122/urllib3-1.26.15.tar.gz'
+    sha256 '8a388717b9476f934a21484e8c8e61875ab60644d29b9b39e11e4b9dc1c6b305'
   end
 
-  resource "sphinx" do
-    url "https://files.pythonhosted.org/packages/95/74/5cef400220b2f22a4c85540b9ba20234525571b8b851be8a9ac219326a11/Sphinx-1.8.6.tar.gz"
-    sha256 "e096b1b369dbb0fcb95a31ba8c9e1ae98c588e601f08eada032248e1696de4b1"
+  resource 'sphinx' do
+    url 'https://files.pythonhosted.org/packages/95/74/5cef400220b2f22a4c85540b9ba20234525571b8b851be8a9ac219326a11/Sphinx-1.8.6.tar.gz'
+    sha256 'e096b1b369dbb0fcb95a31ba8c9e1ae98c588e601f08eada032248e1696de4b1'
   end
 
-  resource "sphinxcontrib-websupport" do
-    url "https://files.pythonhosted.org/packages/e8/3d/b33240484f128c4fcbf7bb04837a9b93f8259d3fbcb8e521c7c6267a0da9/sphinxcontrib-websupport-1.1.2.tar.gz"
-    sha256 "1501befb0fdf1d1c29a800fdbf4ef5dc5369377300ddbdd16d2cd40e54c6eefc"
+  resource 'sphinxcontrib-websupport' do
+    url 'https://files.pythonhosted.org/packages/e8/3d/b33240484f128c4fcbf7bb04837a9b93f8259d3fbcb8e521c7c6267a0da9/sphinxcontrib-websupport-1.1.2.tar.gz'
+    sha256 '1501befb0fdf1d1c29a800fdbf4ef5dc5369377300ddbdd16d2cd40e54c6eefc'
   end
 
   def install
-    if build.with? "docs"
-      ENV.prepend_create_path "PYTHONPATH", buildpath/"sphinx/lib/python2.7/site-packages"
-      resources.each do |r|
-        r.stage do
-          system "python", *Language::Python.setup_install_args(buildpath/"sphinx")
-        end
-      end
+    if build.with? 'docs'
+      ENV.prepend_create_path 'PYTHONPATH', buildpath/'sphinx/lib/python2.7/site-packages'
+      resources.each{ |r| r.stage { system 'python2', *Language::Python.setup_install_args(buildpath/'sphinx') } }
 
-      # There is an existing issue around OS X & Python locale setting
-      # See http://bugs.python.org/issue18378#msg215215 for explanation
-      ENV["LC_ALL"] = "en_US.UTF-8"
+      # There is a known issue with BSDs / Mac OS & Python2 locale setting.  See (http://bugs.python.org/issue18378#msg215215).
+      ENV['LC_ALL'] = 'en_US.UTF-8' if ENV['LC_ALL'] == 'UTF-8'
     end
 
     args = %W[
@@ -155,29 +149,20 @@ class Cmake < Formula
       --docdir=/share/doc/cmake
       --mandir=/share/man
     ]
+    args << '--sphinx-man' << "--sphinx-build=#{buildpath}/sphinx/bin/sphinx-build" if build.with? 'docs'
+    args << '--' << '-DCMAKE_OSX_SYSROOT=/'  # GCC 4.2 does not find stdarg.h if the sysroot is set to an SDK
 
-    if build.with? "docs"
-      args << "--sphinx-man" << "--sphinx-build=#{buildpath}/sphinx/bin/sphinx-build"
-    end
+    system './bootstrap', *args
+    system 'make'
+    system 'make', 'install'
 
-    # gcc-4.2 does not find stdarg.h if the sysroot is set to an SDK
-    args << "--" << "-DCMAKE_OSX_SYSROOT=/"
+    cd 'Auxiliary/bash-completion/' { bash_completion.install 'ctest', 'cmake', 'cpack' } if build.with? 'completion'
 
-    system "./bootstrap", *args
-    system "make"
-    system "make", "install"
-
-    if build.with? "completion"
-      cd "Auxiliary/bash-completion/" do
-        bash_completion.install "ctest", "cmake", "cpack"
-      end
-    end
-
-    (share/"emacs/site-lisp/cmake").install "Auxiliary/cmake-mode.el"
+    (share/'emacs/site-lisp/cmake').install 'Auxiliary/cmake-mode.el'
   end
 
   test do
-    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
-    system "#{bin}/cmake", "."
+    (testpath/'CMakeLists.txt').write('find_package(Ruby)')
+    system bin/'cmake', '.'
   end
 end
