423a7762e6e25835f134360d04866b3dbcc97f5e
Gordon Steemson <gsteemso@gmail.com>
2025-12-06 15:31:49 -0800

    testdisk:  v7.0 → 7.2; " → '; endtags

--- Library/Formula/t/testdisk.rb
+++ Library/Formula/t/testdisk.rb
@@ -1,28 +1,19 @@
 class Testdisk < Formula
-  desc "TestDisk is a powerful free data recovery utility"
-  homepage "http://www.cgsecurity.org/wiki/TestDisk"
-  url "http://www.cgsecurity.org/testdisk-7.0.tar.bz2"
-  sha256 "00bb3b6b22e6aba88580eeb887037aef026968c21a87b5f906c6652cbee3442d"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "244b1c7a5e6beec940bb5ee4a27f54be138a1df732d9eb276d5798e7f947799c" => :el_capitan
-    sha256 "0ca542c57ca8ed24c3ecb5d5b449dc18fda306744309fb59598e2a7e9c55c08b" => :yosemite
-    sha256 "e537da6b508c8aac5842c3d2094b5b914effea9ea04484cf09ac9bb80d46e3f0" => :mavericks
-    sha256 "afbf7d1f54c14b40d86baadc34efeaed0399257598d60755d910aec978aee7ef" => :mountain_lion
-  end
+  desc 'powerful free data-recovery utility'
+  homepage 'http://www.cgsecurity.org/wiki/TestDisk'
+  url 'http://www.cgsecurity.org/testdisk-7.2.tar.bz2'
+  sha256 'f8343be20cb4001c5d91a2e3bcd918398f00ae6d8310894a5a9f2feb813c283f'
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
-  end
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules'
+    system 'make', 'install'
+  end # install
 
   test do
-    path = "test.dmg"
-    system "hdiutil", "create", "-megabytes", "10", path
-    system "#{bin}/testdisk", "/list", path
+    path = 'test.dmg'
+    system 'hdiutil', 'create', '-megabytes', '10', path
+    system bin/'testdisk', '/list', path
   end
-end
+end # Testdisk
