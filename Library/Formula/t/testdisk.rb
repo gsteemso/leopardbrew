1a8823b558a7f9246e7797f93b2e461ab423bcc3
Gordon Steemson <gsteemso@gmail.com>
2026-01-14 17:58:47 -0800

    testdisk:  Patch a trivial erroneous datum.

--- Library/Formula/t/testdisk.rb
+++ Library/Formula/t/testdisk.rb
@@ -5,6 +5,9 @@ class Testdisk < Formula
   url 'http://www.cgsecurity.org/testdisk-7.2.tar.bz2'
   sha256 'f8343be20cb4001c5d91a2e3bcd918398f00ae6d8310894a5a9f2feb813c283f'
 
+  # Correct a typo in the partition‐GUID data.
+  patch :DATA
+
   def install
     system './configure', "--prefix=#{prefix}",
                           '--disable-dependency-tracking',
@@ -18,3 +21,16 @@ class Testdisk < Formula
     system bin/'testdisk', '/list', path
   end
 end # Testdisk
+
+__END__
+--- old/src/common.h
++++ new/src/common.h
+@@ -141,7 +141,7 @@
+ #define GPT_ENT_TYPE_FREEBSD_UFS	\
+ 	(const efi_guid_t){le32(0x516e7cb6),le16(0x6ecf),le16(0x11d6),0x8f,0xf8,{0x00,0x02,0x2d,0x09,0x71,0x2b}}
+ #define GPT_ENT_TYPE_FREEBSD_ZFS	\
+-	(const efi_guid_t){le32(0x516e7cb),le16(0x6ecf),le16(0x11d6),0x8f,0xf8,{0x00,0x02,0x2d,0x09,0x71,0x2b}}
++	(const efi_guid_t){le32(0x516e7cba),le16(0x6ecf),le16(0x11d6),0x8f,0xf8,{0x00,0x02,0x2d,0x09,0x71,0x2b}}
+ /*
+  * The following is unused but documented here to avoid reuse.
+  *
