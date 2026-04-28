407ed4e2fc232b35c18c2c59a84e20f2e44b8744
Gordon Steemson <gsteemso@gmail.com>
2026-04-28 14:41:48 -0700

    vice:  dates; v2.4→3.10; needs C++11 (incomplete)

--- Library/Formula/v/vice.rb
+++ Library/Formula/v/vice.rb
@@ -1,47 +1,32 @@
+#stable release 2025-12-24; checked 2026-03-21.
 class Vice < Formula
-  desc "Versatile Commodore Emulator"
-  homepage "http://vice-emu.sourceforge.net/"
-  url "http://www.zimmers.net/anonftp/pub/cbm/crossplatform/emulators/VICE/vice-2.4.tar.gz"
-  sha256 "ff8b8d5f0f497d1f8e75b95bbc4204993a789284a08a8a59ba727ad81dcace10"
-  revision 3
+  desc 'Versatile Commodore Emulator'
+  homepage 'https://vice-emu.sourceforge.io/'
+  url 'https://sourceforge.net/projects/vice-emu/files/releases/vice-3.10.tar.gz'
+  sha256 '8e5bac18cbcb9f192380ad3ef881f8790f5b75c41d7b3da65d831985d864d6d1'
 
-  bottle do
-    cellar :any
-    sha256 "b64f33472ea5655c1aac3795b79d99b14738c28642c0cf21d9708441d02323ef" => :yosemite
-    sha256 "05446f9614d5ee6170cd2d323ad24289a0312ac42a5f2ec575200036513731b1" => :mavericks
-    sha256 "de32b3004dbc9a1dad21a546c983ba55d3559eae78f898a54be96c8f2c278b3b" => :mountain_lion
-  end
+  needs :cxx11
 
-  depends_on "pkg-config" => :build
-  depends_on "jpeg"
-  depends_on "libpng"
-  depends_on "giflib"
-  depends_on "lame" => :optional
-
-  fails_with :llvm do
-    build 2335
-  end
+  depends_on 'autoconf'
+  depends_on 'automake'
+  depends_on 'dos2unix'
+  depends_on 'lame'
+  depends_on 'libomp'
+  depends_on 'libpng'
+  depends_on 'pkg-config'
+  depends_on 'sdl'
+  depends_on 'sdl-image'
+  depends_on 'texinfo'
+  depends_on 'xa'
 
   def install
-    # Use Cocoa instead of X
-    # Use a static lame, otherwise Vice is hard-coded to look in
-    # /opt for the library.
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--with-cocoa",
-                          "--without-x",
-                          "--enable-static-lame",
-                          # VICE can't compile against FFMPEG newer than 0.11:
-                          # http://sourceforge.net/tracker/?func=detail&aid=3585471&group_id=223021&atid=1057617
-                          "--disable-ffmpeg"
-    system "make"
-    system "make", "bindist"
-    prefix.install Dir["vice-macosx-*/*"]
-    bin.install_symlink Dir[prefix/"tools/*"]
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-silent-rules',
+                          "--enable-macos-minimum-version=#{MacOS.version}"
+                          '--enable-sdl1ui',
+                          '--enable-platformdox'
+    system 'make'
+    system 'make', 'bindist'
   end
-
-  def caveats; <<-EOS.undent
-    Cocoa apps for these emulators have been installed to #{prefix}.
-  EOS
-  end
-end
+end # Vice
