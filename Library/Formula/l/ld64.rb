3b15e5b5e5a79d0b7dcbd894053ff6c785ea6623
Gordon Steemson <gsteemso@gmail.com>
2026-05-20 21:50:15 -0700

    ld64:  fix various types of optional reporting

--- Library/Formula/l/ld64.rb
+++ Library/Formula/l/ld64.rb
@@ -6,7 +6,7 @@ class Ld64 < Formula
   # patching.  Leopard users:  If you like, add a 127.2 option, or fix the build on Tiger.
   url "https://github.com/apple-oss-distributions/ld64/archive/refs/tags/ld64-97.17.tar.gz"
   sha256 "dc609d295365f8f5853b45e8dbcb44ca85e7dbc7a530e6fb5342f81d3c042db5"
-  revision 4  # For "configure.h".
+  revision 5  # For fixes to reporting.
 
   resource "makefile" do
     url "https://trac.macports.org/export/123511/trunk/dports/devel/ld64/files/Makefile-97", :using => :nounzip
@@ -31,10 +31,7 @@ class Ld64 < Formula
     cause 'It incorrectly gets hung up on “protected” status in the C++ code.'
   end
 
-  # Fix the messed‐up PowerPC maximum‐displacement constants, incorporating MacPorts’ un‐botching of the logic that chooses whether
-  # to do a branch island.  Also incorporates a variation on MacPorts’ version‐number patch via configure.h, tuned to this revision
-  # of ld64, as well as fixing the logic for what to do when internally handed a file pathname instead of a proper install path.
-  patch :DATA
+  patch :DATA  # Annotated inline.
 
   # Remove LTO support
   patch :p0 do
@@ -88,8 +85,104 @@ class Ld64 < Formula
 end
 
 __END__
+--- old/src/ld/ArchiveReader.hpp
++++ new/src/ld/ArchiveReader.hpp
+# Correct a comment documenting what file‐load‐tracing command‐line option is being implemented, & make it work in the real world.
+@@ -274,9 +274,9 @@
+ 	if ( strncmp((const char*)fileContent, "!<arch>\n", 8) != 0 )
+ 		throw "not an archive";
+ 
+-	// write out path for -whatsloaded option
++	// write out path for -t option
+ 	if ( options.fLogAllFiles )
+-		printf("%s\n", path);
++		fprintf(stderr, "[ld64:  archive loaded] %s\n", path);
+ 
+ 	if ( !options.fFullyLoadArchives && !fForceLoad ) {
+ 		const Entry* const firstMember = (Entry*)&fFileContent[8];
+# Make why‐loaded reporting work in the real world.
+@@ -339,9 +339,9 @@
+ 				continue;
+ 			if ( fOptions.fWhyLoad ) {
+ 				if ( fForceLoad )
+-					printf("-force_load forced load of %s(%s)\n", this->getPath(), memberName);
++					fprintf(stderr, "[ld64] -force_load forced load of %s(%s)\n", this->getPath(), memberName);
+ 				else
+-					printf("-all_load forced load of %s(%s)\n", this->getPath(), memberName);
++					fprintf(stderr, "[ld64] -all_load forced load of %s(%s)\n", this->getPath(), memberName);
+ 			}
+ 			ObjectFile::Reader* r = this->makeObjectReaderForMember(p);
+ 			std::vector<class ObjectFile::Atom*>&	atoms = r->getAtoms();
+@@ -357,7 +357,7 @@
+ 				const Entry* member = (Entry*)&fFileContent[E::get32(it->second->ran_off)];
+ 				if ( fInstantiatedEntries.count(member) == 0 ) {
+ 					if ( fOptions.fWhyLoad )
+-						printf("-ObjC forced load of %s(%s)\n", this->getPath(), member->getName());
++						fprintf(stderr, "[ld64] -ObjC forced load of %s(%s)\n", this->getPath(), member->getName());
+ 					// only return these atoms once
+ 					fInstantiatedEntries.insert(member);
+ 					ObjectFile::Reader* r = makeObjectReaderForMember(member);
+# Make ToC dumping work in the real world.
+@@ -426,7 +426,7 @@
+ {
+ 	for (unsigned int i=0; i < fTableOfContentCount; ++i) {
+ 		const struct ranlib* e = &fTableOfContents[i];
+-		printf("%s in %s\n", &fStringPool[E::get32(e->ran_un.ran_strx)], ((Entry*)&fFileContent[E::get32(e->ran_off)])->getName());
++		fprintf(stderr, "[ld64] %s in %s\n", &fStringPool[E::get32(e->ran_un.ran_strx)], ((Entry*)&fFileContent[E::get32(e->ran_off)])->getName());
+ 	}
+ }
+ 
+# Make why‐loaded reporting work in the real world.
+@@ -444,7 +444,7 @@
+ 			const Entry* member = (Entry*)&fFileContent[E::get32(result->ran_off)];
+ 			if ( fInstantiatedEntries.count(member) == 0 ) {
+ 				if ( fOptions.fWhyLoad ) 
+-					printf("%s forced load of %s(%s)\n", name, this->getPath(), member->getName());
++					fprintf(stderr, "[ld64] %s forced load of %s(%s)\n", name, this->getPath(), member->getName());
+ 				// only return these atoms once
+ 				fInstantiatedEntries.insert(member);
+ 				ObjectFile::Reader* r = makeObjectReaderForMember(member);
+--- old/src/ld/MachOReaderDylib.hpp
++++ new/src/ld/MachOReaderDylib.hpp
+# Correct a comment documenting what file‐load‐tracing command‐line option is being implemented, & make it work in the real world.
+@@ -345,9 +345,9 @@
+ 	const macho_load_command<P>* const cmds = (macho_load_command<P>*)((char*)header + sizeof(macho_header<P>));
+ 	const macho_load_command<P>* const cmdsEnd = (macho_load_command<P>*)((char*)header + sizeof(macho_header<P>) + header->sizeofcmds());
+ 
+-	// write out path for -whatsloaded option
++	// write out path for -t option
+ 	if ( options.fLogAllFiles )
+-		printf("%s\n", path);
++		fprintf(stderr, "[ld64:  dylib loaded] %s\n", path);
+ 
+ 	if ( options.fRootSafe && ((header->flags() & MH_ROOT_SAFE) == 0) )
+ 		warning("using -root_safe but linking against %s which is not root safe", path);
+--- old/src/ld/MachOReaderRelocatable.hpp
++++ new/src/ld/MachOReaderRelocatable.hpp
+# Make file-load tracing work in the real world.
+@@ -1950,7 +1950,7 @@
+ 
+ 	// write out path for -t or -whatsloaded option
+ 	if ( options.fLogObjectFiles || options.fLogAllFiles )
+-		printf("%s\n", path);
++		fprintf(stderr, "[ld64:  Mach-O file loaded] %s\n", path);
+ 
+ 	// cache intersting pointers
+ 	const macho_header<P>* header = (const macho_header<P>*)fileContent;
+# Make error reporting work in the real world.
+@@ -4335,7 +4335,7 @@
+ 							displacement |= 0xFC000000;
+ 					}
+ 					else {
+-						printf("bad instruction for BR24 reloc");
++						fprintf(stderr, "[ld64] bad instruction for BR24 reloc");
+ 					}
+ 					if ( reloc->r_extern() ) {
+ 						offsetInTarget = srcAddr + displacement;
 --- old/src/ld/MachOWriterExecutable.hpp
 +++ new/src/ld/MachOWriterExecutable.hpp
+# Fix the messed‐up PowerPC maximum‐displacement constants, incorporating MacPorts’ un‐botching of the logic choosing whether to do
+# a branch island.
 @@ -7559,10 +7559,10 @@
  					displacement -= ref->getTarget().getAddress();
  				}
@@ -147,6 +240,7 @@ __END__
  	}
 --- old/src/ld/Options.cpp
 +++ new/src/ld/Options.cpp
+# Incorporate a variation on MacPorts’ version‐number patch via configure.h, tuned to this revision of ld64.
 @@ -31,12 +31,19 @@
  #include <vector>
  
@@ -167,6 +261,26 @@ __END__
  // magic to place command line in crash reports
  extern "C" char* __crashreporter_info__;
  static char crashreporterBuffer[1000];
+# Make file‐search reporting work in the real world.
+@@ -588,7 +595,7 @@
+ 	sprintf(possiblePath, format,  dir, rootName);
+ 	bool found = (stat(possiblePath, &statBuffer) == 0);
+ 	if ( fTraceDylibSearching )
+-		printf("[Logging for XBS]%sfound library: '%s'\n", (found ? " " : " not "), possiblePath);
++		fprintf(stderr, "[Logging for XBS]%sfound library: '%s'\n", (found ? " " : " not "), possiblePath);
+ 	if ( found ) {
+ 		result.path = strdup(possiblePath);
+ 		result.fileLen = statBuffer.st_size;
+@@ -706,7 +713,7 @@
+ 		}
+ 		bool found = (stat(possiblePath, &statBuffer) == 0);
+ 		if ( fTraceDylibSearching )
+-			printf("[Logging for XBS]%sfound framework: '%s'\n",
++			fprintf(stderr, "[Logging for XBS]%sfound framework: '%s'\n",
+ 				   (found ? " " : " not "), possiblePath);
+ 		if ( found ) {
+ 			FileInfo result;
+# Continue the version‐number patch.
 @@ -2596,7 +2603,6 @@
  			addStandardLibraryDirectories = false;
  		else if ( strcmp(argv[i], "-v") == 0 ) {
@@ -175,7 +289,36 @@ __END__
  			fprintf(stderr, "%s", ldVersionString);
  			 // if only -v specified, exit cleanly
  			 if ( argc == 2 ) {
-@@ -2936,17 +2942,10 @@
+# This logic was missing the ppc64 case.  It was also incorrect if run on a pre‐Snow Leopard system.  It still is, but less so.
+@@ -2865,9 +2871,12 @@
+ 			switch ( fArchitecture ) {
+ 				case CPU_TYPE_I386:
+ 				case CPU_TYPE_X86_64:
+-				case CPU_TYPE_POWERPC:			
+-					fReaderOptions.fMacVersionMin = ObjectFile::ReaderOptions::k10_6; // FIX FIX, this really should be a check of the OS version the linker is running o
++				case CPU_TYPE_POWERPC64:
++					fReaderOptions.fMacVersionMin = ObjectFile::ReaderOptions::k10_4;  // FIXME:  Should use OS version we’re running on.
+ 					break;
++				case CPU_TYPE_POWERPC:
++					fReaderOptions.fMacVersionMin = ObjectFile::ReaderOptions::k10_3;  // FIXME:  Should use OS version we’re running on.
++					break;
+ 				case CPU_TYPE_ARM:
+ 					fReaderOptions.fIPhoneVersionMin = ObjectFile::ReaderOptions::k2_0;
+ 					break;
+# This logic was missing the ppc64 case.  This is _probably_ correct?
+@@ -2908,9 +2917,7 @@
+ 				fAllowTextRelocs = true;
+ 				fUndefinedTreatment = kUndefinedDynamicLookup;
+ 				break;
+-			case CPU_TYPE_POWERPC:
+-			case CPU_TYPE_I386:
+-			case CPU_TYPE_ARM:
++			default:
+ 				// use .o files
+ 				fOutputKind = kObjectFile;
+ 				break;
+# Fix the logic for what to do when internally handed a file pathname instead of a proper install path.
+@@ -2936,17 +2943,10 @@
  		parseSegAddrTable(fSegAddrTablePath, this->installPath());
  		// HACK to support seg_addr_table entries that are physical paths instead of install paths
  		if ( fBaseAddress == 0 ) {
