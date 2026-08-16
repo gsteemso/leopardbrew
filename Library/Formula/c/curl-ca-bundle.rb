f29cc064311e67c1da01eebc0da8f5a602ab6cab
Gordon Steemson <gsteemso@gmail.com>
2026-08-16 16:30:07 -0700

    curl-ca-bundle:  v2025-07-15 → 2026-07-16

--- Library/Formula/c/curl-ca-bundle.rb
+++ Library/Formula/c/curl-ca-bundle.rb
@@ -1,10 +1,10 @@
-# stable release 2025-07-15, checked 2025-08-02
+# stable release 2026-07-16, checked 2026-08-07
 class CurlCaBundle < Formula
   desc 'Modern certificate-authority bundle from the Curl project'
   homepage 'http://curl.haxx.se/docs/caextract.html'
-  url 'https://curl.se/ca/cacert-2025-07-15.pem', :using => :nounzip
-  version '2025-07-15'
-  sha256 '7430e90ee0cdca2d0f02b1ece46fbf255d5d0408111f009638e3b892d6ca089c'
+  url 'https://curl.se/ca/cacert-2026-07-16.pem', :using => :nounzip
+  version '2026-07-16'
+  sha256 '3ff344e30b9b1ed2971044eabb438a08f2e2245ddb5f8ab1a3ad8b63ab4eaf91'
 
   bottle do
     cellar :any
@@ -14,7 +14,7 @@ class CurlCaBundle < Formula
     share.install "cacert-#{version}.pem" => 'ca-bundle.crt'
     vendor_cert_bundle = HOMEBREW_RUBY_LIBRARY/'vendor/portable-curl/current/share/cacert.pem'
     rm_f vendor_cert_bundle if vendor_cert_bundle.exists?
-    cp share/'ca-bundle.crt', vendor_cert_bundle, :preserve => true
+    cp_p share/'ca-bundle.crt', vendor_cert_bundle
   end
 
   test { :does_not_apply }
